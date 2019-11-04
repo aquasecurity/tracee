1a99e28ccba2e5f6f2393abd2e475eee7070770b
Simarpreet Singh <simar@linux.com>
2019-11-04 14:09:35 -0800

    tracee_test: Add tests for execveat_flags_to_str
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- test_container_tracer.py
+++ test_container_tracer.py
@@ -14,6 +14,35 @@ class TestEventMonitor(unittest.TestCase):
         actualbpf = tracee.container_tracer.load_bpf_program()
         self.assertEqual(expectedbpf, actualbpf, "should be a valid bpf program")
 
+    def test_execveat_flags_to_str(self):
+        self.longMessage = True
+
+        test_cases = [
+            {
+                "name": "invalid flags",
+                "input": 0x666,
+                "expected": "",
+            },
+            {
+                "name": "empty path",
+                "input": 0x1000,
+                "expected": "AT_EMPTY_PATH",
+            },
+            {
+                "name": "symlink no follow",
+                "input": 0x100,
+                "expected": "AT_SYMLINK_NOFOLLOW",
+            },
+            {
+                "name": "symlink no follow with empty path",
+                "input": 0x1100,
+                "expected": "AT_EMPTY_PATH|AT_SYMLINK_NOFOLLOW",
+            },
+        ]
+        for test_case in test_cases:
+            self.assertEqual(test_case["expected"], tracee.container_tracer.execveat_flags_to_str(test_case["input"]),
+                             test_case["name"])
+
 
 if __name__ == '__main__':
     unittest.main()
