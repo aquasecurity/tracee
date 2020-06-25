32e7754d3863253f16f401e97fcd87ca62835109
yanivagman <yanivagman@gmail.com>
2020-06-25 09:12:44 +0300

    Check for privileges

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/iovisor/gobpf v0.0.0-20200529092446-49b58e11a4b5
 	github.com/stretchr/testify v1.5.1
+	github.com/syndtr/gocapability v0.0.0-20180916011248-d98352740cb2
 	github.com/urfave/cli/v2 v2.1.1
 )
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/aquasecurity/tracee/tracee"
 	"github.com/urfave/cli/v2"
+	"github.com/syndtr/gocapability/capability"
 )
 
 func main() {
@@ -108,10 +109,28 @@ func main() {
 		},
 	}
 
+	if (!isCapable()) {
+		log.Fatal("Not enough privileges to run this program")
+	}
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func isCapable() bool {
+	c, err := capability.NewPid2(0)
+	if err != nil {
+		fmt.Println("Current user capabilities could not be retrieved. Assure running with enough privileges")
+		return true
+	}
+	err = c.Load()
+	if err != nil {
+		fmt.Println("Current user capabilities could not be retrieved. Assure running with enough privileges")
+		return true
+	}
+
+	return c.Get(capability.EFFECTIVE, capability.CAP_SYS_ADMIN)
 }
 
 func printList() {
