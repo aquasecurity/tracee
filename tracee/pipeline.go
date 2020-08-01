46d9ccc6a5e49b21a47c1fb388fa8177137c5b59
yanivagman <yanivagman@gmail.com>
2020-08-02 01:25:52 +0300

    Handle events in a pipeline
    In order to achieve better performance, and as a preparation for the
    addition of plugins, handle events in a pipeline

---
+++ tracee/pipeline.go
@@ -0,0 +1,198 @@
+package tracee
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"sync"
+)
+
+func (t *Tracee) runEventPipeline(done <-chan struct{}) error {
+	var errcList []<-chan error
+
+	// Source pipeline stage.
+	rawEventChan, errc, err := t.decodeRawEvent(done)
+	if err != nil {
+		return err
+	}
+	errcList = append(errcList, errc)
+
+	processedEventChan, errc, err := t.processRawEvent(done, rawEventChan)
+	if err != nil {
+		return err
+	}
+	errcList = append(errcList, errc)
+
+	printEventChan, errc, err := t.prepareEventForPrint(done, processedEventChan)
+	if err != nil {
+		return err
+	}
+	errcList = append(errcList, errc)
+
+	errc, err = t.printEvent(done, printEventChan)
+	if err != nil {
+		return err
+	}
+	errcList = append(errcList, errc)
+
+	// Pipeline started. Waiting for pipeline to complete
+	return t.WaitForPipeline(errcList...)
+}
+
+type RawEvent struct {
+	Ctx      context
+	RawArgs  map[argTag]interface{}
+	ArgsTags []argTag
+}
+
+func (t *Tracee) decodeRawEvent(done <-chan struct{}) (<-chan RawEvent, <-chan error, error) {
+	out := make(chan RawEvent)
+	errc := make(chan error, 1)
+	go func() {
+		defer close(out)
+		defer close(errc)
+		for dataRaw := range t.eventsChannel {
+			dataBuff := bytes.NewBuffer(dataRaw)
+			var ctx context
+			err := binary.Read(dataBuff, binary.LittleEndian, &ctx)
+			if err != nil {
+				errc <- err
+				continue
+			}
+
+			rawArgs := make(map[argTag]interface{})
+			argsTags := make([]argTag, ctx.Argnum)
+			for i := 0; i < int(ctx.Argnum); i++ {
+				tag, val, err := readArgFromBuff(dataBuff)
+				if err != nil {
+					errc <- err
+					continue
+				}
+				argsTags[i] = tag
+				rawArgs[tag] = val
+			}
+			select {
+			case out <- RawEvent{ctx, rawArgs, argsTags}:
+			case <-done:
+				return
+			}
+		}
+	}()
+	return out, errc, nil
+}
+
+func (t *Tracee) processRawEvent(done <-chan struct{}, in <-chan RawEvent) (<-chan RawEvent, <-chan error, error) {
+	out := make(chan RawEvent)
+	errc := make(chan error, 1)
+	go func() {
+		defer close(out)
+		defer close(errc)
+		for rawEvent := range in {
+			err := t.processEvent(&rawEvent.Ctx, rawEvent.RawArgs)
+			if err != nil {
+				errc <- err
+				continue
+			}
+			select {
+			case out <- rawEvent:
+			case <-done:
+				return
+			}
+		}
+	}()
+	return out, errc, nil
+}
+
+func (t *Tracee) prepareEventForPrint(done <-chan struct{}, in <-chan RawEvent) (<-chan Event, <-chan error, error) {
+	out := make(chan Event, 1000)
+	errc := make(chan error, 1)
+	go func() {
+		defer close(out)
+		defer close(errc)
+		for rawEvent := range in {
+			if !t.shouldPrintEvent(rawEvent.Ctx.EventID) {
+				continue
+			}
+			err := t.prepareArgsForPrint(&rawEvent.Ctx, rawEvent.RawArgs)
+			if err != nil {
+				errc <- err
+				continue
+			}
+			args := make([]interface{}, rawEvent.Ctx.Argnum)
+			argsNames := make([]string, rawEvent.Ctx.Argnum)
+			for i, tag := range rawEvent.ArgsTags {
+				args[i] = rawEvent.RawArgs[tag]
+				argName, ok := argNames[tag]
+				if ok {
+					argsNames[i] = argName
+				} else {
+					errc <- fmt.Errorf("Invalid arg tag")
+					continue
+				}
+			}
+			evt, err := newEvent(rawEvent.Ctx, argsNames, args)
+			if err != nil {
+				errc <- err
+				continue
+			}
+			select {
+			case out <- evt:
+			case <-done:
+				return
+			}
+		}
+	}()
+	return out, errc, nil
+}
+
+func (t *Tracee) printEvent(done <-chan struct{}, in <-chan Event) (<-chan error, error) {
+	errc := make(chan error, 1)
+	go func() {
+		defer close(errc)
+		for printEvent := range in {
+			t.stats.eventCounter.Increment()
+			t.printer.Print(printEvent)
+		}
+	}()
+	return errc, nil
+}
+
+// WaitForPipeline waits for results from all error channels.
+func (t *Tracee) WaitForPipeline(errs ...<-chan error) error {
+	errc := MergeErrors(errs...)
+	for err := range errc {
+		t.handleError(err)
+	}
+	return nil
+}
+
+// MergeErrors merges multiple channels of errors.
+// Based on https://blog.golang.org/pipelines.
+func MergeErrors(cs ...<-chan error) <-chan error {
+	var wg sync.WaitGroup
+	// We must ensure that the output channel has the capacity to hold as many errors
+	// as there are error channels. This will ensure that it never blocks, even
+	// if WaitForPipeline returns early.
+	out := make(chan error, len(cs))
+
+	// Start an output goroutine for each input channel in cs.  output
+	// copies values from c to out until c is closed, then calls wg.Done.
+	output := func(c <-chan error) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	// Start a goroutine to close out once all the output goroutines are
+	// done.  This must start after the wg.Add call.
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
