83409d847777acb3cdde93e29a20201a4483f4be
Itay Shakury <itay@itaysk.com>
2020-08-05 13:59:06 +0000

    retstore tid to table

--- tracee/printer.go
+++ tracee/printer.go
@@ -125,9 +125,9 @@ func (p tableEventPrinter) Preamble() {
 		}
 	} else {
 		if p.containerMode {
-			fmt.Fprintf(p.out, "%-14s %-6s %-16s %-15s %-16s %-20s %s", "TIME(s)", "UID", "COMM", "PID/host", "RET", "EVENT", "ARGS")
+			fmt.Fprintf(p.out, "%-14s %-6s %-16s %-15s %-15s %-16s %-20s %s", "TIME(s)", "UID", "COMM", "PID/host", "TID/host", "RET", "EVENT", "ARGS")
 		} else {
-			fmt.Fprintf(p.out, "%-14s %-6s %-16s %-7s %-16s %-20s %s", "TIME(s)", "UID", "COMM", "PID", "RET", "EVENT", "ARGS")
+			fmt.Fprintf(p.out, "%-14s %-6s %-16s %-7s %-7s %-16s %-20s %s", "TIME(s)", "UID", "COMM", "PID", "TID", "RET", "EVENT", "ARGS")
 		}
 	}
 	fmt.Fprintln(p.out)
@@ -142,9 +142,9 @@ func (p tableEventPrinter) Print(event Event) {
 		}
 	} else {
 		if p.containerMode {
-			fmt.Fprintf(p.out, "%-14f %-6d %-16s %-7d/%-7d %-16d %-20s ", event.Timestamp, event.UserID, event.ProcessName, event.ProcessID, event.HostProcessID, event.ReturnValue, event.EventName)
+			fmt.Fprintf(p.out, "%-14f %-6d %-16s %-7d/%-7d %-7d/%-7d %-16d %-20s ", event.Timestamp, event.UserID, event.ProcessName, event.ProcessID, event.HostProcessID, event.ThreadID, event.HostThreadID, event.ReturnValue, event.EventName)
 		} else {
-			fmt.Fprintf(p.out, "%-14f %-6d %-16s %-7d %-16d %-20s ", event.Timestamp, event.UserID, event.ProcessName, event.ProcessID, event.ReturnValue, event.EventName)
+			fmt.Fprintf(p.out, "%-14f %-6d %-16s %-7d %-7d% -16d %-20s ", event.Timestamp, event.UserID, event.ProcessName, event.ProcessID, event.ThreadID, event.ReturnValue, event.EventName)
 		}
 	}
 	for _, arg := range event.Args {
