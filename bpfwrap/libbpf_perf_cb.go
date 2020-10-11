f44f4b29db0676878e3224e6bcf9eee894ba10ea
yanivagman <yanivagman@gmail.com>
2020-10-11 12:34:21 +0300

    Add bpfwrap - a thin libbpf wrapper
    bpfwrap will replace gobpf as the bpf library used by Tracee.
    bpfwrap is a wrapper to libbpf, which is written in C.
    The advantages of using bpfwrap over gobpf are the following:
    - no bcc dependancy - only clang is required to compile the code
    - compile once - compilation can take place during installation
      on a given node. Then, the compiled object is used in future runs.
      This have the advantage that Tracee will not have unnecessary delays
      on start.
    - libbpf is being developed as part of the linux kernel. This means it
      will always get the latest features, and that it is well maintained.
    - the move to libbpf will allow us to add support for CO-RE in the
      future. With CO-RE, bpf object is Compiled Once, and can then
      Run Everywhere without the need to compile the code again for different
      kernels

---
+++ bpfwrap/libbpf_perf_cb.go
@@ -0,0 +1,22 @@
+package bpfwrap
+
+import (
+	"C"
+	"unsafe"
+)
+
+// This callback definition needs to be in a different file from where it is declared in C
+// Otherwise, multiple definition compilation error will occur
+
+//export perfCallback
+func perfCallback(ctx unsafe.Pointer, cpu C.int, data unsafe.Pointer, size C.int) {
+	eventChannels[uintptr(ctx)] <- C.GoBytes(data, size)
+}
+
+//export perfLostCallback
+func perfLostCallback(ctx unsafe.Pointer, cpu C.int, cnt C.ulonglong) {
+	lostChan := lostChannels[uintptr(ctx)]
+	if lostChan != nil {
+		lostChan <- uint64(cnt)
+	}
+}
