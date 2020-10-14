4a0819331f6981ddc604824355f72e425bedf776
yanivagman <yanivagman@gmail.com>
2020-10-14 12:21:21 +0300

    fix memfd files not shown in vfs_write

--- tracee/event_monitor_ebpf.c
+++ tracee/event_monitor_ebpf.c
@@ -804,7 +804,7 @@ static __always_inline int save_file_path_to_str_buf(buf_t *string_p, struct fil
         dentry = d_parent;
     }
 
-    if (buf_off == MAX_PERCPU_BUFSIZE - MAX_STRING_SIZE) {
+    if (buf_off == (MAX_PERCPU_BUFSIZE >> 1)) {
         // memfd files have no path in the filesystem -> extract their name
         buf_off = 0;
         struct qstr d_name = get_d_name_from_dentry(dentry);
