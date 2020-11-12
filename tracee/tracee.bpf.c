aefd3cd5a0ebe8817d1ec4d1a29701488aa7bf6d
yanivagman <yanivagman@gmail.com>
2020-11-12 20:51:59 +0200

    Fix asm_inline for kernel > 5.4

--- tracee/tracee.bpf.c
+++ tracee/tracee.bpf.c
@@ -1,6 +1,16 @@
 // +build ignore
 // ^^ this is a golang build tag meant to exclude this C file from compilation by the CGO compiler
 
+/* In Linux 5.4 asm_inline was introduced, but it's not supported by clang.
+ * Redefine it to just asm to enable successful compilation.
+ * see https://github.com/iovisor/bcc/commit/2d1497cde1cc9835f759a707b42dea83bee378b8 for more details
+ */
+#include <linux/types.h>
+#ifdef asm_inline
+#undef asm_inline
+#define asm_inline asm
+#endif
+
 #include <uapi/linux/ptrace.h>
 #include <uapi/linux/in.h>
 #include <uapi/linux/in6.h>
@@ -21,7 +31,6 @@
 
 #include <uapi/linux/bpf.h>
 #include <linux/kconfig.h>
-#include <linux/types.h>
 #include <linux/version.h>
 
 #undef container_of
