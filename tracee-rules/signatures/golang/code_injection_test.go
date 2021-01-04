4025eff51bb490ad52f36c8699ca46b81050940d
Itay Shakury <itay@itaysk.com>
2021-01-04 15:27:33 +0200

    add code injection signature

---
+++ tracee-rules/signatures/golang/code_injection_test.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aquasecurity/tracee/tracee-rules/types"
+)
+
+func TestCodeInjection(t *testing.T) {
+	sigTests := []sigTest{
+		{
+			events: []types.Event{
+				types.TraceeEvent{
+					EventName: "ptrace",
+					Args: []types.TraceeEventArgument{
+						{
+							Name:  "request",
+							Value: "PTRACE_POKETEXT",
+						},
+					},
+				},
+			},
+			expect: true,
+		},
+		{
+			events: []types.Event{
+				types.TraceeEvent{
+					EventName: "ptrace",
+					Args: []types.TraceeEventArgument{
+						{
+							Name:  "request",
+							Value: "PTRACE_TRACEME",
+						},
+					},
+				},
+			},
+			expect: false,
+		},
+		{
+			events: []types.Event{
+				types.TraceeEvent{
+					EventName: "open",
+					Args: []types.TraceeEventArgument{
+						{
+							Name:  "pathname",
+							Value: "/proc/self/mem",
+						},
+						{
+							Name:  "flags",
+							Value: "o_wronly",
+						},
+					},
+				},
+			},
+			expect: true,
+		},
+		{
+			events: []types.Event{
+				types.TraceeEvent{
+					EventName: "open",
+					Args: []types.TraceeEventArgument{
+						{
+							Name:  "pathname",
+							Value: "/proc/foo",
+						},
+						{
+							Name:  "flags",
+							Value: "o_wronly",
+						},
+					},
+				},
+			},
+			expect: false,
+		},
+		{
+			events: []types.Event{
+				types.TraceeEvent{
+					EventName: "open",
+					Args: []types.TraceeEventArgument{
+						{
+							Name:  "pathname",
+							Value: "/proc/self/mem",
+						},
+						{
+							Name:  "flags",
+							Value: "o_rdonly",
+						},
+					},
+				},
+			},
+			expect: false,
+		},
+		{
+			events: []types.Event{
+				types.TraceeEvent{
+					EventName: "execve",
+					Args: []types.TraceeEventArgument{
+						{
+							Name:  "envp",
+							Value: []string{"FOO=BAR", "LD_PRELOAD=/something"},
+						},
+						{
+							Name:  "argv",
+							Value: []string{"ls"},
+						},
+					},
+				},
+			},
+			expect: true,
+		},
+		{
+			events: []types.Event{
+				types.TraceeEvent{
+					EventName: "execve",
+					Args: []types.TraceeEventArgument{
+						{
+							Name:  "envp",
+							Value: []string{"FOO=BAR"},
+						},
+						{
+							Name:  "argv",
+							Value: []string{"ls"},
+						},
+					},
+				},
+			},
+			expect: false,
+		},
+		{
+			events: []types.Event{
+				types.TraceeEvent{
+					EventName: "execve",
+					Args: []types.TraceeEventArgument{
+						{
+							Name:  "argv",
+							Value: []string{"ls"},
+						},
+					},
+				},
+			},
+			expect: false,
+		},
+	}
+	for _, st := range sigTests {
+		sig := codeInjection{}
+		st.init(&sig)
+		for _, e := range st.events {
+			err := sig.OnEvent(e)
+			if err != nil {
+				t.Error(err, st)
+			}
+		}
+		if st.expect != st.status {
+			t.Error("unexpected result", st)
+		}
+	}
+}
