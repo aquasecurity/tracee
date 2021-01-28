8a615acd33b651b0580b46da6b1e1b434426127e
Simarpreet Singh <simar@linux.com>
2021-01-27 17:17:09 -0800

    feat: engine_test.go: Add more testcases for for consumeSources
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- tracee-rules/engine/engine_test.go
+++ tracee-rules/engine/engine_test.go
@@ -1,40 +1,90 @@
 package engine
 
 import (
-	"path/filepath"
 	"testing"
-
-	"github.com/aquasecurity/tracee/tracee-rules/signatures"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"time"
 
 	"github.com/aquasecurity/tracee/tracee-rules/types"
+	"github.com/stretchr/testify/assert"
 )
+
+type fakeSignature struct {
+	getMetadata       func() (types.SignatureMetadata, error)
+	getSelectedEvents func() ([]types.SignatureEventSelector, error)
+	init              func(types.SignatureHandler) error
+	onEvent           func(types.Event) error
+	onSignal          func(signal types.Signal) error
+}
+
+func (fs fakeSignature) GetMetadata() (types.SignatureMetadata, error) {
+	if fs.getMetadata != nil {
+		return fs.getMetadata()
+	}
+
+	return types.SignatureMetadata{
+		Name: "Fake Signature",
+	}, nil
+}
+
+func (fs fakeSignature) GetSelectedEvents() ([]types.SignatureEventSelector, error) {
+	if fs.getSelectedEvents != nil {
+		return fs.getSelectedEvents()
+	}
+
+	return []types.SignatureEventSelector{}, nil
+}
+
+func (fs fakeSignature) Init(cb types.SignatureHandler) error {
+	if fs.init != nil {
+		return fs.init(cb)
+	}
+	return nil
+}
+
+func (fs fakeSignature) OnEvent(event types.Event) error {
+	if fs.onEvent != nil {
+		return fs.onEvent(event)
+	}
+	return nil
+}
+
+func (fs fakeSignature) OnSignal(signal types.Signal) error {
+	if fs.onSignal != nil {
+		return fs.onSignal(signal)
+	}
+	return nil
+}
 
 func TestEngine_consumeSource(t *testing.T) {
 	testCases := []struct {
-		name         string
-		inputEvent   types.TraceeEvent
-		expectedArgs []types.TraceeEventArgument
+		name              string
+		inputEvent        types.TraceeEvent
+		inputSignature    fakeSignature
+		expectedNumEvents int
 	}{
 		{
-			name: "happy path - with no matching filter",
+			name: "happy path - with one matching selector",
 			inputEvent: types.TraceeEvent{
-				EventName:       "test_event",
-				ProcessID:       2,
-				ParentProcessID: 1,
+				EventName: "test_event",
+				ProcessID: 2, ParentProcessID: 1,
 				Args: []types.TraceeEventArgument{{
 					Name: "test_foo",
 				}},
 			},
-			expectedArgs: []types.TraceeEventArgument{
-				{
-					Name: "test_foo",
+			inputSignature: fakeSignature{
+				getSelectedEvents: func() ([]types.SignatureEventSelector, error) {
+					return []types.SignatureEventSelector{
+						{
+							Name:   "test_event",
+							Source: "tracee",
+						},
+					}, nil
 				},
 			},
+			expectedNumEvents: 1,
 		},
 		{
-			name: "happy path - with matching filter on EventName",
+			name: "happy path - with no matching event selector",
 			inputEvent: types.TraceeEvent{
 				EventName:       "execve",
 				ProcessID:       2,
@@ -43,13 +93,42 @@ func TestEngine_consumeSource(t *testing.T) {
 					Name: "foobarbaz",
 				}},
 			},
-			expectedArgs: []types.TraceeEventArgument{
-				{
-					Name: "foobarbaz",
+			inputSignature: fakeSignature{
+				getSelectedEvents: func() ([]types.SignatureEventSelector, error) {
+					return []types.SignatureEventSelector{
+						{
+							Name:   "not execve",
+							Source: "tracee",
+						},
+					}, nil
 				},
 			},
+			expectedNumEvents: 0,
+		},
+		{
+			name: "happy path - with all events selector",
+			inputEvent: types.TraceeEvent{
+				EventName:       "execve",
+				ProcessID:       2,
+				ParentProcessID: 1,
+				Args: []types.TraceeEventArgument{{
+					Name: "foobarbaz",
+				}},
+			},
+			inputSignature: fakeSignature{
+				getSelectedEvents: func() ([]types.SignatureEventSelector, error) {
+					return []types.SignatureEventSelector{
+						{
+							Name:   "*",
+							Source: "tracee",
+						},
+					}, nil
+				},
+			},
+			expectedNumEvents: 1,
 		},
 	}
+	//TODO: Implement a sig which returns bad GetSelectedEvents()
 
 	for _, tc := range testCases {
 		inputs := EventSources{}
@@ -67,12 +146,17 @@ func TestEngine_consumeSource(t *testing.T) {
 				close(inputs.Tracee)
 			}()
 
-			// gather all sigs
-			sigs, err := signatures.GetSignatures(filepath.Join("../dist/rules"), nil)
-			require.NoError(t, err, tc.name)
-			require.NotEmpty(t, sigs, tc.name)
-			e := NewEngine(sigs, inputs, outputChan, nil)
+			var sigs []types.Signature
+			sigs = append(sigs, &tc.inputSignature)
 
+			var gotNumEvents int
+			tc.inputSignature.onEvent = func(event types.Event) error {
+				assert.Equal(t, tc.inputEvent, event, tc.name)
+				gotNumEvents++
+				return nil
+			}
+
+			e := NewEngine(sigs, inputs, outputChan, nil)
 			go func() {
 				e.Start(done)
 			}()
@@ -81,20 +165,16 @@ func TestEngine_consumeSource(t *testing.T) {
 			e.inputs.Tracee <- tc.inputEvent
 
 			// assert
-			for _, s := range e.signaturesIndex[types.SignatureEventSelector{Source: "tracee", Name: "*"}] {
-				gotEvent := (<-e.signatures[s]).(types.TraceeEvent)
-				if gotEvent.EventName == tc.inputEvent.EventName {
-					assert.Equal(t, tc.expectedArgs, gotEvent.Args, tc.name)
-					return
-				}
-			}
+			var gotEvent types.Event
+			time.Sleep(time.Second * 1) // wait for events to propagate
 
-			for _, s := range e.signaturesIndex[types.SignatureEventSelector{Source: "tracee", Name: tc.inputEvent.EventName}] {
-				gotEvent := (<-e.signatures[s]).(types.TraceeEvent)
-				if gotEvent.EventName == tc.inputEvent.EventName {
-					assert.Equal(t, tc.expectedArgs, gotEvent.Args, tc.name)
-					return
-				}
+			if tc.expectedNumEvents <= 0 {
+				assert.Nil(t, gotEvent, tc.name)
+				assert.Zero(t, gotNumEvents, tc.name)
+				return
+			} else {
+				assert.Equal(t, tc.expectedNumEvents, gotNumEvents, tc.name)
+				return
 			}
 		})
 	}
