5f4ab2d52edb96b189ab9b6a434ae3fbf952eaff
Itay Shakury <itay@itaysk.com>
2021-03-13 14:44:16 +0000

    remove falcosidekick from container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,29 @@
 #!/bin/sh
 
 TRACEE_EBPF_EXE=${TRACEE_EBPF_EXE:="/tracee/tracee-ebpf"}
-TRACEE_WEBHOOK_CONFIG=${TRACEE_WEBHOOK_CONFIG:="/tmp/tracee/integrations-config.yaml"}
-TRACEE_WEBHOOK_EXE=${TRACEE_WEBHOOK_EXE:="/tracee/falcosidekick"}
 TRACEE_RULES_EXE=${TRACEE_RULES_EXE:="/tracee/tracee-rules"}
 
+# handle sub commands
 if [ "$1" = "trace" ]; then
 	shift
 	$TRACEE_EBPF_EXE $@
-	exit
+	exit "$?"
 fi
 
-if [ "$1" = "--list" ]; then
-	shift
-	$TRACEE_RULES_EXE --list
-	exit
-fi
+# override existing flags from tracee-rules
+for arg in "$@"; do
+	case arg in
+		--list)
+			$TRACEE_RULES_EXE --list
+			exit "$?"
+			;;
+		--help)
+			echo "Tracee - Runtime Security using eBPF"
+			echo "For help, see https://github.com/aquasecurity/tracee"
+			exit
+			;;
+	esac
+done
 
-if [ "$1" = "--rules" ]; then
-	flag_rules="$1=$2"
-fi
-
-if [ -f $TRACEE_WEBHOOK_CONFIG ]; then
-	flag_webhook_config="--config-file=$TRACEE_WEBHOOK_CONFIG"
-fi
-
-if $TRACEE_WEBHOOK_EXE $flag_webhook_config& then
-	flag_webhook_url="--webhook=http://localhost:2801"
-fi
-
-$TRACEE_EBPF_EXE --output=format:gob --security-alerts | $TRACEE_RULES_EXE --input-tracee=file:stdin --input-tracee=format:gob $flag_webhook_url $flag_rules
+# start, and pass all remaining flags to tracee-rules
+$TRACEE_EBPF_EXE --output=format:gob --security-alerts | $TRACEE_RULES_EXE --input-tracee=file:stdin --input-tracee=format:gob $@
