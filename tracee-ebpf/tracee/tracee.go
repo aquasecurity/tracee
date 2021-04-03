89b4ff8a78c33ca9042d5f0711dd76ec2ec5f611
grantseltzer <grantseltzer@gmail.com>
2021-04-03 23:27:37 +0000

    feat: load in kernel config options from go using libbpfgo
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- tracee-ebpf/tracee/tracee.go
+++ tracee-ebpf/tracee/tracee.go
@@ -721,6 +721,15 @@ func (t *Tracee) populateBPFMaps() error {
 	stringStoreMap, _ := t.bpfModule.GetMap("string_store")
 	stringStoreMap.Update(uint32(0), []byte("/dev/null"))
 
+	kernelConfig := helpers.KernelConfig{}
+	kernelConfig.InitKernelConfig()
+
+	kernelConfigMap, _ := t.bpfModule.GetMap("kernel_config_map")
+	v, err := kernelConfig.GetKernelConfigValue(helpers.CONFIG_DEBUG_INFO_BTF)
+	if err == nil && v == "y" {
+		kernelConfigMap.Update(helpers.CONFIG_DEBUG_INFO_BTF, uint32(1))
+	}
+
 	eventsParams := t.initEventsParams()
 
 	// After initializing event params, we can also initialize argument filters argTags
