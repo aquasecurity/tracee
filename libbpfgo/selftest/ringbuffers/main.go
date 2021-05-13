242d721bad3dc698540464b15a15f6aa1f16f760
Eugene Yakubovich <eyakubovich@gmail.com>
2021-05-13 07:21:45 -0700

    libbpfgo: Check for ERR_PTR return values (#709)
    * libbpfgo: Check for ERR_PTR return values
    
    Some libbpf functions fail and return an -errno
    encoded inside the pointer. Therefore checking
    for NULL is not enough.
    
    Checks the returned pointers for NULL or ERR_PTR
    encoding and converts the errno (if available)
    to an error message.
    
    * libbpfgo: run unit tests via Makefile

--- libbpfgo/selftest/ringbuffers/main.go
+++ libbpfgo/selftest/ringbuffers/main.go
@@ -32,28 +32,33 @@ func main() {
 
 	bpfModule, err := bpf.NewModuleFromFile("self.bpf.o")
 	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(-1)
 	}
 	defer bpfModule.Close()
 
 	if err = resizeMap(bpfModule, "events", 8192); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(-1)
 	}
 
 	bpfModule.BPFLoadObject()
 	prog, err := bpfModule.GetProgram("kprobe__sys_mmap")
 	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(-1)
 	}
 
 	_, err = prog.AttachKprobe("__x64_sys_mmap")
 	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(-1)
 	}
 
 	eventsChannel := make(chan []byte)
 	rb, err := bpfModule.InitRingBuf("events", eventsChannel)
 	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(-1)
 	}
 
