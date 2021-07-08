170c926677e2cc116e8da3e68f2437ecbcdf76b9
Simar <simar@linux.com>
2021-07-07 18:50:42 -0700

    example: update to use latest libtracee bindings

--- example/go.mod
+++ example/go.mod
@@ -4,5 +4,5 @@ go 1.16
 
 require (
 	github.com/aquasecurity/tracee/tracee-rules v0.0.0-20210707140844-faa56142fcd8
-	github.com/simar7/libtracee v0.0.0-20210707200548-204dfeed1b68
+	github.com/simar7/libtracee v0.0.0-20210708012022-f2d45711915f
 )
--- example/main.go
+++ example/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
+
+	"github.com/aquasecurity/tracee/tracee-rules/benchmark/signature/rego"
 
 	"github.com/aquasecurity/tracee/tracee-rules/benchmark/signature/golang"
 	"github.com/aquasecurity/tracee/tracee-rules/types"
@@ -11,22 +14,40 @@ import (
 )
 
 const (
-	inputEventsCount = 10000
+	inputEventsCount = 100
 )
 
 func main() {
-	totalEvents, tmpDir := libtracee.SetupTracee(libtracee.AddNSigs([]func() (types.Signature, error){
+	eventsProcessed, outputChan, err := libtracee.SetupTracee(libtracee.AddNSigs([]func() (types.Signature, error){
 		golang.NewCodeInjectionSignature,
-		//rego.NewCodeInjectionSignature,
+		rego.NewCodeInjectionSignature,
 		//wasm.NewCodeInjectionSignature,
-	}), inputEventsCount)
-	defer func() {
-		_ = os.RemoveAll(tmpDir)
-	}()
+	}), inputEventsCount, getBPFObjectPath())
+	if err != nil {
+		log.Fatal("unable to initialize Tracee: ", err)
+	}
+
+	// show all findings
+	go func(outputChan chan types.Finding) {
+		showFindings(outputChan)
+	}(outputChan)
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt)
 	fmt.Println("waiting for interrupt to finish...")
 	<-sig
-	fmt.Println("total events: ", *totalEvents)
+	fmt.Println("total events processed: ", *eventsProcessed)
+}
+
+func showFindings(outputChan chan types.Finding) {
+	for {
+		select {
+		case finding := <-outputChan:
+			fmt.Println(finding)
+		}
+	}
+}
+
+func getBPFObjectPath() string {
+	return "/home/vagrant/repos/tracee/tracee-ebpf/dist/tracee.bpf.5_8_0-55-generic.v0_5_4-18-g31f21b8.o" // TODO: Export getBPFObject() in Tracee
 }
