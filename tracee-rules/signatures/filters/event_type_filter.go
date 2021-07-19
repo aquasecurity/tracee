48b982dd533acbafa2fda2273610011f0a6646dd
Alon Zivony <alon.zivony@aquasec.com>
2021-07-19 19:36:42 +0300

    Removed dependency on tracee-ebpf

--- tracee-rules/signatures/filters/event_type_filter.go
+++ tracee-rules/signatures/filters/event_type_filter.go
@@ -5,9 +5,10 @@ import (
 
 	"github.com/RoaringBitmap/roaring"
 	tracee "github.com/aquasecurity/tracee/tracee-ebpf/external"
-	tracee_consts "github.com/aquasecurity/tracee/tracee-ebpf/tracee"
 	"github.com/aquasecurity/tracee/tracee-rules/types"
 )
+
+const ALL_EVENT_TYPES = "*"
 
 type EventTypeFilter struct {
 	signatureBitmapMatcher map[types.Event]*roaring.Bitmap
@@ -19,9 +20,8 @@ func CreateEventFilter(signatures []types.Signature, logger *log.Logger) (*Event
 	eventFilter := EventTypeFilter{}
 	eventFilter.logger = logger
 	eventFilter.signatureBitmapMatcher = make(map[types.Event]*roaring.Bitmap)
-	for _, event := range tracee_consts.EventsIDToEvent {
-		eventFilter.signatureBitmapMatcher[event.Name] = roaring.New()
-	}
+	// Bitmap for all event types must be initialized.
+	eventFilter.signatureBitmapMatcher[ALL_EVENT_TYPES] = roaring.New()
 
 	// Add all signatures to the matching event filter Bitmap.
 	for signatureIndex, signature := range signatures {
@@ -36,13 +36,13 @@ func CreateEventFilter(signatures []types.Signature, logger *log.Logger) (*Event
 			continue
 		}
 		for _, selectedEvent := range selectedEvents {
-			if selectedEvent.Name == "*" || selectedEvent.Name == "" {
-				for _, eventFilterBitmap := range eventFilter.signatureBitmapMatcher {
-					eventFilterBitmap.Add(uint32(signatureIndex))
-				}
-			} else {
-				eventFilter.signatureBitmapMatcher[selectedEvent.Name].Add(uint32(signatureIndex))
+			if selectedEvent.Name == "" {
+				selectedEvent.Name = ALL_EVENT_TYPES
 			}
+			if eventFilter.signatureBitmapMatcher[selectedEvent.Name] == nil {
+				eventFilter.signatureBitmapMatcher[selectedEvent.Name] = roaring.New()
+			}
+			eventFilter.signatureBitmapMatcher[selectedEvent.Name].Add(uint32(signatureIndex))
 		}
 	}
 	return &eventFilter, nil
@@ -50,12 +50,17 @@ func CreateEventFilter(signatures []types.Signature, logger *log.Logger) (*Event
 
 // Return a bitmap representing all the signatures that watch the given event't type
 func (eventFilter *EventTypeFilter) FilterByEvent(filteredEvent types.Event) (*roaring.Bitmap, error) {
-	return eventFilter.signatureBitmapMatcher[filteredEvent.(tracee.Event).EventName], nil
+	eventBitmap := eventFilter.signatureBitmapMatcher[filteredEvent.(tracee.Event).EventName]
+	allEventsBitmap := eventFilter.signatureBitmapMatcher[ALL_EVENT_TYPES]
+	return roaring.Or(eventBitmap, allEventsBitmap), nil
 }
 
 func (eventFilter *EventTypeFilter) AddSignature(signature types.Signature, uid uint32) error {
 	sigSelectedEvents, _ := signature.GetSelectedEvents()
 	for _, selectedEvent := range sigSelectedEvents {
+		if selectedEvent.Name == "" {
+			selectedEvent.Name = ALL_EVENT_TYPES
+		}
 		eventFilter.signatureBitmapMatcher[selectedEvent.Name].Add(uid)
 	}
 	return nil
