73f9f709c4d1c6c9dfe05a64f9e4cfee92832333
Alon Zivony <alon.zivony@aquasec.com>
2021-07-27 14:49:45 +0300

    Added test for signature removal in FilterManager

--- tracee-rules/signatures/filters/filter_manager_test.go
+++ tracee-rules/signatures/filters/filter_manager_test.go
@@ -37,41 +37,55 @@ func (fs basicFakeSignature) OnSignal(signal types.Signal) error {
 }
 func (fs basicFakeSignature) Close() {}
 
-func TestNewFilterManager(t *testing.T) {
-	matchingSignature := basicFakeSignature{
-		metadata: types.SignatureMetadata{
-			Name: "Fake Signature",
+var matchingSignature = basicFakeSignature{
+	metadata: types.SignatureMetadata{
+		Name: "Fake Signature",
+	},
+	selector: []types.SignatureEventSelector{
+		{
+			Name:   "test_event",
+			Source: "tracee",
 		},
-		selector: []types.SignatureEventSelector{
-			{
-				Name:   "test_event",
-				Source: "tracee",
-			},
+	},
+}
+var notMatchingSignature = basicFakeSignature{
+	metadata: types.SignatureMetadata{
+		Name: "Fake Signature",
+	},
+	selector: []types.SignatureEventSelector{
+		{
+			Name:   "not_matching_type",
+			Source: "tracee",
 		},
-	}
-	notMatchingSignature := basicFakeSignature{
-		metadata: types.SignatureMetadata{
-			Name: "Fake Signature",
-		},
-		selector: []types.SignatureEventSelector{
-			{
-				Name:   "not_matching_type",
-				Source: "tracee",
-			},
-		},
-	}
-	allMatchingSignature := basicFakeSignature{
-		metadata: types.SignatureMetadata{
-			Name: "Fake Signature",
-		},
-		selector: []types.SignatureEventSelector{
-			{
-				Name:   "*",
-				Source: "tracee",
-			},
-		},
-	}
+	},
+}
 
+var allMatchingSignature = basicFakeSignature{
+	metadata: types.SignatureMetadata{
+		Name: "Fake Signature",
+	},
+	selector: []types.SignatureEventSelector{
+		{
+			Name:   "*",
+			Source: "tracee",
+		},
+	},
+}
+var event = tracee.Event{
+	EventName:       "test_event",
+	ProcessID:       2,
+	ParentProcessID: 1,
+	Args: []tracee.Argument{
+		{
+			ArgMeta: tracee.ArgMeta{
+				Name: "pathname",
+			},
+			Value: "/proc/self/mem",
+		},
+	},
+}
+
+func TestNewFilterManager(t *testing.T) {
 	testCases := []struct {
 		name            string
 		inputSignatures []basicFakeSignature
@@ -104,19 +118,6 @@ func TestNewFilterManager(t *testing.T) {
 			matchingSigs:    []basicFakeSignature{matchingSignature, allMatchingSignature},
 		},
 	}
-	event := tracee.Event{
-		EventName:       "test_event",
-		ProcessID:       2,
-		ParentProcessID: 1,
-		Args: []tracee.Argument{
-			{
-				ArgMeta: tracee.ArgMeta{
-					Name: "pathname",
-				},
-				Value: "/proc/self/mem",
-			},
-		},
-	}
 	logger := *log.New(os.Stdout, "", 0)
 
 	for _, testCase := range testCases {
@@ -135,4 +136,34 @@ func TestNewFilterManager(t *testing.T) {
 		require.NoError(t, err, "get filtered events for example one")
 		assert.Equalf(t, expectedResultSignatures, filteredSignatures, testCase.name)
 	}
+}
+
+func TestFilterManager_AddSignature(t *testing.T) {
+	logger := *log.New(os.Stdout, "", 0)
+	filterManager, err := NewFilterManager(&logger, []types.Signature{})
+	require.NoError(t, err, "creating FilterManager")
+	filteredSigs, err := filterManager.GetFilteredSignatures(event)
+	require.NoError(t, err, "Getting matching signatures without registered signatures")
+	require.EqualValues(t, []types.Signature{}, filteredSigs, "Getting matching signatures without registered signatures")
+	err = filterManager.AddSignature(matchingSignature)
+	require.NoError(t, err, "Adding new signature to the FilterManager")
+	filteredSigs, err = filterManager.GetFilteredSignatures(event)
+	assert.NoError(t, err, "Getting matching signatures with matching registered signature")
+	assert.EqualValues(t, []types.Signature{matchingSignature}, filteredSigs, "Getting matching signatures with matching registered signature")
+}
+
+func TestFilterManager_RemoveSignature(t *testing.T) {
+	logger := *log.New(os.Stdout, "", 0)
+	filterManager, err := NewFilterManager(&logger, []types.Signature{matchingSignature})
+	require.NoError(t, err, "creating FilterManager with a single signature")
+	filteredSigs, err := filterManager.GetFilteredSignatures(event)
+	require.NoError(t, err, "Getting matching signatures with matching registered signature")
+	require.EqualValues(t, []types.Signature{matchingSignature}, filteredSigs, "Getting matching signatures with matching registered signature")
+	err = filterManager.RemoveSignature(matchingSignature)
+	require.NoError(t, err, "Removing the matching signature from the FilterManager")
+	filteredSigs, err = filterManager.GetFilteredSignatures(event)
+	assert.NoError(t, err, "Getting matching signatures without matching registered signature")
+	assert.EqualValues(t, []types.Signature{}, filteredSigs, "Getting matching signatures without matching registered signature")
+	err = filterManager.RemoveSignature(notMatchingSignature)
+	assert.Error(t, err, "Removing non existing signature from the FilterManager")
 }
