bd2ccf64448de0f1238a6f3af2245353eafdd8e1
Jose Donizetti <jdbjunior@gmail.com>
2021-08-04 13:16:07 -0300

    tracee-ebpf: add Print16BytesSliceIP test

--- tracee-ebpf/tracee/argprinters_test.go
+++ tracee-ebpf/tracee/argprinters_test.go
@@ -13,3 +13,11 @@ func TestPrintUint32IP(t *testing.T) {
 	expectedIP := "192.168.11.3"
 	assert.Equal(t, expectedIP, ip)
 }
+
+func TestPrint16BytesSliceIP(t *testing.T) {
+	input := []byte{32, 1, 13, 184, 133, 163, 0, 0, 0, 0, 138, 46, 3, 112, 115, 52}
+	ip := Print16BytesSliceIP(input)
+
+	expectedIP := "2001:db8:85a3::8a2e:370:7334"
+	assert.Equal(t, expectedIP, ip)
+}
