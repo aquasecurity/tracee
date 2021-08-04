3ebcffe3038dd68de3c52172e00c5cc20da92bde
Jose Donizetti <jdbjunior@gmail.com>
2021-08-04 13:31:03 -0300

    tracee-ebpf: add PrintAlert test

--- tracee-ebpf/tracee/argprinters_test.go
+++ tracee-ebpf/tracee/argprinters_test.go
@@ -21,3 +21,23 @@ func TestPrint16BytesSliceIP(t *testing.T) {
 	expectedIP := "2001:db8:85a3::8a2e:370:7334"
 	assert.Equal(t, expectedIP, ip)
 }
+
+func TestPrintAlert(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    alert
+		expected string
+	}{
+		{"SecurityAlert1", alert{Msg: 1}, "Mmaped region with W+E permissions!"},
+		{"SecurityAlert2", alert{Msg: 2}, "Protection changed to Executable!"},
+		{"SecurityAlert3", alert{Msg: 3}, "Protection changed from E to W+E!"},
+		{"SecurityAlert4", alert{Msg: 4}, "Protection changed from W+E to E!"},
+		{"AlertNotDefined", alert{Msg: 5}, "5"},
+		{"AlertNotDefinedWithPayload", alert{Msg: 6, Payload: 1, Ts: 1628094500}, "6 Saving data to bin.1628094500"},
+	}
+
+	for _, tc := range testCases {
+		actual := PrintAlert(tc.input)
+		assert.Equal(t, tc.expected, actual, tc.name)
+	}
+}
