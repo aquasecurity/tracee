65f121aa82bde16c960ba1e0038a311f01fa0808
Jose Donizetti <jdbjunior@gmail.com>
2021-08-05 11:01:00 -0300

    tracee-ebpf: format OutputConfig.Validate()

--- tracee-ebpf/tracee/tracee.go
+++ tracee-ebpf/tracee/tracee.go
@@ -134,9 +134,14 @@ type netProbe struct {
 
 // Validate does static validation of the configuration
 func (cfg OutputConfig) Validate() error {
-	if cfg.Format != "table" && cfg.Format != "table-verbose" && cfg.Format != "json" && cfg.Format != "gob" && !strings.HasPrefix(cfg.Format, "gotemplate=") {
+	if cfg.Format != "table" &&
+		cfg.Format != "table-verbose" &&
+		cfg.Format != "json" &&
+		cfg.Format != "gob" &&
+		!strings.HasPrefix(cfg.Format, "gotemplate=") {
 		return fmt.Errorf("unrecognized output format: %s. Valid format values: 'table', 'table-verbose', 'json', 'gob' or 'gotemplate='. Use '--output help' for more info.", cfg.Format)
 	}
+
 	return nil
 }
 
