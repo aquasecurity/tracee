282bcbd4717f15c34c81468fbb7453880c38059f
Jose Donizetti <jdbjunior@gmail.com>
2021-08-06 12:35:46 -0300

    tracee-ebpf: fix help flag to print to stdout

--- tracee-ebpf/main_test.go
+++ tracee-ebpf/main_test.go
@@ -1301,3 +1301,25 @@ func TestPrepareOutput(t *testing.T) {
 		})
 	}
 }
+
+func Test_checkCommandIsHelp(t *testing.T) {
+	testCases := []struct {
+		testName string
+		input    []string
+		expected bool
+	}{
+		{"no flag", []string{""}, false},
+		{"help flag", []string{"help"}, true},
+		{"capture flag", []string{"capture"}, false},
+		{"output flag", []string{"output"}, false},
+		{"trace flag", []string{"trace"}, false},
+		{"multiple flags", []string{"help", "capture"}, false},
+	}
+
+	for _, testcase := range testCases {
+		t.Run(testcase.testName, func(t *testing.T) {
+			actual := checkCommandIsHelp(testcase.input)
+			assert.Equal(t, testcase.expected, actual)
+		})
+	}
+}
