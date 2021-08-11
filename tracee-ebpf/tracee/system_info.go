7304ac25757b2b3e01d55066477e570e8b69a53a
Alon Zivony <alon.zivony@aquasec.com>
2021-08-11 13:41:06 +0300

    Save fetched system information into file system_info.json

--- tracee-ebpf/tracee/system_info.go
+++ tracee-ebpf/tracee/system_info.go
@@ -1,6 +1,8 @@
 package tracee
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -8,7 +10,25 @@ import (
 	"strings"
 )
 
+const SYSTEM_INFO_FILE_NAME = "system_info.json"
+
 const INIT_PROC_NS_DIR = "/proc/1/ns"
+
+const INIT_NAMESPACES_KEY = "initNamespaces"
+
+// SaveSystemInfo Writes a map that represent the system info fetched from the machine to output file in json format.
+func SaveSystemInfo(systemInfo map[string]interface{}, outDir string) error {
+	systemInfoFile, err := os.Create(filepath.Join(outDir, SYSTEM_INFO_FILE_NAME))
+	if err != nil {
+		return fmt.Errorf("couldn't create system info dump file: %v", err)
+	}
+	encoder := json.NewEncoder(systemInfoFile)
+	err = encoder.Encode(systemInfo)
+	if err != nil {
+		return fmt.Errorf("couldn't write system info to the output fil: %v", err)
+	}
+	return nil
+}
 
 // FetchSystemInfo Fetch info from the system that might be significant for later use or for signatures.
 func FetchSystemInfo() map[string]interface{} {
