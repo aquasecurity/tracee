acc92734c11d4bf19c91c6c93f88bd8b0018d50c
Alon Zivony <alon.zivony@aquasec.com>
2021-08-19 17:39:59 +0300

    Removed unused system info file save

--- tracee-ebpf/tracee/system_info.go
+++ tracee-ebpf/tracee/system_info.go
@@ -1,8 +1,6 @@
 package tracee
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/aquasecurity/tracee/tracee-ebpf/external"
 	"io/ioutil"
 	"os"
@@ -18,20 +16,6 @@ const SYSTEM_INFO_FILE_NAME = "system_info.json"
 const INIT_PROC_NS_DIR = "/proc/1/ns"
 
 const INIT_NAMESPACES_KEY = "initNamespaces"
-
-// SaveSystemInfo Writes a map that represent the system info fetched from the machine to output file in json format.
-func SaveSystemInfo(systemInfo map[string]interface{}, outDir string) error {
-	systemInfoFile, err := os.Create(filepath.Join(outDir, SYSTEM_INFO_FILE_NAME))
-	if err != nil {
-		return fmt.Errorf("couldn't create system info dump file: %v", err)
-	}
-	encoder := json.NewEncoder(systemInfoFile)
-	err = encoder.Encode(systemInfo)
-	if err != nil {
-		return fmt.Errorf("couldn't write system info to the output fil: %v", err)
-	}
-	return nil
-}
 
 // FetchSystemInfo Fetch info from the system that might be significant for later use or for signatures.
 func FetchSystemInfo() []external.Argument {
