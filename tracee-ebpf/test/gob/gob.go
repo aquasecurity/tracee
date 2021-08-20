05daef9de1b1d8d2b3e858b5e0b01d55b8c5ff07
Jose Donizetti <jdbjunior@gmail.com>
2021-08-20 17:01:23 -0300

    tracee-ebpf: fix gob test (#941)

--- tracee-ebpf/test/gob/gob.go
+++ tracee-ebpf/test/gob/gob.go
@@ -8,34 +8,9 @@ import (
 	"os"
 	"os/signal"
 	"time"
+
+	"github.com/aquasecurity/tracee/tracee-ebpf/external"
 )
-
-// Event is a user facing data structure representing a single event
-type Event struct {
-	Timestamp           float64    `json:"timestamp"`
-	ProcessID           int        `json:"processId"`
-	ThreadID            int        `json:"threadId"`
-	ParentProcessID     int        `json:"parentProcessId"`
-	HostProcessID       int        `json:"hostProcessId"`
-	HostThreadID        int        `json:"hostThreadId"`
-	HostParentProcessID int        `json:"hostParentProcessId"`
-	UserID              int        `json:"userId"`
-	MountNS             int        `json:"mountNamespace"`
-	PIDNS               int        `json:"pidNamespace"`
-	ProcessName         string     `json:"processName"`
-	HostName            string     `json:"hostName"`
-	EventID             int        `json:"eventId,string"`
-	EventName           string     `json:"eventName"`
-	ArgsNum             int        `json:"argsNum"`
-	ReturnValue         int        `json:"returnValue"`
-	Args                []Argument `json:"args"` //Arguments are ordered according their appearance in the original event
-}
-
-// Argument holds the information for one argument
-type Argument struct {
-	Name  string      `json:"name"`
-	Value interface{} `json:"value"`
-}
 
 func main() {
 	var in io.Reader
@@ -61,7 +36,7 @@ LOOP:
 		case <-sig:
 			break LOOP
 		default:
-			var event Event
+			var event external.Event
 			err = dec.Decode(&event)
 			if err != nil {
 				if err == io.EOF {
