2172120378d2a6105755f809fa0d4b2c744b8b07
Itay Shakury <itay@itaysk.com>
2021-08-26 10:03:27 +0000

    improve entrypoint
    use named pipe (more flexible e.g to exporting trace-ebpf raw data)
    
    start tracee-ebpf before tracee-rules
    
    better handle tracee-ebpf failures
    
    remove unmaintained entrypoint test

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,5 +25,26 @@ for arg in "$@"; do
 	esac
 done
 
-# start, and pass all remaining flags to tracee-rules
-$TRACEE_EBPF_EXE --output=format:gob --security-alerts | $TRACEE_RULES_EXE --input-tracee=file:stdin --input-tracee=format:gob $@
+# create named pipe for communicating between tracee-ebpf and tracee-rules
+rm -f pipe && mkfifo pipe && exec <>pipe
+
+tracee_ebpf_readiness=/tmp/tracee/out/tracee.pid
+# start tracee-ebpf in the background
+rm -f $tracee_ebpf_readiness
+echo "starting tracee-ebpf..."
+$TRACEE_EBPF_EXE --output=format:gob --security-alerts >pipe &
+tracee_ebpf_pid=$!
+
+# wait for tracee-ebpf to: load / exit / timeout
+timeout=15
+i=0
+while [ ! -f $tracee_ebpf_readiness ] && $(ls /proc/$tracee_ebpf_pid >/dev/null 2>&1) && [ $i -le $timeout ]
+do
+	i=$(( i + 1 ))
+	sleep 1
+done
+[ ! -f $tracee_ebpf_readiness ] && exit
+
+# start tracee-rules
+echo "starting tracee-rules..."
+$TRACEE_RULES_EXE --input-tracee=file:stdin --input-tracee=format:gob $@ <pipe
