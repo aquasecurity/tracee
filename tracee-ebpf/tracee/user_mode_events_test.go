e9e336c9fc0e0849ee45393a882bab9aa9a0b7d3
Alon Zivony <alon.zivony@aquasec.com>
2021-08-31 11:23:48 +0300

    Adjusted test to match current structure

--- tracee-ebpf/tracee/user_mode_events_test.go
+++ tracee-ebpf/tracee/user_mode_events_test.go
@@ -6,16 +6,19 @@ import (
 )
 
 func getProcNamespaces() []string {
-	return []string{"mnt", "cgroup", "pid", "time", "user", "ipc", "net", "uts"}
+	return []string{"mnt", "cgroup", "pid", "pid_for_children", "time", "time_for_children", "user", "ipc", "net", "uts"}
 }
 
 func TestFetchInitNamespaces(t *testing.T) {
 	initNamespacesArgs := getInitNamespaceArguments()
 	initNamespaces := make(map[string]int)
 	for _, arg := range initNamespacesArgs {
-		initNamespaces[arg.Name] = arg.Value.(int)
+		namespaceVale, ok := arg.Value.(int)
+		assert.Truef(t, ok, "Value of namespace %s is not valid: %v", arg.Name, arg.Value)
+		initNamespaces[arg.Name] = namespaceVale
 	}
 	for _, namespace := range getProcNamespaces() {
 		assert.Contains(t, initNamespaces, namespace)
+		assert.NotZero(t, initNamespaces[namespace])
 	}
 }
