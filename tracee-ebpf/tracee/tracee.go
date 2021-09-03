743fc4429458e670f3663404b6fbd0a3a4eebbd8
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2021-09-03 02:36:04 -0300

    tracee: remove UnameRelease and kernelIsAtLeast functions

--- tracee-ebpf/tracee/tracee.go
+++ tracee-ebpf/tracee/tracee.go
@@ -451,46 +451,6 @@ func New(cfg Config) (*Tracee, error) {
 	return t, nil
 }
 
-// UnameRelease gets the version string of the current running kernel
-func UnameRelease() string {
-	var uname syscall.Utsname
-	if err := syscall.Uname(&uname); err != nil {
-		return ""
-	}
-	var buf [65]byte
-	for i, b := range uname.Release {
-		buf[i] = byte(b)
-	}
-	ver := string(buf[:])
-	if i := strings.Index(ver, "\x00"); i != -1 {
-		ver = ver[:i]
-	}
-	return ver
-}
-
-func kernelIsAtLeast(verMajor, verMinor int) (bool, error) {
-	ver := UnameRelease()
-	if ver == "" {
-		return false, fmt.Errorf("could not determine current release")
-	}
-	verSplit := strings.Split(ver, ".")
-	if len(verSplit) < 2 {
-		return false, fmt.Errorf("invalid version returned by uname")
-	}
-	major, err := strconv.Atoi(verSplit[0])
-	if err != nil {
-		return false, fmt.Errorf("invalid major number: %s", verSplit[0])
-	}
-	minor, err := strconv.Atoi(verSplit[1])
-	if err != nil {
-		return false, fmt.Errorf("invalid minor number: %s", verSplit[1])
-	}
-	if ((major == verMajor) && (minor >= verMinor)) || (major > verMajor) {
-		return true, nil
-	}
-	return false, nil
-}
-
 type eventParam struct {
 	encType argType
 	encName argTag
