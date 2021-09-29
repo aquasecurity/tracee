35de4da209883fac88bcf9d33678a5e54c8fd3c0
Simar <simar@linux.com>
2021-09-29 15:29:52 -0700

    feat: Refactor various tracee launch subtests

--- tests/tracee_test.go
+++ tests/tracee_test.go
@@ -11,32 +11,46 @@ import (
 )
 
 const (
-	waitTime                   = 3
+	waitTime                   = time.Second * 3
 	traceeDockerRunBTFEnabled  = `run --detach --name tracee --rm --pid=host --privileged -v /tmp/tracee:/tmp/tracee -t aquasec/tracee:latest`
 	traceeDockerRunBTFDisabled = `run --detach --name tracee --rm --pid=host --privileged -v /tmp/tracee:/tmp/tracee -v /lib/modules/:/lib/modules/:ro -v /usr/src:/usr/src:ro -it aquasec/tracee:latest`
 )
 
+func launchTracee(t *testing.T, traceeCmd string) string {
+	t.Helper()
+
+	fmt.Println("Launching Tracee container...")
+	b, err := exec.Command("docker", strings.Split(traceeCmd, " ")...).CombinedOutput()
+	assert.NoError(t, err)
+	containerID := strings.TrimSpace(string(b))
+	fmt.Println("Tracee container ID: ", containerID)
+	return containerID
+}
+
+func runCommand(t *testing.T, cmd string, args ...string) string {
+	t.Helper()
+
+	fmt.Println("Running", cmd, args, "...")
+	output, err := exec.Command(cmd, args...).CombinedOutput()
+	assert.NoError(t, err)
+	return string(output)
+}
+
 func TestLaunchTracee(t *testing.T) {
 	t.Run("BTF enabled", func(t *testing.T) {
-		// launch tracee container
-		fmt.Println("Launching Tracee container...")
-		b, err := exec.Command("docker", strings.Split(traceeDockerRunBTFEnabled, " ")...).CombinedOutput()
-		assert.NoError(t, err)
-		containerID := strings.TrimSpace(string(b))
-		fmt.Println("Tracee container ID: ", containerID)
+		containerID := launchTracee(t, traceeDockerRunBTFEnabled)
 
 		// wait for tracee to get ready
-		time.Sleep(time.Second * waitTime)
+		time.Sleep(waitTime)
 
 		// do an `strace ls`
-		fmt.Println("Running `strace ls`...")
-		assert.NoError(t, exec.Command("strace", "ls").Run())
+		_ = runCommand(t, "strace", "ls")
 
 		// wait for tracee to detect
-		time.Sleep(time.Second * waitTime)
+		time.Sleep(waitTime)
 
 		// get tracee container logs
-		containerLogs, err := exec.Command("docker", "logs", containerID).CombinedOutput()
+		containerLogs := runCommand(t, "docker", "logs", containerID)
 
 		// assert results
 		fmt.Println("Asserting Logs...")
@@ -48,25 +62,19 @@ func TestLaunchTracee(t *testing.T) {
 	})
 
 	t.Run("BTF disabled", func(t *testing.T) {
-		// launch tracee container
-		fmt.Println("Launching Tracee container...")
-		b, err := exec.Command("docker", strings.Split(traceeDockerRunBTFDisabled, " ")...).CombinedOutput()
-		assert.NoError(t, err)
-		containerID := strings.TrimSpace(string(b))
-		fmt.Println("Tracee container ID: ", containerID)
+		containerID := launchTracee(t, traceeDockerRunBTFDisabled)
 
 		// wait for tracee to get ready
-		time.Sleep(time.Second * waitTime)
+		time.Sleep(waitTime)
 
 		// do an `strace ls`
-		fmt.Println("Running `strace ls`...")
-		assert.NoError(t, exec.Command("strace", "ls").Run())
+		_ = runCommand(t, "strace", "ls")
 
 		// wait for tracee to detect
-		time.Sleep(time.Second * waitTime)
+		time.Sleep(waitTime)
 
 		// get tracee container logs
-		containerLogs, err := exec.Command("docker", "logs", containerID).CombinedOutput()
+		containerLogs := runCommand(t, "docker", "logs", containerID)
 
 		// assert results
 		fmt.Println("Asserting Logs...")
