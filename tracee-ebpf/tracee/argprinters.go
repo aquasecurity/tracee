39dbb3c63f9019502d6226525f4fb6aac324d381
ItamarMaouda <itamarmaouda101@gmail.com>
2021-12-01 09:23:33 +0200

    fixed conflict

--- tracee-ebpf/tracee/argprinters.go
+++ tracee-ebpf/tracee/argprinters.go
@@ -34,7 +34,6 @@ func getModuleOwnerBySymbol(addr uint64) (string, error) {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 	var text []string
-
 	for scanner.Scan() {
 		text = append(text, scanner.Text())
 	}
