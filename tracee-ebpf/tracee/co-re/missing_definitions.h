e2ed525a9bc663a649b47d425c5a98562a7d7767
roikol <roi.kol@aquasec.com>
2021-12-06 16:38:06 +0200

    get file types from inode struct instead of file_operations (#1213)
    * get file types from inode struct instead of file_operations
    
    * remove essential event 'socket' for socket_dup
    
    * generic check type of fd

--- tracee-ebpf/tracee/co-re/missing_definitions.h
+++ tracee-ebpf/tracee/co-re/missing_definitions.h
@@ -162,6 +162,18 @@
 
 #define __user
 
+#define S_IFMT  00170000
+#define S_IFSOCK 0140000
+#define S_IFLNK	 0120000
+#define S_IFREG  0100000
+#define S_IFBLK  0060000
+#define S_IFDIR  0040000
+#define S_IFCHR  0020000
+#define S_IFIFO  0010000
+#define S_ISUID  0004000
+#define S_ISGID  0002000
+#define S_ISVTX  0001000
+
 static inline bool ipv6_addr_any(const struct in6_addr *a)
 {
     return (a->in6_u.u6_addr32[0] | a->in6_u.u6_addr32[1] | a->in6_u.u6_addr32[2] | a->in6_u.u6_addr32[3]) == 0;
