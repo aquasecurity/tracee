1363b5fb749a84963cf15cc98f8926bbb5725c2b
Alon Zivony <alon.zivony@aquasec.com>
2021-12-12 15:33:39 +0200

    Created global process tree with exported methods

---
+++ tracee-rules/process_tree/export.go
@@ -0,0 +1,18 @@
+package process_tree
+
+import "github.com/aquasecurity/tracee/tracee-ebpf/external"
+
+// The process tree instance to be used by the engine and the signatures
+var globalTree ProcessTree
+
+func GetProcessInfo(containerID string, threadID int) (*ProcessInfo, error) {
+	return globalTree.GetProcessInfo(containerID, threadID)
+}
+
+func GetContainerRoot(containerID string) (*ProcessInfo, error) {
+	return globalTree.GetContainerRoot(containerID)
+}
+
+func ProcessEvent(event external.Event) error {
+	return globalTree.ProcessEvent(event)
+}
