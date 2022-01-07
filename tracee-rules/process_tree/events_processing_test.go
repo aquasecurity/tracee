ab6057e75f6dae22f20b194a67c9e27f2fdda546
Alon Zivony <alon.zivony@aquasec.com>
2022-01-07 16:57:31 +0200

    Created raw events processing integration tests

--- tracee-rules/process_tree/events_processing_test.go
+++ tracee-rules/process_tree/events_processing_test.go
@@ -10,18 +10,15 @@ import (
 const TestContainerID = "a7f965fba4e145e02c99b1577febe0cb723a943d850278365994ac9b0190540e"
 
 func TestProcessTree_ProcessEvent(t *testing.T) {
-	tree := ProcessTree{
-		processes: map[int]*processNode{},
-	}
-
-	ptid := 22482
+	pid := 22482
 	ppid := 22447
+	tid := pid + 1
 	forkTimestamp := 1639044471927303690
 	execCmd := []string{"ls"}
 	execBinaryPath := "/bin/busybox"
 	execBinaryCtime := uint(1625759227634052514)
 
-	forkEvent := external.Event{
+	processForkEvent := external.Event{
 		Timestamp:           forkTimestamp,
 		ProcessID:           0,
 		ThreadID:            0,
@@ -45,9 +42,9 @@ func TestProcessTree_ProcessEvent(t *testing.T) {
 			{ArgMeta: external.ArgMeta{Name: "parent_ns_tid", Type: "int"}, Value: int32(0)},
 			{ArgMeta: external.ArgMeta{Name: "parent_pid", Type: "int"}, Value: int32(ppid)},
 			{ArgMeta: external.ArgMeta{Name: "parent_ns_pid", Type: "int"}, Value: int32(0)},
-			{ArgMeta: external.ArgMeta{Name: "child_tid", Type: "int"}, Value: int32(ptid)},
+			{ArgMeta: external.ArgMeta{Name: "child_tid", Type: "int"}, Value: int32(pid)},
 			{ArgMeta: external.ArgMeta{Name: "child_ns_tid", Type: "int"}, Value: int32(0)},
-			{ArgMeta: external.ArgMeta{Name: "child_pid", Type: "int"}, Value: int32(ptid)},
+			{ArgMeta: external.ArgMeta{Name: "child_pid", Type: "int"}, Value: int32(pid)},
 			{ArgMeta: external.ArgMeta{Name: "child_ns_pid", Type: "int"}, Value: int32(0)},
 		},
 	}
@@ -56,8 +53,8 @@ func TestProcessTree_ProcessEvent(t *testing.T) {
 		ProcessID:           0,
 		ThreadID:            0,
 		ParentProcessID:     0,
-		HostProcessID:       ptid,
-		HostThreadID:        ptid,
+		HostProcessID:       pid,
+		HostThreadID:        pid,
 		HostParentProcessID: ppid,
 		UserID:              0,
 		MountNS:             4026532548,
@@ -82,13 +79,43 @@ func TestProcessTree_ProcessEvent(t *testing.T) {
 			{ArgMeta: external.ArgMeta{Name: "sha256", Type: "const char*"}, Value: interface{}("abfd081fd7fad08d4743443061a12ebfbd25e3c5e446441795d472c389444527")},
 		},
 	}
-	exitEvent := external.Event{
-		Timestamp:           1639044471928009089,
+	threadForkEvent := external.Event{
+		Timestamp:           1639044471927556767,
 		ProcessID:           0,
 		ThreadID:            0,
 		ParentProcessID:     0,
-		HostProcessID:       ptid,
-		HostThreadID:        ptid,
+		HostProcessID:       pid,
+		HostThreadID:        pid,
+		HostParentProcessID: ppid,
+		UserID:              0,
+		MountNS:             4026532548,
+		PIDNS:               4026532551,
+		ProcessName:         "ls",
+		HostName:            "aac1fa476fcd",
+		ContainerID:         TestContainerID,
+		EventID:             1002,
+		EventName:           "sched_process_fork",
+		ArgsNum:             4,
+		ReturnValue:         0,
+		StackAddresses:      nil,
+		Args: []external.Argument{
+			{ArgMeta: external.ArgMeta{Name: "parent_tid", Type: "int"}, Value: int32(pid)},
+			{ArgMeta: external.ArgMeta{Name: "parent_ns_tid", Type: "int"}, Value: int32(0)},
+			{ArgMeta: external.ArgMeta{Name: "parent_pid", Type: "int"}, Value: int32(pid)},
+			{ArgMeta: external.ArgMeta{Name: "parent_ns_pid", Type: "int"}, Value: int32(0)},
+			{ArgMeta: external.ArgMeta{Name: "child_tid", Type: "int"}, Value: int32(tid)},
+			{ArgMeta: external.ArgMeta{Name: "child_ns_tid", Type: "int"}, Value: int32(0)},
+			{ArgMeta: external.ArgMeta{Name: "child_pid", Type: "int"}, Value: int32(pid)},
+			{ArgMeta: external.ArgMeta{Name: "child_ns_pid", Type: "int"}, Value: int32(0)},
+		},
+	}
+	threadExitEvent := external.Event{
+		Timestamp:           1639044471928003089,
+		ProcessID:           0,
+		ThreadID:            0,
+		ParentProcessID:     0,
+		HostProcessID:       pid,
+		HostThreadID:        tid,
 		HostParentProcessID: ppid,
 		UserID:              0,
 		MountNS:             4026532548,
@@ -105,22 +132,347 @@ func TestProcessTree_ProcessEvent(t *testing.T) {
 			{ArgMeta: external.ArgMeta{Name: "exit_code", Type: "long"}, Value: 0},
 		},
 	}
-
-	err := tree.ProcessEvent(forkEvent)
-	require.NoError(t, err)
-	err = tree.ProcessEvent(execEvent)
-	require.NoError(t, err)
-	process, err := tree.GetProcessInfo(ptid)
-	assert.NoError(t, err)
-	assert.Equal(t, ptid, process.InHostIDs.Pid)
-	assert.Equal(t, ppid, process.InHostIDs.Ppid)
-	assert.Equal(t, forkTimestamp, process.StartTime)
-	assert.Equal(t, execCmd, process.Cmd)
-	assert.Equal(t, execBinaryPath, process.ExecutionBinary.Path)
-	assert.Equal(t, execBinaryCtime, process.ExecutionBinary.Ctime)
-	err = tree.processExitEvent(exitEvent)
-	tree.emptyDeadProcessesCache()
-	require.NoError(t, err)
-	_, err = tree.GetProcessInfo(ptid)
-	assert.Error(t, err)
+	processExitEvent := external.Event{
+		Timestamp:           1639044471928009089,
+		ProcessID:           0,
+		ThreadID:            0,
+		ParentProcessID:     0,
+		HostProcessID:       pid,
+		HostThreadID:        pid,
+		HostParentProcessID: ppid,
+		UserID:              0,
+		MountNS:             4026532548,
+		PIDNS:               4026532551,
+		ProcessName:         "ls",
+		HostName:            "aac1fa476fcd",
+		ContainerID:         TestContainerID,
+		EventID:             1004,
+		EventName:           "sched_process_exit",
+		ArgsNum:             1,
+		ReturnValue:         0,
+		StackAddresses:      nil,
+		Args: []external.Argument{
+			{ArgMeta: external.ArgMeta{Name: "exit_code", Type: "long"}, Value: 0},
+		},
+	}
+	t.Run("Ordered flows", func(t *testing.T) {
+		t.Run("Ordered normal flow", func(t *testing.T) {
+			tree := ProcessTree{
+				processes: map[int]*processNode{},
+			}
+			var err error
+			err = tree.ProcessEvent(processForkEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(execEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(threadForkEvent)
+			require.NoError(t, err)
+			process, err := tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Equal(t, pid, process.InHostIDs.Pid)
+			assert.Equal(t, ppid, process.InHostIDs.Ppid)
+			assert.Equal(t, forkTimestamp, process.StartTime)
+			assert.Equal(t, execCmd, process.Cmd)
+			assert.Equal(t, execBinaryPath, process.ExecutionBinary.Path)
+			assert.Equal(t, execBinaryCtime, process.ExecutionBinary.Ctime)
+			assert.Contains(t, process.ExistingThreads, pid)
+			assert.Contains(t, process.ExistingThreads, tid)
+			err = tree.processExitEvent(threadExitEvent)
+			require.NoError(t, err)
+			err = tree.processExitEvent(processExitEvent)
+			require.NoError(t, err)
+			tree.emptyDeadProcessesCache()
+			_, err = tree.GetProcessInfo(pid)
+			assert.Error(t, err)
+		})
+		t.Run("Ordered main thread exit first", func(t *testing.T) {
+			tree := ProcessTree{
+				processes: map[int]*processNode{},
+			}
+			var err error
+			err = tree.ProcessEvent(processForkEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(execEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(threadForkEvent)
+			require.NoError(t, err)
+			process, err := tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Equal(t, pid, process.InHostIDs.Pid)
+			assert.Equal(t, ppid, process.InHostIDs.Ppid)
+			assert.Equal(t, forkTimestamp, process.StartTime)
+			assert.Equal(t, execCmd, process.Cmd)
+			assert.Equal(t, execBinaryPath, process.ExecutionBinary.Path)
+			assert.Equal(t, execBinaryCtime, process.ExecutionBinary.Ctime)
+			assert.Contains(t, process.ExistingThreads, pid)
+			assert.Contains(t, process.ExistingThreads, tid)
+			err = tree.processExitEvent(processExitEvent)
+			require.NoError(t, err)
+			err = tree.processExitEvent(threadExitEvent)
+			require.NoError(t, err)
+			tree.emptyDeadProcessesCache()
+			_, err = tree.GetProcessInfo(pid)
+			assert.Error(t, err)
+		})
+	})
+	t.Run("Unordered events flows", func(t *testing.T) {
+		t.Run("Unordered exec before main fork", func(t *testing.T) {
+			tree := ProcessTree{
+				processes: map[int]*processNode{},
+			}
+			var err error
+			err = tree.ProcessEvent(execEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(processForkEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(threadForkEvent)
+			require.NoError(t, err)
+			process, err := tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Equal(t, pid, process.InHostIDs.Pid)
+			assert.Equal(t, ppid, process.InHostIDs.Ppid)
+			assert.Equal(t, forkTimestamp, process.StartTime)
+			assert.Equal(t, execCmd, process.Cmd)
+			assert.Equal(t, execBinaryPath, process.ExecutionBinary.Path)
+			assert.Equal(t, execBinaryCtime, process.ExecutionBinary.Ctime)
+			assert.Contains(t, process.ExistingThreads, pid)
+			assert.Contains(t, process.ExistingThreads, tid)
+			err = tree.processExitEvent(processExitEvent)
+			require.NoError(t, err)
+			err = tree.processExitEvent(threadExitEvent)
+			require.NoError(t, err)
+			tree.emptyDeadProcessesCache()
+			_, err = tree.GetProcessInfo(pid)
+			assert.Error(t, err)
+		})
+		t.Run("Unordered fork thread before main fork", func(t *testing.T) {
+			tree := ProcessTree{
+				processes: map[int]*processNode{},
+			}
+			var err error
+			err = tree.ProcessEvent(threadForkEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(processForkEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(execEvent)
+			require.NoError(t, err)
+			process, err := tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Equal(t, pid, process.InHostIDs.Pid)
+			assert.Equal(t, ppid, process.InHostIDs.Ppid)
+			assert.Equal(t, forkTimestamp, process.StartTime)
+			assert.Equal(t, execCmd, process.Cmd)
+			assert.Equal(t, execBinaryPath, process.ExecutionBinary.Path)
+			assert.Equal(t, execBinaryCtime, process.ExecutionBinary.Ctime)
+			assert.Contains(t, process.ExistingThreads, pid)
+			assert.Contains(t, process.ExistingThreads, tid)
+			err = tree.processExitEvent(processExitEvent)
+			require.NoError(t, err)
+			err = tree.processExitEvent(threadExitEvent)
+			require.NoError(t, err)
+			tree.emptyDeadProcessesCache()
+			_, err = tree.GetProcessInfo(pid)
+			assert.Error(t, err)
+		})
+		t.Run("Unordered exit thread before thread fork", func(t *testing.T) {
+			tree := ProcessTree{
+				processes: map[int]*processNode{},
+			}
+			var err error
+			err = tree.ProcessEvent(processForkEvent)
+			require.NoError(t, err)
+			err = tree.processExitEvent(threadExitEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(execEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(threadForkEvent)
+			require.NoError(t, err)
+			process, err := tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Equal(t, pid, process.InHostIDs.Pid)
+			assert.Equal(t, ppid, process.InHostIDs.Ppid)
+			assert.Equal(t, forkTimestamp, process.StartTime)
+			assert.Equal(t, execCmd, process.Cmd)
+			assert.Equal(t, execBinaryPath, process.ExecutionBinary.Path)
+			assert.Equal(t, execBinaryCtime, process.ExecutionBinary.Ctime)
+			assert.Contains(t, process.ExistingThreads, pid)
+			assert.Contains(t, process.ExistingThreads, tid)
+			err = tree.processExitEvent(processExitEvent)
+			require.NoError(t, err)
+			tree.emptyDeadProcessesCache()
+			process, err = tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Contains(t, process.ExistingThreads, tid)
+			assert.NoError(t, err)
+		})
+		t.Run("Unordered exit main thread before process fork", func(t *testing.T) {
+			tree := ProcessTree{
+				processes: map[int]*processNode{},
+			}
+			var err error
+			err = tree.processExitEvent(processExitEvent)
+			require.Error(t, err)
+			err = tree.ProcessEvent(processForkEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(execEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(threadForkEvent)
+			require.NoError(t, err)
+			process, err := tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Equal(t, pid, process.InHostIDs.Pid)
+			assert.Equal(t, ppid, process.InHostIDs.Ppid)
+			assert.Equal(t, forkTimestamp, process.StartTime)
+			assert.Equal(t, execCmd, process.Cmd)
+			assert.Equal(t, execBinaryPath, process.ExecutionBinary.Path)
+			assert.Equal(t, execBinaryCtime, process.ExecutionBinary.Ctime)
+			assert.Contains(t, process.ExistingThreads, pid)
+			assert.Contains(t, process.ExistingThreads, tid)
+			err = tree.processExitEvent(threadExitEvent)
+			require.NoError(t, err)
+			tree.emptyDeadProcessesCache()
+			process, err = tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Contains(t, process.ExistingThreads, pid)
+			assert.NoError(t, err)
+		})
+	})
+	t.Run("Missing event flow", func(t *testing.T) {
+		t.Run("Missing main fork event", func(t *testing.T) {
+			tree := ProcessTree{
+				processes: map[int]*processNode{},
+			}
+			var err error
+			err = tree.ProcessEvent(execEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(threadForkEvent)
+			require.NoError(t, err)
+			process, err := tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Equal(t, pid, process.InHostIDs.Pid)
+			assert.Equal(t, ppid, process.InHostIDs.Ppid)
+			assert.Equal(t, 0, process.StartTime)
+			assert.Equal(t, execCmd, process.Cmd)
+			assert.Equal(t, execBinaryPath, process.ExecutionBinary.Path)
+			assert.Equal(t, execBinaryCtime, process.ExecutionBinary.Ctime)
+			assert.Contains(t, process.ExistingThreads, pid)
+			assert.Contains(t, process.ExistingThreads, tid)
+			err = tree.processExitEvent(threadExitEvent)
+			require.NoError(t, err)
+			err = tree.processExitEvent(processExitEvent)
+			require.NoError(t, err)
+			tree.emptyDeadProcessesCache()
+			_, err = tree.GetProcessInfo(pid)
+			assert.Error(t, err)
+		})
+		t.Run("Missing exec event", func(t *testing.T) {
+			tree := ProcessTree{
+				processes: map[int]*processNode{},
+			}
+			var err error
+			err = tree.ProcessEvent(processForkEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(threadForkEvent)
+			require.NoError(t, err)
+			process, err := tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Equal(t, pid, process.InHostIDs.Pid)
+			assert.Equal(t, ppid, process.InHostIDs.Ppid)
+			assert.Equal(t, forkTimestamp, process.StartTime)
+			assert.Equal(t, []string(nil), process.Cmd)
+			assert.Equal(t, "", process.ExecutionBinary.Path)
+			assert.Equal(t, uint(0), process.ExecutionBinary.Ctime)
+			assert.Contains(t, process.ExistingThreads, pid)
+			assert.Contains(t, process.ExistingThreads, tid)
+			err = tree.processExitEvent(threadExitEvent)
+			require.NoError(t, err)
+			err = tree.processExitEvent(processExitEvent)
+			require.NoError(t, err)
+			tree.emptyDeadProcessesCache()
+			_, err = tree.GetProcessInfo(pid)
+			assert.Error(t, err)
+		})
+		t.Run("Missing thread fork event", func(t *testing.T) {
+			tree := ProcessTree{
+				processes: map[int]*processNode{},
+			}
+			var err error
+			err = tree.ProcessEvent(processForkEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(execEvent)
+			require.NoError(t, err)
+			process, err := tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Equal(t, pid, process.InHostIDs.Pid)
+			assert.Equal(t, ppid, process.InHostIDs.Ppid)
+			assert.Equal(t, forkTimestamp, process.StartTime)
+			assert.Equal(t, execCmd, process.Cmd)
+			assert.Equal(t, execBinaryPath, process.ExecutionBinary.Path)
+			assert.Equal(t, execBinaryCtime, process.ExecutionBinary.Ctime)
+			assert.Contains(t, process.ExistingThreads, pid)
+			err = tree.processExitEvent(threadExitEvent)
+			require.NoError(t, err)
+			err = tree.processExitEvent(processExitEvent)
+			require.NoError(t, err)
+			tree.emptyDeadProcessesCache()
+			_, err = tree.GetProcessInfo(pid)
+			assert.Error(t, err)
+		})
+		t.Run("Missing thread exit", func(t *testing.T) {
+			tree := ProcessTree{
+				processes: map[int]*processNode{},
+			}
+			var err error
+			err = tree.ProcessEvent(processForkEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(execEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(threadForkEvent)
+			require.NoError(t, err)
+			process, err := tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Equal(t, pid, process.InHostIDs.Pid)
+			assert.Equal(t, ppid, process.InHostIDs.Ppid)
+			assert.Equal(t, forkTimestamp, process.StartTime)
+			assert.Equal(t, execCmd, process.Cmd)
+			assert.Equal(t, execBinaryPath, process.ExecutionBinary.Path)
+			assert.Equal(t, execBinaryCtime, process.ExecutionBinary.Ctime)
+			assert.Contains(t, process.ExistingThreads, pid)
+			assert.Contains(t, process.ExistingThreads, tid)
+			err = tree.processExitEvent(processExitEvent)
+			require.NoError(t, err)
+			tree.emptyDeadProcessesCache()
+			process, err = tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Contains(t, process.ExistingThreads, tid)
+		})
+		t.Run("Missing main thread exit", func(t *testing.T) {
+			tree := ProcessTree{
+				processes: map[int]*processNode{},
+			}
+			var err error
+			err = tree.ProcessEvent(processForkEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(execEvent)
+			require.NoError(t, err)
+			err = tree.ProcessEvent(threadForkEvent)
+			require.NoError(t, err)
+			process, err := tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Equal(t, pid, process.InHostIDs.Pid)
+			assert.Equal(t, ppid, process.InHostIDs.Ppid)
+			assert.Equal(t, forkTimestamp, process.StartTime)
+			assert.Equal(t, execCmd, process.Cmd)
+			assert.Equal(t, execBinaryPath, process.ExecutionBinary.Path)
+			assert.Equal(t, execBinaryCtime, process.ExecutionBinary.Ctime)
+			assert.Contains(t, process.ExistingThreads, pid)
+			assert.Contains(t, process.ExistingThreads, tid)
+			err = tree.processExitEvent(threadExitEvent)
+			require.NoError(t, err)
+			tree.emptyDeadProcessesCache()
+			process, err = tree.GetProcessInfo(pid)
+			assert.NoError(t, err)
+			assert.Contains(t, process.ExistingThreads, pid)
+		})
+	})
 }
