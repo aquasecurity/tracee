aquasecurity/tracee

commit e252c46347b7a5ed288eded81d9ae6090f11bc85

Process context map  (#1300)

tracee-ebpf/tracee/tracee.bpf.c
@@ -389,6 +389,20 @@ typedef struct event_context {
     u8 argnum;
 } context_t;
 
+typedef struct process_context {
+    u64 ts;                        // Timestamp
+    u64 cgroup_id;
+    u32 pid;                       // PID as in the userspace term
+    u32 tid;                       // TID as in the userspace term
+    u32 ppid;                      // Parent PID as in the userspace term
+    u32 host_pid;                  // PID in host pid namespace
+    u32 host_tid;                  // TID in host pid namespace
+    u32 host_ppid;                 // Parent PID in host pid namespace
+    u32 uid;
+    u32 mnt_id;
+    u32 pid_id;
+} process_context_t;
+
 typedef struct args {
     unsigned long args[6];
 } args_t;
@@ -547,6 +561,7 @@ BPF_HASH(bin_args_map, u64, bin_args_t);                // persist args for send
 BPF_HASH(sys_32_to_64_map, u32, u32);                   // map 32bit to 64bit syscalls
 BPF_HASH(params_types_map, u32, u64);                   // encoded parameters types for event
 BPF_HASH(process_tree_map, u32, u32);                   // filter events by the ancestry of the traced process
+BPF_HASH(process_context_map, u32, process_context_t);  // holds the process_context data for every tid
 BPF_LRU_HASH(sock_ctx_map, u64, net_ctx_ext_t);         // socket address to process context
 BPF_LRU_HASH(network_map, local_net_id_t, net_ctx_t);   // network identifier to process context
 BPF_ARRAY(file_filter, path_filter_t, 3);               // filter vfs_write events
@@ -2349,6 +2364,10 @@ int tracepoint__sched__sched_process_fork(struct bpf_raw_tracepoint_args *ctx)
     struct task_struct *parent = (struct task_struct*)ctx->args[0];
     struct task_struct *child = (struct task_struct*)ctx->args[1];
 
+    process_context_t *process = (process_context_t*) &data.context;
+    process->tid = get_task_ns_pid(child);
+    process->host_tid = get_task_host_pid(child);
+    bpf_map_update_elem(&process_context_map, &process->host_tid, &process, BPF_ANY);
     int parent_pid = get_task_host_pid(parent);
     int child_pid = get_task_host_pid(child);
 
@@ -2404,6 +2423,8 @@ int tracepoint__sched__sched_process_exec(struct bpf_raw_tracepoint_args *ctx)
     if (!init_event_data(&data, ctx))
         return 0;
 
+    process_context_t *process = (process_context_t*) &data.context;
+
     // Perform the following checks before should_trace() so we can filter by
     // newly created containers/processes.  We assume that a new container/pod
     // has started when a process of a newly created cgroup and mount ns
@@ -2426,6 +2447,7 @@ int tracepoint__sched__sched_process_exec(struct bpf_raw_tracepoint_args *ctx)
 
     // We passed all filters (in should_trace()) - add this pid to traced pids set
     bpf_map_update_elem(&traced_pids_map, &data.context.host_tid, &data.context.host_tid, BPF_ANY);
+    bpf_map_update_elem(&process_context_map, &data.context.host_tid, process, BPF_ANY);
 
     struct task_struct *task = (struct task_struct *)ctx->args[0];
     struct linux_binprm *bprm = (struct linux_binprm *)ctx->args[2];
@@ -2503,6 +2525,7 @@ int tracepoint__sched__sched_process_exit(struct bpf_raw_tracepoint_args *ctx)
     bpf_map_delete_elem(&traced_pids_map, &data.context.host_tid);
     bpf_map_delete_elem(&new_pids_map, &data.context.host_tid);
     bpf_map_delete_elem(&syscall_data_map, &data.context.host_tid);
+    bpf_map_delete_elem(&process_context_map, &data.context.host_tid);
 
     int proc_tree_filter_set = get_config(CONFIG_PROC_TREE_FILTER);
 

pkg/containers/containers.go
@@ -90,6 +90,25 @@ func (c *Containers) procMountsCgroups() error {
 	return nil
 }
 
+// gets a containerID from a given task or process directory path
+func GetContainerIdFromTaskDir(taskPath string) (string, error) {
+	containerId := ""
+	taskPath = fmt.Sprintf("%s/cgroup", taskPath)
+	cgroupFile, err := os.Open(taskPath)
+	if err != nil {
+		return containerId, err
+	}
+	defer cgroupFile.Close()
+	scanner := bufio.NewScanner(cgroupFile)
+	for scanner.Scan() {
+		containerId, _ := getContainerIdFromCgroup(scanner.Text())
+		if containerId != "" {
+			break
+		}
+	}
+	return containerId, nil
+}
+
 // populate walks the cgroup fs informing CgroupUpdate() about existing dirs.
 func (c *Containers) populate() error {
 	if c.cgroupMP == "" {

tracee-ebpf/tracee/events_processor.go
@@ -123,13 +123,15 @@ func (t *Tracee) processEvent(event *external.Event) error {
 		}
 
 	case SchedProcessExecEventID:
+		//update the process tree
+		processData := ProcessCtx{event.Timestamp, event.ContainerID, uint32(event.ProcessID), uint32(event.ThreadID), uint32(event.ParentProcessID), uint32(event.HostProcessID), uint32(event.HostThreadID), uint32(event.HostParentProcessID), uint32(event.UserID), uint32(event.MountNS), uint32(event.PIDNS)}
+		t.processTree.processTreeMap[event.HostThreadID] = processData
 		//cache this pid by it's mnt ns
 		if event.ProcessID == 1 {
 			t.pidsInMntns.ForceAddBucketItem(uint32(event.MountNS), uint32(event.HostProcessID))
 		} else {
 			t.pidsInMntns.AddBucketItem(uint32(event.MountNS), uint32(event.HostProcessID))
 		}
-
 		//capture executed files
 		if t.config.Capture.Exec || t.config.Output.ExecHash {
 			filePath, err := getEventArgStringVal(event, "pathname")
@@ -213,7 +215,17 @@ func (t *Tracee) processEvent(event *external.Event) error {
 			}
 			return err
 		}
-
+	case SchedProcessExitEventID:
+		delete(t.processTree.processTreeMap, event.HostProcessID)
+	case SchedProcessForkEventID:
+		hostTid, _ := getEventArgUint32Val(event, "child_tid")
+		childPid, _ := getEventArgUint32Val(event, "child_pid")
+		pid, _ := getEventArgUint32Val(event, "child_ns_pid")
+		ppid, _ := getEventArgUint32Val(event, "parent_ns_pid")
+		hostPpid, _ := getEventArgUint32Val(event, "parent_pid")
+		childTid, _ := getEventArgUint32Val(event, "child_ns_tid")
+		processData := ProcessCtx{event.Timestamp, event.ContainerID, pid, childPid, ppid, hostTid, hostTid, hostPpid, uint32(event.UserID), uint32(event.MountNS), uint32(event.ProcessID)}
+		t.processTree.processTreeMap[int(childTid)] = processData
 	case CgroupMkdirEventID:
 		cgroupId, err := getEventArgUint64Val(event, "cgroup_id")
 		if err != nil {

tracee-ebpf/tracee/net_events.go
@@ -63,17 +63,43 @@ func (t *Tracee) processNetEvents() {
 						t.handleError(err)
 						continue
 					}
+					networkProcess, err := t.getProcessCtx(int(hostTid))
+					hr, min, sec := timeStampObj.Clock()
+					nsec := uint16(timeStampObj.Nanosecond())
 
-					fmt.Printf("%v  %-16s  %-7d  debug_net/packet               Len: %d, SrcIP: %v, SrcPort: %d, DestIP: %v, DestPort: %d, Protocol: %d\n",
-						timeStampObj,
-						comm,
-						hostTid,
-						pktLen,
-						netaddr.IPFrom16(pktMeta.SrcIP),
-						pktMeta.SrcPort,
-						netaddr.IPFrom16(pktMeta.DestIP),
-						pktMeta.DestPort,
-						pktMeta.Protocol)
+					if err != nil {
+						fmt.Printf("%v:%v:%v:%v  %-16s  %-7d  debug_net/packet               Len: %d, SrcIP: %v, SrcPort: %d, DestIP: %v, DestPort: %d, Protocol: %d\n",
+							hr,
+							min,
+							sec,
+							nsec,
+							comm,
+							hostTid,
+							pktLen,
+							netaddr.IPFrom16(pktMeta.SrcIP),
+							pktMeta.SrcPort,
+							netaddr.IPFrom16(pktMeta.DestIP),
+							pktMeta.DestPort,
+							pktMeta.Protocol)
+					} else {
+						fmt.Printf("%v:%v:%v:%v   %v    %-16s  %v  %v    %d             debug_net/packet              Len: %d, SrcIP: %v, SrcPort: %d, DestIP: %v, DestPort: %d, Protocol: %d\n",
+							hr,
+							min,
+							sec,
+							nsec,
+							networkProcess.Uid,
+							comm,
+							networkProcess.Pid,
+							networkProcess.Tid,
+							0,
+							pktLen,
+							netaddr.IPFrom16(pktMeta.SrcIP),
+							pktMeta.SrcPort,
+							netaddr.IPFrom16(pktMeta.DestIP),
+							pktMeta.DestPort,
+							pktMeta.Protocol)
+					}
+
 				}
 
 				info := gopacket.CaptureInfo{
@@ -148,6 +174,7 @@ func (t *Tracee) processNetEvents() {
 						timeStampObj, comm, hostTid, netaddr.IPFrom16(pkt.LocalIP), pkt.LocalPort, pkt.Protocol)
 				}
 			}
+
 		case lost := <-t.lostNetChannel:
 			t.stats.lostNtCounter.Increment(int(lost))
 		}

tracee-ebpf/tracee/process_tree.go
@@ -1,0 +1,178 @@
+package tracee
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/aquasecurity/tracee/pkg/containers"
+)
+
+type ProcessCtx struct {
+	StartTime   int
+	ContainerID string
+	Pid         uint32
+	Tid         uint32
+	Ppid        uint32
+	HostTid     uint32
+	HostPid     uint32
+	HostPpid    uint32
+	Uid         uint32
+	MntId       uint32
+	PidId       uint32
+}
+
+type ProcessTree struct {
+	processTreeMap map[int]ProcessCtx
+}
+
+func (t *Tracee) ParseProcessContext(ctx []byte) (ProcessCtx, error) {
+	var procCtx = ProcessCtx{}
+	procCtx.StartTime = int(binary.LittleEndian.Uint64(ctx[0:8]))
+	cgroupId := binary.LittleEndian.Uint64(ctx[8:16])
+	procCtx.ContainerID = t.containers.GetCgroupInfo(cgroupId).ContainerId
+	dataBuff := bytes.NewBuffer(ctx[16:]) // this is the offset after the cgroup and startTime in the ctx byte array
+	var errs []error
+	errs = append(errs, binary.Read(dataBuff, binary.LittleEndian, &procCtx.Pid))
+	errs = append(errs, binary.Read(dataBuff, binary.LittleEndian, &procCtx.Tid))
+	errs = append(errs, binary.Read(dataBuff, binary.LittleEndian, &procCtx.Ppid))
+	errs = append(errs, binary.Read(dataBuff, binary.LittleEndian, &procCtx.HostTid))
+	errs = append(errs, binary.Read(dataBuff, binary.LittleEndian, &procCtx.HostPid))
+	errs = append(errs, binary.Read(dataBuff, binary.LittleEndian, &procCtx.HostPpid))
+	errs = append(errs, binary.Read(dataBuff, binary.LittleEndian, &procCtx.Uid))
+	errs = append(errs, binary.Read(dataBuff, binary.LittleEndian, &procCtx.MntId))
+	errs = append(errs, binary.Read(dataBuff, binary.LittleEndian, &procCtx.PidId))
+	for _, e := range errs {
+		if e != nil {
+			return procCtx, e
+		}
+	}
+	return procCtx, nil
+}
+
+//returns the file creation time
+//Note: this is not the real process start time because it's based on the file creation time, but it is a close approximation
+func getFileCtime(path string) (int, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return 0, err
+	}
+	stat_t := fileInfo.Sys().(*syscall.Stat_t)
+	ts := stat_t.Ctim
+	return int(ts.Sec)*int(time.Second) + int(ts.Nsec)*int(time.Nanosecond), nil
+}
+
+//parse the status file of a process or given task and returns process context struct
+func parseProcStatus(status []byte, taskStatusPath string) (ProcessCtx, error) {
+	var process ProcessCtx
+	processVals := make(map[string]uint32)
+	for _, line := range strings.Split(string(status), "\n") {
+		lineFields := strings.Split(line, ":")
+		if len(lineFields) > 1 {
+			valFields := strings.Fields(lineFields[1])
+			if len(valFields) > 0 {
+				val, err := strconv.ParseUint(strings.TrimSpace(valFields[0]), 10, 32)
+				if err != nil {
+					continue
+				}
+				processVals[lineFields[0]] = uint32(val)
+			}
+
+		}
+	}
+
+	var err error
+	process.StartTime, err = getFileCtime(taskStatusPath)
+	if err != nil {
+		return process, err
+	}
+	process.HostPid = processVals["Tgid"]
+	process.HostTid = processVals["Pid"]
+	process.HostPpid = processVals["PPid"]
+	process.Uid = processVals["Uid"]
+	process.Pid = processVals["NStgid"]
+	process.Tid = processVals["NSpid"]
+	process.Ppid = processVals["NSpgid"]
+	process.MntId, process.PidId, err = getNsIdData(taskStatusPath)
+	if err != nil {
+		return process, err
+	}
+	return process, nil
+}
+
+//gets the namespace data for the process context struct by parsing the /proc/<Pid>/task directory
+func getNsIdData(taskStatusPath string) (uint32, uint32, error) {
+	path := fmt.Sprintf("%s/ns/mnt", taskStatusPath[:len(taskStatusPath)-7])
+	processMntId, err := os.Readlink(path)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	processMntId = strings.TrimSuffix(processMntId, "]")
+	processMntId = strings.TrimPrefix(processMntId, "mnt:[")
+	mntId, err := strconv.ParseUint(processMntId, 10, 32)
+	if err != nil {
+		return 0, 0, err
+	}
+	path = fmt.Sprintf("%s/ns/pid", taskStatusPath[:len(taskStatusPath)-7])
+	processPidId, err := os.Readlink(path)
+	if err != nil {
+		return 0, 0, err
+	}
+	processPidId = strings.TrimSuffix(processPidId, "]")
+	processPidId = strings.TrimPrefix(processPidId, "pid:[")
+	pidId, err := strconv.ParseUint(processPidId, 10, 32)
+	if err != nil {
+		return 0, 0, err
+	}
+	return uint32(mntId), uint32(pidId), nil
+}
+
+//initialize new process-tree
+func NewProcessTree() (*ProcessTree, error) {
+	p := ProcessTree{make(map[int]ProcessCtx)}
+	procDir, err := os.Open("/proc")
+	if err != nil {
+		return nil, err
+	}
+	defer procDir.Close()
+	entries, err := procDir.Readdirnames(-1)
+	if err != nil {
+		return nil, err
+	}
+
+	// Iterate over each pid
+	for _, procEntry := range entries {
+		pid, err := strconv.ParseUint(procEntry, 10, 32)
+		if err != nil {
+			continue
+		}
+		taskDir, err := os.Open(fmt.Sprintf("/proc/%d/task", pid))
+		processTasks, err := taskDir.Readdirnames(-1)
+		for _, task := range processTasks {
+			taskDir := fmt.Sprintf("/proc/%d/task/%v", pid, task)
+			taskStatus := fmt.Sprintf("/proc/%d/task/%v/status", pid, task)
+			data, err := ioutil.ReadFile(taskStatus)
+			if err != nil {
+				continue
+			}
+			processStatus, err := parseProcStatus(data, taskStatus)
+			if err != nil {
+				return nil, err
+			}
+			containerId, err := containers.GetContainerIdFromTaskDir(taskDir)
+			if err != nil {
+				return nil, err
+			}
+			processStatus.ContainerID = containerId
+			p.processTreeMap[int(processStatus.HostTid)] = processStatus
+		}
+	}
+	return &p, nil
+}

tracee-ebpf/tracee/tracee.go
@@ -179,6 +179,7 @@ type Tracee struct {
 	pcapFile          *os.File
 	ngIfacesIndex     map[int]int
 	containers        *containers.Containers
+	processTree       *ProcessTree
 }
 
 type counter int32
@@ -390,7 +391,11 @@ func New(cfg Config) (*Tracee, error) {
 		return nil, fmt.Errorf("error getting acces to 'stack_addresses' eBPF Map %v", err)
 	}
 	t.StackAddressesMap = StackAddressesMap
-
+	t.processTree, err = NewProcessTree()
+	if err != nil {
+		t.Close()
+		return nil, fmt.Errorf("error creating process tree: %v", err)
+	}
 	return t, nil
 }
 
@@ -911,6 +916,25 @@ func (t *Tracee) writeProfilerStats(wr io.Writer) error {
 		return err
 	}
 	return nil
+}
+
+func (t *Tracee) getProcessCtx(hostTid int) (ProcessCtx, error) {
+	processCtx, procExist := t.processTree.processTreeMap[hostTid]
+	if procExist {
+		return processCtx, nil
+	} else {
+		processContextMap, err := t.bpfModule.GetMap("process_context_map")
+		if err != nil {
+			return processCtx, err
+		}
+		processCtxBpfMap, err := processContextMap.GetValue(unsafe.Pointer(&hostTid))
+		if err != nil {
+			return processCtx, err
+		}
+		processCtx, err = t.ParseProcessContext(processCtxBpfMap)
+		t.processTree.processTreeMap[hostTid] = processCtx
+		return processCtx, err
+	}
 }
 
 // Run starts the trace. it will run until interrupted

commit 178021ddbdc4c361c3134c1ab47a3935de53811d

tracee-ebpf: fix eBPF load regression for v5.4 kernel
Commit e252c46 introduced a minor regression in non CO-RE eBPF object
loading in kernels v5.4. The kernel verifier complains:

  ; bpf_map_update_elem(&process_context_map, &process->host_tid, &process, BPF_ANY);
  394: (18) r1 = 0xffffa0b8aa8f8400
  396: (b7) r4 = 0
  397: (85) call bpf_map_update_elem#2
  invalid indirect read from stack off -240+0 size 56
  processed 358 insns (limit 1000000) max_states_per_insn 0 total_states
  21 peak_states 21 mark_read 21

Copying (struct process_context_t *) &data.context to function's stack
seems to solve the issue.

Fixes: e252c46 ("Process context map  (#1300)")

tracee-ebpf/tracee/tracee.bpf.c
@@ -2364,10 +2364,13 @@ int tracepoint__sched__sched_process_fork(struct bpf_raw_tracepoint_args *ctx)
     struct task_struct *parent = (struct task_struct*)ctx->args[0];
     struct task_struct *child = (struct task_struct*)ctx->args[1];
 
-    process_context_t *process = (process_context_t*) &data.context;
-    process->tid = get_task_ns_pid(child);
-    process->host_tid = get_task_host_pid(child);
-    bpf_map_update_elem(&process_context_map, &process->host_tid, &process, BPF_ANY);
+    // note: v5.4 verifier does not like using (process_context_t *) from &data->context
+    process_context_t process = {};
+    __builtin_memcpy(&process, &data.context, sizeof(process_context_t));
+    process.tid = get_task_ns_pid(child);
+    process.host_tid = get_task_host_pid(child);
+    bpf_map_update_elem(&process_context_map, &process.host_tid, &process, BPF_ANY);
+
     int parent_pid = get_task_host_pid(parent);
     int child_pid = get_task_host_pid(child);
 