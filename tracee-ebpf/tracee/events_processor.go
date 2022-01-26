59f760998720389e572b4bb0b7a1776b5aa209dd
AsafEitani <54033369+AsafEitani@users.noreply.github.com>
2022-01-26 12:15:40 +0200

    add stats printing to stderr in case of lost events (#1387)

--- tracee-ebpf/tracee/events_processor.go
+++ tracee-ebpf/tracee/events_processor.go
@@ -15,7 +15,13 @@ import (
 func (t *Tracee) processLostEvents() {
 	for {
 		lost := <-t.lostEvChannel
-		t.stats.lostEvCounter.Increment(int(lost))
+		// When terminating tracee-ebpf the lost channel receives multiple "0 lost events" events.
+		// This check prevents those 0 lost events messages to be written to stderr until the bug is fixed:
+		// https://github.com/aquasecurity/libbpfgo/issues/122
+		if lost > 0 {
+			t.stats.lostEvCounter.Increment(int(lost))
+			t.config.ChanErrors <- fmt.Errorf("lost %d events", lost)
+		}
 	}
 }
 
--- tracee-ebpf/tracee/net_events.go
+++ tracee-ebpf/tracee/net_events.go
@@ -176,7 +176,13 @@ func (t *Tracee) processNetEvents() {
 			}
 
 		case lost := <-t.lostNetChannel:
-			t.stats.lostNtCounter.Increment(int(lost))
+			// When terminating tracee-ebpf the lost channel receives multiple "0 lost events" events.
+			// This check prevents those 0 lost events messages to be written to stderr until the bug is fixed:
+			// https://github.com/aquasecurity/libbpfgo/issues/122
+			if lost > 0 {
+				t.stats.lostNtCounter.Increment(int(lost))
+				t.config.ChanErrors <- fmt.Errorf("lost %d network events", lost)
+			}
 		}
 	}
 }
