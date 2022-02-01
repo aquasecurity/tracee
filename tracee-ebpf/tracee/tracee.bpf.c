2b6d2cf0080e1f2937e5b3a2dd785bc9c61fe30f
Yaniv Agman <yanivagman@gmail.com>
2022-02-01 13:08:05 +0200

    tracee-ebpf: fix cap_capable when using CO-RE
    Add missing cap_opts definition in CO-RE to fix cap_capable event which
    didn't monitor the requested capabilities correctly.
    
    Fix #1341

--- tracee-ebpf/tracee/tracee.bpf.c
+++ tracee-ebpf/tracee/tracee.bpf.c
@@ -2942,8 +2942,6 @@ int BPF_KPROBE(trace_switch_task_namespaces)
 SEC("kprobe/cap_capable")
 int BPF_KPROBE(trace_cap_capable)
 {
-    int audit;
-
     event_data_t data = {};
     if (!init_event_data(&data, ctx))
         return 0;
@@ -2951,18 +2949,10 @@ int BPF_KPROBE(trace_cap_capable)
     if (!should_trace(&data.context))
         return 0;
 
-    //const struct cred *cred = (const struct cred *)PT_REGS_PARM1(ctx);
-    //struct user_namespace *targ_ns = (struct user_namespace *)PT_REGS_PARM2(ctx);
     int cap = PT_REGS_PARM3(ctx);
     int cap_opt = PT_REGS_PARM4(ctx);
 
-  #ifdef CAP_OPT_NONE
-    audit = (cap_opt & 0b10) == 0;
-  #else
-    audit = cap_opt;
-  #endif
-
-    if (audit == 0)
+    if (cap_opt & CAP_OPT_NOAUDIT)
         return 0;
 
     save_to_submit_buf(&data, (void*)&cap, sizeof(int), 0);
