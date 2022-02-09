2e3eb3cd0ac1f8491deafdf53691014275f28d36
RoiKol <roi.kol@aquasec.com>
2022-02-09 12:21:00 +0200

    pass sched_process_fork to userspace even if it was not chosen as it is an essential event

--- pkg/ebpf/c/tracee.bpf.c
+++ pkg/ebpf/c/tracee.bpf.c
@@ -2440,23 +2440,21 @@ int tracepoint__sched__sched_process_fork(struct bpf_raw_tracepoint_args *ctx)
         bpf_map_update_elem(&new_pids_map, &child_pid, &child_pid, BPF_ANY);
     }
 
-    if (event_chosen(SCHED_PROCESS_FORK)) {
-        int parent_ns_pid = get_task_ns_pid(parent);
-        int parent_ns_tgid = get_task_ns_tgid(parent);
-        int child_ns_pid = get_task_ns_pid(child);
-        int child_ns_tgid = get_task_ns_tgid(child);
+    int parent_ns_pid = get_task_ns_pid(parent);
+    int parent_ns_tgid = get_task_ns_tgid(parent);
+    int child_ns_pid = get_task_ns_pid(child);
+    int child_ns_tgid = get_task_ns_tgid(child);
 
-        save_to_submit_buf(&data, (void*)&parent_pid, sizeof(int), 0);
-        save_to_submit_buf(&data, (void*)&parent_ns_pid, sizeof(int), 1);
-        save_to_submit_buf(&data, (void*)&parent_tgid, sizeof(int), 2);
-        save_to_submit_buf(&data, (void*)&parent_ns_tgid, sizeof(int), 3);
-        save_to_submit_buf(&data, (void*)&child_pid, sizeof(int), 4);
-        save_to_submit_buf(&data, (void*)&child_ns_pid, sizeof(int), 5);
-        save_to_submit_buf(&data, (void*)&child_tgid, sizeof(int), 6);
-        save_to_submit_buf(&data, (void*)&child_ns_tgid, sizeof(int), 7);
+    save_to_submit_buf(&data, (void*)&parent_pid, sizeof(int), 0);
+    save_to_submit_buf(&data, (void*)&parent_ns_pid, sizeof(int), 1);
+    save_to_submit_buf(&data, (void*)&parent_tgid, sizeof(int), 2);
+    save_to_submit_buf(&data, (void*)&parent_ns_tgid, sizeof(int), 3);
+    save_to_submit_buf(&data, (void*)&child_pid, sizeof(int), 4);
+    save_to_submit_buf(&data, (void*)&child_ns_pid, sizeof(int), 5);
+    save_to_submit_buf(&data, (void*)&child_tgid, sizeof(int), 6);
+    save_to_submit_buf(&data, (void*)&child_ns_tgid, sizeof(int), 7);
 
-        events_perf_submit(&data, SCHED_PROCESS_FORK, 0);
-    }
+    events_perf_submit(&data, SCHED_PROCESS_FORK, 0);
 
     return 0;
 }
