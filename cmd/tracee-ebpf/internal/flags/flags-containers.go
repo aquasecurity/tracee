779a5e056e93a00ed47c99d7aeca0babbafed201
Nadav Strahilevitz <nadav.strahilevitz@aquasec.com>
2022-03-30 10:33:56 +0000

    flags: add flag to control container runtime connections

---
+++ cmd/tracee-ebpf/internal/flags/flags-containers.go
@@ -0,0 +1,116 @@
+package flags
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aquasecurity/tracee/pkg/containers/runtime"
+)
+
+func ContainersHelp() string {
+	return `Select which container runtimes to connect to for container events enrichment.
+By default, if no flag is passed, tracee will automatically detect installed runtimes by going through known runtime socket paths.
+
+Tracee will look for the following paths:
+1. Docker:     /var/run/docker.sock
+2. Containerd: /var/run/containerd/containerd.sock
+3. CRI-O:      /var/run/crio/crio.sock
+
+If runtimes are specified, only the ones passed through flags will be connected to through the provided socket file path.
+Supported runtimes are:
+
+1. CRI-O      (crio, cri-o)
+2. Containerd (containerd)
+3. Docker     (docker)
+
+Example:
+  --containers crio:/var/run/crio/crio.sock
+`
+}
+
+func contains(s []string, val string) bool {
+	for _, str := range s {
+		if str == val {
+			return true
+		}
+	}
+	return false
+}
+
+func PrepareContainers(containerFlags []string) (runtime.Sockets, error) {
+	if len(containerFlags) == 0 {
+		return autoDiscoverSockets(debug), nil
+	}
+
+	supportedRuntimes := []string{"crio", "cri-o", "containerd", "docker"}
+
+	sockets := runtime.Sockets{}
+
+	for _, flag := range containerFlags {
+		parts := strings.Split(flag, ":")
+		if len(parts) != 2 {
+			return sockets, fmt.Errorf("failed to parse container flags (must be of format {runtime}:{socket path})")
+		}
+		containerRuntime := parts[0]
+
+		if !contains(supportedRuntimes, containerRuntime) {
+			return sockets, fmt.Errorf("provided unsupported container runtime (see --containers help for supported runtimes)")
+		}
+
+		socket := parts[1]
+		err := sockets.Register(runtimeStringToRuntimeId(containerRuntime), socket)
+
+		if err != nil {
+			return sockets, fmt.Errorf("failed to register runtime socket, %s", err.Error())
+		}
+	}
+
+	return sockets, nil
+}
+
+//check default paths for all supported container runtimes and aggregate them
+func autoDiscoverSockets(debug bool) runtime.Sockets {
+	sockets := runtime.Sockets{}
+	const (
+		defaultContainerd = "/var/run/containerd/containerd.sock"
+		defaultDocker     = "/var/run/docker.sock"
+		defaultCrio       = "/var/run/crio/crio.sock"
+		defaultPodman     = "/var/run/podman/podman.sock"
+	)
+
+	registerSocket(&sockets, "containerd", defaultContainerd)
+	registerSocket(&sockets, "docker", defaultDocker)
+	registerSocket(&sockets, "crio", defaultCrio)
+	registerSocket(&sockets, "podman", defaultPodman)
+
+	return sockets
+}
+
+func registerSocket(sockets *runtime.Sockets, runtime string, socket string) {
+	err := sockets.Register(runtimeStringToRuntimeId(runtime), socket)
+	if DebugModeEnabled() {
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "RuntimeSockets: failed to register default %s socket:\n%v\n", runtime, err)
+		} else {
+			fmt.Fprintf(os.Stdout, "RuntimeSockets: registered default %s runtime socket from %s\n", runtime, socket)
+		}
+	}
+}
+
+func runtimeStringToRuntimeId(containerRuntime string) runtime.RuntimeId {
+	switch containerRuntime {
+	case "docker":
+		return runtime.Docker
+	case "crio":
+		return runtime.Crio
+	case "cri-o":
+		return runtime.Crio
+	case "podman":
+		return runtime.Podman
+	case "containerd":
+		return runtime.Containerd
+	default:
+		return runtime.Unknown
+	}
+}
