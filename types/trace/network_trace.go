ca8aafbe6202720ce8950484a39b602105456b54
RoiKol <roi.kol@aquasec.com>
2022-05-15 18:32:52 +0300

    network events: more values in metadata
    add packet length and the network interface from which it comes.

--- types/trace/network_trace.go
+++ types/trace/network_trace.go
@@ -1,9 +1,11 @@
 package trace
 
 type PktMeta struct {
-	SrcIP    string `json:"src_ip"`
-	DstIP    string `json:"dst_ip"`
-	SrcPort  uint16 `json:"src_port"`
-	DstPort  uint16 `json:"dst_port"`
-	Protocol uint8  `json:"protocol"`
+	SrcIP     string `json:"src_ip"`
+	DstIP     string `json:"dst_ip"`
+	SrcPort   uint16 `json:"src_port"`
+	DstPort   uint16 `json:"dst_port"`
+	Protocol  uint8  `json:"protocol"`
+	PacketLen uint32 `json:"packet_len"`
+	Iface     string `json:"iface"`
 }
