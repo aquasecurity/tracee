cc15085019f99d3c49733f8bb8fc883dd6e0f405
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2022-05-16 22:43:39 -0300

    enricher: service register errors are debug info
    Instead of always displaying error messages to stderr when not able to
    register an enrichment service, make it an informative message through
    debug if it is enabled.
    
    Fixes: https://github.com/aquasecurity/tracee/pull/1572#issuecomment-1128079602

--- pkg/containers/containers.go
+++ pkg/containers/containers.go
@@ -44,7 +44,7 @@ type CgroupInfo struct {
 
 // InitContainers initializes a Containers object and returns a pointer to it.
 // User should further call "Populate" and iterate with Containers data.
-func InitContainers(sockets cruntime.Sockets) (*Containers, error) {
+func InitContainers(sockets cruntime.Sockets, debug bool) (*Containers, error) {
 	cgroupV1 := false
 	if _, err := os.Stat("/sys/fs/cgroup/cgroup.controllers"); os.IsNotExist(err) {
 		cgroupV1 = true
@@ -57,16 +57,16 @@ func InitContainers(sockets cruntime.Sockets) (*Containers, error) {
 
 	//attempt to register for all supported runtimes
 	err := runtimeService.Register(cruntime.Containerd, cruntime.ContainerdEnricher)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to register enricher: %v\n", err)
+	if err != nil && debug {
+		fmt.Fprintf(os.Stderr, "Enricher: %v\n", err)
 	}
 	err = runtimeService.Register(cruntime.Crio, cruntime.CrioEnricher)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to register enricher: %v\n", err)
+	if err != nil && debug {
+		fmt.Fprintf(os.Stderr, "Enricher: %v\n", err)
 	}
 	err = runtimeService.Register(cruntime.Docker, cruntime.DockerEnricher)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to register enricher: %v\n", err)
+	if err != nil && debug {
+		fmt.Fprintf(os.Stderr, "Enricher: %v\n", err)
 	}
 
 	return &Containers{
