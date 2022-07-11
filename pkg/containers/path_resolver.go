b02939c5f8b9432cd07058ae3701ef219340363d
Alon Zivony <alon.zivony@aquasec.com>
2022-07-11 14:59:16 +0300

    pkg/containers: resolve host absolute container path
    Created a struct using Tracee's cache for mount ns process to resolve
    absolute path in the host of a path inside a container.
    This enables the host to access files inside a container, without
    the need to access the container mount point.

---
+++ pkg/containers/path_resolver.go
@@ -0,0 +1,45 @@
+package containers
+
+import (
+	"fmt"
+	"github.com/aquasecurity/tracee/pkg/bucketscache"
+	"io/fs"
+	"os"
+	"strings"
+)
+
+// PathResolver generate an accessible absolute path from the root mount namespace to a relative path
+// in a container.
+// Notice - to resolve host mount namespace, tracee reads from /proc/1/ns which requires CAP_SYS_PTRACE capability.
+type PathResolver struct {
+	fs               fs.FS
+	mountNSPIDsCache *bucketscache.BucketsCache
+}
+
+// InitPathResolver create a resolver for paths from within containers.
+func InitPathResolver(mountNSPIDsCache *bucketscache.BucketsCache) PathResolver {
+	return PathResolver{
+		fs:               os.DirFS("/"),
+		mountNSPIDsCache: mountNSPIDsCache,
+	}
+}
+
+// ResolveAbsolutePath resolves an absolute path, which might be inside a container, to an absolute path
+// within the host mount namespace.
+func (cPathRes PathResolver) ResolveAbsolutePath(mountNSAbsolutePath string, mountNS int) (string, error) {
+	// path should be absolute, except for e.g memfd_create files
+	if mountNSAbsolutePath == "" || mountNSAbsolutePath[0] != '/' {
+		return "", fmt.Errorf("file path is not absolute in its container mount point")
+	}
+	// try to access the root fs via another process in the same mount namespace (since the current process might have already died)
+	pids := cPathRes.mountNSPIDsCache.GetBucket(uint32(mountNS))
+	for _, pid := range pids {
+		procRootPath := fmt.Sprintf("/proc/%d/root", int(pid))
+		// fs.FS interface requires relative paths, so the '/' prefix should be trimmed.
+		_, err := fs.Stat(cPathRes.fs, strings.TrimPrefix(procRootPath, "/"))
+		if err == nil {
+			return fmt.Sprintf("%s%s", procRootPath, mountNSAbsolutePath), nil
+		}
+	}
+	return "", fmt.Errorf("has no access to container fs - no recorded living process of mount namespace %d", mountNS)
+}
---
+++ pkg/containers/path_resolver_test.go
@@ -0,0 +1,146 @@
+package containers
+
+import (
+	"fmt"
+	"github.com/aquasecurity/tracee/pkg/bucketscache"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"testing/fstest"
+)
+
+func TestPathResolver_ResolveAbsolutePath(t *testing.T) {
+	t.Run("Mountns cache tests", func(t *testing.T) {
+		type process struct {
+			pid   uint32
+			alive bool
+		}
+		testCases := []struct {
+			Name          string
+			nsProcesses   []process
+			ExpectedError bool
+		}{
+			{
+				Name: "Existing single process",
+				nsProcesses: []process{
+					{pid: 1, alive: true},
+				},
+				ExpectedError: false,
+			},
+			{
+				Name: "Existing single process and dead children",
+				nsProcesses: []process{
+					{pid: 1, alive: true},
+					{pid: 2, alive: false},
+					{pid: 3, alive: false},
+				},
+				ExpectedError: false,
+			},
+			{
+				Name: "Existing single child process",
+				nsProcesses: []process{
+					{pid: 1, alive: false},
+					{pid: 2, alive: true},
+					{pid: 1, alive: false},
+				},
+				ExpectedError: false,
+			},
+			{
+				Name: "No living processes",
+				nsProcesses: []process{
+					{pid: 1, alive: false},
+					{pid: 2, alive: false},
+					{pid: 1, alive: false},
+				},
+				ExpectedError: true,
+			},
+			{
+				Name:          "No processes in NS",
+				nsProcesses:   []process{},
+				ExpectedError: true,
+			},
+		}
+		testMntNS := 1
+		testFilePath := "/tmp/tmp.so"
+		for _, testCase := range testCases {
+			t.Run(testCase.Name, func(t *testing.T) {
+				// Initialize a mock for the os.Stat function
+				mfs := fstest.MapFS{}
+				bucket := bucketscache.BucketsCache{}
+				bucket.Init(20)
+				for _, p := range testCase.nsProcesses {
+					if p.alive {
+						mfs[fmt.Sprintf("proc/%d/root/%s", p.pid, testFilePath)] = &fstest.MapFile{}
+						bucket.AddBucketItem(uint32(testMntNS), p.pid)
+					}
+				}
+
+				pres := InitPathResolver(&bucket)
+				pres.fs = mfs
+				_, err := pres.ResolveAbsolutePath(testFilePath, testMntNS)
+				if testCase.ExpectedError {
+					assert.Error(t, err)
+				} else {
+					assert.NoError(t, err)
+				}
+			})
+		}
+	})
+
+	t.Run("Path tests", func(t *testing.T) {
+		testCases := []struct {
+			name          string
+			path          string
+			pathExist     bool
+			expectedError bool
+		}{
+			{
+				name:          "Absolute path",
+				path:          "/tmp/tmp.so",
+				pathExist:     true,
+				expectedError: false,
+			},
+			{
+				name:          "Relative path",
+				path:          "./temp.so",
+				pathExist:     true,
+				expectedError: true,
+			},
+			{
+				name:          "Empty path",
+				path:          "",
+				pathExist:     true,
+				expectedError: true,
+			},
+			{
+				name:          "Illegal path",
+				path:          "/tmp/tmp.so",
+				pathExist:     false,
+				expectedError: true,
+			},
+		}
+		testMntNS := 1
+		testPID := 1
+		bucket := bucketscache.BucketsCache{}
+		bucket.Init(20)
+		bucket.AddBucketItem(uint32(testMntNS), 1)
+		for _, testCase := range testCases {
+			t.Run(testCase.name, func(t *testing.T) {
+				// Initialize a mock for the os.Stat function
+				mfs := fstest.MapFS{}
+				if testCase.pathExist {
+					mfs[fmt.Sprintf("proc/%d/root/%s", testPID, testCase.path)] = &fstest.MapFile{}
+				}
+
+				pres := InitPathResolver(&bucket)
+				pres.fs = mfs
+				_, err := pres.ResolveAbsolutePath(testCase.path, testMntNS)
+				if testCase.expectedError {
+					assert.Error(t, err)
+				} else {
+					assert.NoError(t, err)
+				}
+			})
+		}
+	})
+
+}
