cd7d448056ea9132be924eb197df00be01e10c26
Geyslan Gregório <geyslan@gmail.com>
2022-08-11 15:00:48 -0300

    workflows: add test-daily.yaml and libbpfgo.sh
    Keep dependency (libbpfgo) tests inside tracee.
    
    Create test-upstream-libbpfgo Makefile rule.

---
+++ tests/libbpfgo.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#
+# This test is executed by github workflows inside the action runners
+#
+
+info() {
+    echo -n "INFO: "
+    echo "$@"
+}
+
+error_exit() {
+    echo -n "ERROR: "
+    echo "$@"
+    exit 1
+}
+
+git_setup() {
+    git add go.mod go.sum
+    if ! go get github.com/aquasecurity/libbpfgo@main;
+    then
+        git restore --staged go.mod go.sum
+        error_exit "could not go get libbpfgo@main"
+    fi
+}
+
+git_restore() {
+    git checkout go.mod go.sum
+    git restore --staged go.mod go.sum
+}
+
+BASE_DIR="$(dirname "$(realpath "${0}")")"
+TRACEE_DIR="$(realpath "${BASE_DIR}"/..)"
+GO_ENV_EBPF=( "$@" )
+export "${GO_ENV_EBPF[@]}"
+
+git_setup
+trap git_restore ERR
+
+set -e
+STATIC=1 make -C "${TRACEE_DIR}"
+set +e
+
+git_restore
+
+info "SUCCESS"
+
+exit 0
