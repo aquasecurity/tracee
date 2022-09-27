1515d1f3ebec841de9468f5b5adc78b1d2a53db5
ShubhamPalriwala <spalriwalau@gmail.com>
2022-09-27 20:50:39 +0530

    fix: add table format and check for all 3 cases(valid,invalid,none)

--- pkg/events/parse/params_test.go
+++ pkg/events/parse/params_test.go
@@ -4,112 +4,399 @@ import (
 	"testing"
 
 	"github.com/aquasecurity/tracee/types/trace"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestArgInt32Val(t *testing.T) {
-	e := trace.Event{Args: []trace.Argument{
-		{
-			ArgMeta: trace.ArgMeta{
-				Name: "thirty_two_val",
-				Type: "int",
-			},
-			Value: int32(1878),
-		},
-	}}
 
-	_, err := ArgInt32Val(&e, "thirty_two_val")
-	if err != nil {
-		t.Errorf(err.Error())
+	tests := []struct {
+		name          string
+		arg           trace.Argument
+		expectedValue int32
+		errorMessage  string
+	}{
+		{
+			name: "valid_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "valid_val",
+					Type: "int",
+				},
+				Value: int32(1878),
+			},
+			expectedValue: int32(1878),
+		},
+		{
+			name: "invalid_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "invalid_val",
+					Type: "int",
+				},
+				Value: int64(1878),
+			},
+			errorMessage: "argument invalid_val is not of type int32",
+		},
+		{
+			name: "no_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "does_not_exist_val",
+					Type: "int",
+				},
+				Value: int32(1878),
+			},
+			errorMessage: "argument no_val not found",
+		},
 	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := trace.Event{Args: []trace.Argument{
+				{
+					ArgMeta: trace.ArgMeta{
+						Name: tt.arg.Name,
+						Type: tt.arg.Type,
+					},
+					Value: tt.arg.Value,
+				},
+			}}
+			val, err := ArgInt32Val(&e, tt.name)
+			if tt.errorMessage != "" {
+				assert.Equal(t, tt.errorMessage, err.Error())
+				assert.Error(t, err)
+			} else {
+				assert.Equal(t, tt.expectedValue, val)
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
 
 func TestArgStringVal(t *testing.T) {
-	e := trace.Event{Args: []trace.Argument{
+	tests := []struct {
+		name          string
+		arg           trace.Argument
+		expectedValue string
+		errorMessage  string
+	}{
 		{
-			ArgMeta: trace.ArgMeta{
-				Name: "string_val",
-				Type: "string",
+			name: "valid_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "valid_val",
+					Type: "string",
+				},
+				Value: "hello_tracee",
 			},
-			Value: "hello_tracee",
+			expectedValue: "hello_tracee",
 		},
-	}}
+		{
+			name: "invalid_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "invalid_val",
+					Type: "int",
+				},
+				Value: int64(1878),
+			},
+			errorMessage: "argument invalid_val is not of type string",
+		},
+		{
+			name: "no_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "does_not_exist_val",
+					Type: "string",
+				},
+				Value: "hola_tracee",
+			},
+			errorMessage: "argument no_val not found",
+		},
+	}
 
-	_, err := ArgStringVal(&e, "string_val")
-	if err != nil {
-		t.Errorf(err.Error())
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := trace.Event{Args: []trace.Argument{
+				{
+					ArgMeta: trace.ArgMeta{
+						Name: tt.arg.Name,
+						Type: tt.arg.Type,
+					},
+					Value: tt.arg.Value,
+				},
+			}}
+			val, err := ArgStringVal(&e, tt.name)
+			if tt.errorMessage != "" {
+				assert.Equal(t, tt.errorMessage, err.Error())
+				assert.Error(t, err)
+			} else {
+				assert.Equal(t, tt.expectedValue, val)
+				assert.NoError(t, err)
+			}
+		})
 	}
 
 }
 
 func TestArgUint64Val(t *testing.T) {
-	e := trace.Event{Args: []trace.Argument{
+	tests := []struct {
+		name          string
+		arg           trace.Argument
+		expectedValue uint64
+		errorMessage  string
+	}{
 		{
-			ArgMeta: trace.ArgMeta{
-				Name: "u_sixty_four_val",
-				Type: "unsigned int",
+			name: "valid_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "valid_val",
+					Type: "int",
+				},
+				Value: uint64(1878),
 			},
-			Value: uint64(187818781878),
+			expectedValue: uint64(1878),
 		},
-	}}
+		{
+			name: "invalid_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "invalid_val",
+					Type: "int",
+				},
+				Value: uint32(1878),
+			},
+			errorMessage: "argument invalid_val is not of type uint64",
+		},
+		{
+			name: "no_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "does_not_exist_val",
+					Type: "int",
+				},
+				Value: uint64(1878),
+			},
+			errorMessage: "argument no_val not found",
+		},
+	}
 
-	_, err := ArgUint64Val(&e, "u_sixty_four_val")
-	if err != nil {
-		t.Errorf(err.Error())
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := trace.Event{Args: []trace.Argument{
+				{
+					ArgMeta: trace.ArgMeta{
+						Name: tt.arg.Name,
+						Type: tt.arg.Type,
+					},
+					Value: tt.arg.Value,
+				},
+			}}
+			val, err := ArgUint64Val(&e, tt.name)
+			if tt.errorMessage != "" {
+				assert.Equal(t, tt.errorMessage, err.Error())
+				assert.Error(t, err)
+			} else {
+				assert.Equal(t, tt.expectedValue, val)
+				assert.NoError(t, err)
+			}
+		})
 	}
 
 }
 
 func TestArgUint32Val(t *testing.T) {
-	e := trace.Event{Args: []trace.Argument{
+	tests := []struct {
+		name          string
+		arg           trace.Argument
+		expectedValue uint32
+		errorMessage  string
+	}{
 		{
-			ArgMeta: trace.ArgMeta{
-				Name: "u_thirty_two_val",
-				Type: "unsigned int",
+			name: "valid_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "valid_val",
+					Type: "int",
+				},
+				Value: uint32(1878),
 			},
-			Value: uint32(18781878),
+			expectedValue: uint32(1878),
 		},
-	}}
+		{
+			name: "invalid_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "invalid_val",
+					Type: "int",
+				},
+				Value: uint64(1878),
+			},
+			errorMessage: "argument invalid_val is not of type uint32",
+		},
+		{
+			name: "no_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "does_not_exist_val",
+					Type: "int",
+				},
+				Value: uint32(1878),
+			},
+			errorMessage: "argument no_val not found",
+		},
+	}
 
-	_, err := ArgUint32Val(&e, "u_thirty_two_val")
-	if err != nil {
-		t.Errorf(err.Error())
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := trace.Event{Args: []trace.Argument{
+				{
+					ArgMeta: trace.ArgMeta{
+						Name: tt.arg.Name,
+						Type: tt.arg.Type,
+					},
+					Value: tt.arg.Value,
+				},
+			}}
+			val, err := ArgUint32Val(&e, tt.name)
+			if tt.errorMessage != "" {
+				assert.Equal(t, tt.errorMessage, err.Error())
+				assert.Error(t, err)
+			} else {
+				assert.Equal(t, tt.expectedValue, val)
+				assert.NoError(t, err)
+			}
+		})
 	}
 
 }
 
 func TestArgStringArrVal(t *testing.T) {
-	e := trace.Event{Args: []trace.Argument{
+	tests := []struct {
+		name          string
+		arg           trace.Argument
+		expectedValue []string
+		errorMessage  string
+	}{
 		{
-			ArgMeta: trace.ArgMeta{
-				Name: "arr_string_val",
-				Type: "string",
+			name: "valid_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "valid_val",
+					Type: "string",
+				},
+				Value: []string{"hello", "tracee", "ebpf"},
 			},
-			Value: []string{"hello", "tracee", "ebpf"},
+			expectedValue: []string{"hello", "tracee", "ebpf"},
 		},
-	}}
-
-	_, err := ArgStringArrVal(&e, "arr_string_val")
-	if err != nil {
-		t.Errorf(err.Error())
+		{
+			name: "invalid_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "invalid_val",
+					Type: "int",
+				},
+				Value: int64(1878),
+			},
+			errorMessage: "argument invalid_val is not of type string",
+		},
+		{
+			name: "no_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "does_not_exist_val",
+					Type: "string",
+				},
+				Value: []string{"hello", "tracee", "ebpf"},
+			},
+			errorMessage: "argument no_val not found",
+		},
 	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := trace.Event{Args: []trace.Argument{
+				{
+					ArgMeta: trace.ArgMeta{
+						Name: tt.arg.Name,
+						Type: tt.arg.Type,
+					},
+					Value: tt.arg.Value,
+				},
+			}}
+			val, err := ArgStringArrVal(&e, tt.name)
+			if tt.errorMessage != "" {
+				assert.Equal(t, tt.errorMessage, err.Error())
+				assert.Error(t, err)
+			} else {
+				assert.Equal(t, tt.expectedValue, val)
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
 
 func TestArgUlongArrVal(t *testing.T) {
-	e := trace.Event{Args: []trace.Argument{
+	tests := []struct {
+		name          string
+		arg           trace.Argument
+		expectedValue []uint64
+		errorMessage  string
+	}{
 		{
-			ArgMeta: trace.ArgMeta{
-				Name: "arr_u_sixty_four",
-				Type: "unsigned int",
+			name: "valid_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "valid_val",
+					Type: "int",
+				},
+				Value: []uint64{1878, 1878, 1878},
 			},
-			Value: []uint64{187818781878, 187818781879, 1878187880},
+			expectedValue: []uint64{1878, 1878, 1878},
 		},
-	}}
-
-	_, err := ArgUlongArrVal(&e, "arr_u_sixty_four")
-	if err != nil {
-		t.Errorf(err.Error())
+		{
+			name: "invalid_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "invalid_val",
+					Type: "int",
+				},
+				Value: []uint32{1878, 1878, 1878},
+			},
+			errorMessage: "argument invalid_val is not of type ulong array",
+		},
+		{
+			name: "no_val",
+			arg: trace.Argument{
+				ArgMeta: trace.ArgMeta{
+					Name: "does_not_exist_val",
+					Type: "int",
+				},
+				Value: []uint64{1878, 1878, 1878},
+			},
+			errorMessage: "argument no_val not found",
+		},
 	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := trace.Event{Args: []trace.Argument{
+				{
+					ArgMeta: trace.ArgMeta{
+						Name: tt.arg.Name,
+						Type: tt.arg.Type,
+					},
+					Value: tt.arg.Value,
+				},
+			}}
+			val, err := ArgUlongArrVal(&e, tt.name)
+			if tt.errorMessage != "" {
+				assert.Equal(t, tt.errorMessage, err.Error())
+				assert.Error(t, err)
+			} else {
+				assert.Equal(t, tt.expectedValue, val)
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
