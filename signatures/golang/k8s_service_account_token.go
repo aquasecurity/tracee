5b19264d52a2c36627c47c6e2f8c42bda143ac65
RoiKol <roi.kol@aquasec.com>
2022-10-19 19:02:28 +0300

    signature: add k8s_service_account_token.go sig

---
+++ signatures/golang/k8s_service_account_token.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/aquasecurity/tracee/signatures/helpers"
+	"github.com/aquasecurity/tracee/types/detect"
+	"github.com/aquasecurity/tracee/types/protocol"
+	"github.com/aquasecurity/tracee/types/trace"
+)
+
+type K8SServiceAccountToken struct {
+	cb               detect.SignatureHandler
+	legitProcs       []string
+	tokenPathPattern string
+	compiledRegex    *regexp.Regexp
+}
+
+func (sig *K8SServiceAccountToken) Init(cb detect.SignatureHandler) error {
+	var err error
+	sig.cb = cb
+	sig.legitProcs = []string{"flanneld", "kube-proxy", "etcd", "kube-apiserver", "coredns", "kube-controller", "kubectl"}
+	sig.tokenPathPattern = `secrets/kubernetes.io/serviceaccount.+token$`
+	sig.compiledRegex, err = regexp.Compile(sig.tokenPathPattern)
+	return err
+}
+
+func (sig *K8SServiceAccountToken) GetMetadata() (detect.SignatureMetadata, error) {
+	return detect.SignatureMetadata{
+		ID:          "TRC-8",
+		Version:     "1",
+		Name:        "K8s service account token file read",
+		Description: "The Kubernetes service account token file was read on your container. This token is used to communicate with the Kubernetes API Server. Adversaries may try to communicate with the API Server to steal information and/or credentials, or even run more containers and laterally extend their grip on the systems.",
+		Properties: map[string]interface{}{
+			"Severity":             0,
+			"Category":             "credential-access",
+			"Technique":            "Exploitation for Credential Access",
+			"Kubernetes_Technique": "Container service account",
+			"id":                   "attack-pattern--9c306d8d-cde7-4b4c-b6e8-d0bb16caca36",
+			"external_id":          "T1212",
+		},
+	}, nil
+}
+
+func (sig *K8SServiceAccountToken) GetSelectedEvents() ([]detect.SignatureEventSelector, error) {
+	return []detect.SignatureEventSelector{
+		{Source: "tracee", Name: "security_file_open", Origin: "container"},
+	}, nil
+}
+
+func (sig *K8SServiceAccountToken) OnEvent(event protocol.Event) error {
+
+	eventObj, ok := event.Payload.(trace.Event)
+	if !ok {
+		return fmt.Errorf("invalid event")
+	}
+
+	switch eventObj.EventName {
+
+	case "security_file_open":
+		// check process touching token is not on allow list
+		for _, legitProc := range sig.legitProcs {
+			if legitProc == eventObj.ProcessName {
+				return nil
+			}
+		}
+
+		pathname, err := helpers.GetTraceeStringArgumentByName(eventObj, "pathname")
+		if err != nil {
+			return err
+		}
+
+		flags, err := helpers.GetTraceeStringArgumentByName(eventObj, "flags")
+		if err != nil {
+			return err
+		}
+
+		if helpers.IsFileRead(flags) && sig.compiledRegex.MatchString(pathname) {
+			metadata, err := sig.GetMetadata()
+			if err != nil {
+				return err
+			}
+			sig.cb(detect.Finding{
+				SigMetadata: metadata,
+				Event:       event,
+				Data:        nil,
+			})
+		}
+	}
+	return nil
+}
+
+func (sig *K8SServiceAccountToken) OnSignal(s detect.Signal) error {
+	return nil
+}
+func (sig *K8SServiceAccountToken) Close() {}
---
+++ signatures/golang/k8s_service_account_token_test.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aquasecurity/tracee/signatures/signaturestest"
+	"github.com/aquasecurity/tracee/types/detect"
+	"github.com/aquasecurity/tracee/types/trace"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestK8SServiceAccountToken(t *testing.T) {
+	testCases := []struct {
+		Name     string
+		Events   []trace.Event
+		Findings map[string]detect.Finding
+	}{
+		{
+			Name: "should trigger detection",
+			Events: []trace.Event{
+				{
+					ProcessName: "test",
+					EventName:   "security_file_open",
+					Args: []trace.Argument{
+						{
+							ArgMeta: trace.ArgMeta{
+								Name: "flags",
+							},
+							Value: interface{}("O_RDONLY"),
+						},
+						{
+							ArgMeta: trace.ArgMeta{
+								Name: "pathname",
+							},
+							Value: interface{}("/var/run/secrets/kubernetes.io/serviceaccount/token"),
+						},
+					},
+				},
+			},
+			Findings: map[string]detect.Finding{
+				"TRC-8": {
+					Data: nil,
+					Event: trace.Event{
+						ProcessName: "test",
+						EventName:   "security_file_open",
+						Args: []trace.Argument{
+							{
+								ArgMeta: trace.ArgMeta{
+									Name: "flags",
+								},
+								Value: interface{}("O_RDONLY"),
+							},
+							{
+								ArgMeta: trace.ArgMeta{
+									Name: "pathname",
+								},
+								Value: interface{}("/var/run/secrets/kubernetes.io/serviceaccount/token"),
+							},
+						},
+					}.ToProtocol(),
+					SigMetadata: detect.SignatureMetadata{
+						ID:          "TRC-8",
+						Version:     "1",
+						Name:        "K8s service account token file read",
+						Description: "The Kubernetes service account token file was read on your container. This token is used to communicate with the Kubernetes API Server. Adversaries may try to communicate with the API Server to steal information and/or credentials, or even run more containers and laterally extend their grip on the systems.",
+						Properties: map[string]interface{}{
+							"Severity":             0,
+							"Category":             "credential-access",
+							"Technique":            "Exploitation for Credential Access",
+							"Kubernetes_Technique": "Container service account",
+							"id":                   "attack-pattern--9c306d8d-cde7-4b4c-b6e8-d0bb16caca36",
+							"external_id":          "T1212",
+						},
+					},
+				},
+			},
+		},
+		{
+			Name: "should not trigger detection - wrong open flags",
+			Events: []trace.Event{
+				{
+					ProcessName: "test",
+					EventName:   "security_file_open",
+					Args: []trace.Argument{
+						{
+							ArgMeta: trace.ArgMeta{
+								Name: "flags",
+							},
+							Value: interface{}("O_WRONLY"),
+						},
+						{
+							ArgMeta: trace.ArgMeta{
+								Name: "pathname",
+							},
+							Value: interface{}("/var/run/secrets/kubernetes.io/serviceaccount/token"),
+						},
+					},
+				},
+			},
+			Findings: map[string]detect.Finding{},
+		},
+		{
+			Name: "should not trigger detection - wrong path",
+			Events: []trace.Event{
+				{
+					ProcessName: "test",
+					EventName:   "security_file_open",
+					Args: []trace.Argument{
+						{
+							ArgMeta: trace.ArgMeta{
+								Name: "flags",
+							},
+							Value: interface{}("O_RDONLY"),
+						},
+						{
+							ArgMeta: trace.ArgMeta{
+								Name: "pathname",
+							},
+							Value: interface{}("/tmp/token"),
+						},
+					},
+				},
+			},
+			Findings: map[string]detect.Finding{},
+		},
+		{
+			Name: "should not trigger detection - legit proc",
+			Events: []trace.Event{
+				{
+					ProcessName: "flanneld",
+					EventName:   "security_file_open",
+					Args: []trace.Argument{
+						{
+							ArgMeta: trace.ArgMeta{
+								Name: "flags",
+							},
+							Value: interface{}("O_RDONLY"),
+						},
+						{
+							ArgMeta: trace.ArgMeta{
+								Name: "pathname",
+							},
+							Value: interface{}("/var/run/secrets/kubernetes.io/serviceaccount/token"),
+						},
+					},
+				},
+			},
+			Findings: map[string]detect.Finding{},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			holder := signaturestest.FindingsHolder{}
+			sig := K8SServiceAccountToken{}
+			sig.Init(holder.OnFinding)
+
+			for _, e := range tc.Events {
+				err := sig.OnEvent(e.ToProtocol())
+				require.NoError(t, err)
+			}
+			assert.Equal(t, tc.Findings, holder.GroupBySigID())
+		})
+	}
+}
