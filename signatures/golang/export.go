b56db5ee3438d97a17cf9551e12cc71e41e66eb6
RoiKol <roi.kol@aquasec.com>
2022-10-20 15:00:22 +0300

    signature: use socket_dup event instead of dup(s) syscalls in stdio_over_socket.go

--- signatures/golang/export.go
+++ signatures/golang/export.go
@@ -5,7 +5,7 @@ import "github.com/aquasecurity/tracee/types/detect"
 // ExportedSignatures fulfills the goplugins contract required by the rule-engine
 // this is a list of signatures that this plugin exports
 var ExportedSignatures = []detect.Signature{
-	&stdioOverSocket{},
+	&StdioOverSocket{},
 	&K8sApiConnection{},
 	&AslrInspection{},
 	&DroppedExecutable{},
