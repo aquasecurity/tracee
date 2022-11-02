ea11896e420aff3fdfd77e5fa1f3c9add1016b38
grantseltzer <grantseltzer@gmail.com>
2022-11-02 12:08:51 -0400

    Run integration test triggers in own PID (#2323)
    This is a change to the integration tests so that the commands
    which are meant to trigger events that the tests look for, are
    actually picked up by tracee. In order for this to happen,
    the events can't be the same PID as tracee. This is accomplished
    using syscall.ForkExec() which runs functions in a shell script.
    
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

---
+++ tests/integration/tester.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+do_magic_write() {
+    tmpFileName=$1
+    echo "AAAAA" > $tmpFileName
+}
+
+do_ls() {
+    ls > /dev/null
+}
+
+do_docker_run() {
+    outputFileName=$1
+    output=$(docker run -d --rm alpine)
+    $(echo $output > $outputFileName)
+}
+
+# $1 is the function to call
+# $2 is the temp file to optionally output to
+$1 $2
