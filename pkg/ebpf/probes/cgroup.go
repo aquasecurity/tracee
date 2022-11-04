8caa531e2e57291aaf824286475c0303c6cfdcdc
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2022-11-04 17:46:38 +0000

    probes: use cgroup interface for cgroup probe attachment

--- pkg/ebpf/probes/cgroup.go
+++ pkg/ebpf/probes/cgroup.go
@@ -2,14 +2,25 @@ package probes
 
 import (
 	"fmt"
-	"os"
 
 	bpf "github.com/aquasecurity/libbpfgo"
+	"github.com/aquasecurity/tracee/pkg/cgroup"
 )
 
 //
 // Cgroup
 //
+
+// When attaching a cgroupProbe, by handle, to its eBPF program:
+//
+//   Handle == cgroupProbe
+//
+//     Attach(EventHandle, *cgroup.Cgroup)
+//     Detach(EventHandle, *cgroup.Cgroup)
+//
+// to detach all probes:
+//
+//     DetachAll()
 
 type cgroupProbe struct {
 	programName string
@@ -19,31 +30,34 @@ type cgroupProbe struct {
 
 // attach attaches an eBPF program to a cgroup
 func (p *cgroupProbe) attach(module *bpf.Module, args ...interface{}) error {
+	var cgroups *cgroup.Cgroups
+
+	for _, arg := range args {
+		switch a := arg.(type) {
+		case *cgroup.Cgroups:
+			cgroups = a
+		}
+	}
+	if cgroups == nil {
+		return fmt.Errorf("cgroup probes needs control groups argument")
+	}
+	cgroupV2 := cgroups.GetCgroup(cgroup.CgroupVersion2)
+	if cgroupV2 == nil {
+		return fmt.Errorf("cgroup probes needs cgroup v2 support")
+	}
+
+	cgroupV2MountPoint := cgroupV2.GetMountPoint()
+
 	var link *bpf.BPFLink
 
 	if p.bpfLink != nil {
 		return nil // already attached, it is ok to call attach again
 	}
 
-	var cgroupDir string
-	for _, arg := range args {
-		switch a := arg.(type) {
-		case string:
-			cgroupDir = a
-		}
-	}
-
 	// sanity checks
 
 	if module == nil {
 		return fmt.Errorf("incorrect arguments for program: %s", p.programName)
-	}
-	if cgroupDir == "" {
-		return fmt.Errorf("program %s needs a cgroup to attach to", p.programName)
-	}
-
-	if _, err := os.Stat(cgroupDir); os.IsNotExist(err) {
-		return fmt.Errorf("program %s could not attach to cgroup directory: %s", p.programName, cgroupDir)
 	}
 
 	// attach
@@ -53,9 +67,9 @@ func (p *cgroupProbe) attach(module *bpf.Module, args ...interface{}) error {
 		return err
 	}
 
-	link, err = prog.AttachCgroupLegacy(cgroupDir, p.attachType)
+	link, err = prog.AttachCgroupLegacy(cgroupV2MountPoint, p.attachType)
 	if err != nil {
-		return fmt.Errorf("failed to attach program %s to cgroup %s", p.programName, cgroupDir)
+		return fmt.Errorf("failed to attach program %s to cgroup %s", p.programName, cgroupV2MountPoint)
 	}
 
 	p.bpfLink = link
