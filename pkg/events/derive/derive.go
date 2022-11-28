97c222a92de902c3bd3d0d8acc38e6fa3a926cff
Alon Zivony <87267148+AlonZivony@users.noreply.github.com>
2022-11-28 14:11:45 +0200

    pkg/events: add API to derive multiple events from single function (#2384)
    Until now, we had a function to support derivation of a single event
    from a single base event.
    
    However, sometimes a single base event can trigger multiple instances
    of a single derived event (aka, one derive function that should derive
    multiple events in a single call).
    
    We add support for it, and unite the two APIs to use the same code.

--- pkg/events/derive/derive.go
+++ pkg/events/derive/derive.go
@@ -45,29 +45,61 @@ type deriveBase struct {
 
 // deriveArgsFunction defines the logic of deriving an Event.
 // It checks the base event and produces arguments for the derived event.
-// If an event can't be derived, the returned arguments should be nil.
+// If an event can't be derived, the returned arguments should be `nil`.
 type deriveArgsFunction func(event trace.Event) ([]interface{}, error)
+
+// multiDeriveArgsFunction defines the logic of deriving multiple Events.
+// It checks the event and produce the arguments of multiple derived event.
+// If no event is derived, then the returned args should equal `nil`.
+// To enable error handling of more than one failed derived events,
+// all errors while events derivation should be appended to a list.
+type multiDeriveArgsFunction func(event trace.Event) ([][]interface{}, []error)
 
 // deriveSingleEvent create an deriveFunction which generates a single derive trace.Event.
 // The event will be created using the original event information, the ID given and resulting
 // arguments from the function.
 // The arguments will be inserted in order, so they should match the resulting definition argument order.
 // If the returned arguments are nil - no event will be derived.
+// This function is an envelope for the deriveMultipleEvents function, to make it easier to create single event
+// derivation function.
 func deriveSingleEvent(id events.ID, deriveArgs deriveArgsFunction) deriveFunction {
+	singleDerive := func(event trace.Event) ([][]interface{}, []error) {
+		var multiArgs [][]interface{}
+		var errs []error
+		args, err := deriveArgs(event)
+		if args != nil {
+			multiArgs = append(multiArgs, args)
+		}
+		if err != nil {
+			errs = append(errs, err)
+		}
+		return multiArgs, errs
+	}
+	return deriveMultipleEvents(id, singleDerive)
+}
+
+// deriveMultipleEvents create an deriveFunction to generate multiple derive trace.Events.
+// The events will be created using the original event information, the ID given and the arguments given.
+// The order of the arguments given will match the order in the event definition, so make sure the order match
+// the order of the params in the events.event struct of the event under events.Definitions.
+// If the arguments given is nil, then no event will be derived.
+func deriveMultipleEvents(id events.ID, multiDeriveArgsFunc multiDeriveArgsFunction) deriveFunction {
 	skeleton := makeDeriveBase(id)
 	return func(event trace.Event) ([]trace.Event, []error) {
-		args, err := deriveArgs(event)
-		if err != nil {
-			return nil, []error{err}
+		multiArgs, errs := multiDeriveArgsFunc(event)
+		if multiArgs == nil {
+			return []trace.Event{}, errs
 		}
-		if args == nil {
-			return []trace.Event{}, nil
+		var derivedEvents []trace.Event
+		for _, args := range multiArgs {
+			de, err := buildDerivedEvent(&event, skeleton, args)
+			if err != nil {
+				errs = append(errs, err)
+			} else {
+				derivedEvents = append(derivedEvents, de)
+			}
 		}
-		de, err := buildDerivedEvent(&event, skeleton, args)
-		if err != nil {
-			return []trace.Event{}, []error{err}
-		}
-		return []trace.Event{de}, nil
+		return derivedEvents, errs
 	}
 }
 
