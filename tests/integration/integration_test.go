de57aa420aa0800685c5b8950b8aaceada701c4c
grantseltzer <grantseltzer@gmail.com>
2022-12-05 09:59:24 -0500

    Embed test script and import environment variable (#2366)
    * Embed test script and import environment variable
    
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -2,6 +2,8 @@ package integration
 
 import (
 	"context"
+	_ "embed"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -294,28 +296,54 @@ const (
 	doFileOpen   testFunc = "do_file_open"
 )
 
+//go:embed tester.sh
+var testerscript []byte
+
 // forkAndExecFunction runs a function in `tester.sh` in it's own system process.
 // This is so Tracee running in the current pid can pick the command up.
 // It returns the output of the process and a possible error.
 func forkAndExecFunction(funcName testFunc) ([]byte, error) {
-	tmpFile, err := os.CreateTemp("/tmp", "tracee-test*")
+
+	f, err := os.CreateTemp("", "tracee-integration-test-script")
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("couldn't create temp file for script: %w", err)
 	}
-	_, err = syscall.ForkExec("./tester.sh", []string{"./tester.sh", string(funcName), tmpFile.Name()},
+
+	_, err = f.Write(testerscript)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't write temp script: %w", err)
+	}
+
+	err = f.Close()
+	if err != nil {
+		return nil, fmt.Errorf("couldn't close fd for script: %w", err)
+	}
+
+	tmpOutputFile, err := os.CreateTemp("/tmp", "tracee-test*")
+	if err != nil {
+		return nil, fmt.Errorf("couldn't create temporary output file: %w", err)
+	}
+
+	err = os.Chmod(f.Name(), 0777)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't chmod script file: %w", err)
+	}
+
+	_, err = syscall.ForkExec(f.Name(), []string{f.Name(), string(funcName), tmpOutputFile.Name()},
 		&syscall.ProcAttr{
-			Files: []uintptr{0, 1, 2, tmpFile.Fd()},
+			Files: []uintptr{0, 1, 2, tmpOutputFile.Fd()},
+			Env:   os.Environ(),
 		})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("couldn't fork/exec: %w", err)
 	}
 
 	// ForkExec doesn't block, wait for output
 	time.Sleep(time.Second)
 
-	output, err := ioutil.ReadAll(tmpFile)
+	output, err := ioutil.ReadAll(tmpOutputFile)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("couldn't read output: %w", err)
 	}
 	return output, nil
 }
