fea9ea0468cc3a759d875e76309f5cfc33f6a218
Jose Donizetti <jdbjunior@gmail.com>
2022-12-06 16:05:10 -0300

    tracee: add rego flags

---
+++ pkg/cmd/flags/rego.go
@@ -0,0 +1,47 @@
+package flags
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/aquasecurity/tracee/pkg/rules/rego"
+	"github.com/open-policy-agent/opa/compile"
+)
+
+func regoHelp() string {
+	return `Rego configurations.
+possible options:
+partial-eval            enable partial evaluation of rego rules.
+aio                     compile rego signatures altogether as an aggregate policy. By default each signature is compiled separately.
+Examples:
+  --rego partial-eval                               | enable partial evaluation
+  --rego partial-eval --rego aio                    | enable partial evaluation, and aggregate policy compilation.
+Use this flag multiple times to choose multiple output options
+`
+}
+
+func PrepareRego(regoSlice []string) (rego.Config, error) {
+	c := rego.Config{
+		RuntimeTarget: compile.TargetRego,
+		PartialEval:   false,
+		AIO:           false,
+	}
+
+	if len(regoSlice) == 0 {
+		return c, nil
+	}
+
+	for _, s := range regoSlice {
+		optValue := strings.TrimSpace(s)
+		switch optValue {
+		case "partial-eval":
+			c.PartialEval = true
+		case "aio":
+			c.AIO = true
+		default:
+			return rego.Config{}, errors.New("invalid rego option specified, use '--rego help' for more info")
+		}
+	}
+
+	return c, nil
+}
