1d7eb76120394c4b3f5335783b5f83e8297cb2f6
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2023-01-15 12:37:13 -0300

    ebpf: turn has_prefix to macro using compound expr (#2581)

--- pkg/ebpf/c/tracee.bpf.c
+++ pkg/ebpf/c/tracee.bpf.c
@@ -2483,10 +2483,40 @@ static __always_inline int events_perf_submit(program_data_t *p, u32 id, long re
 
 // INTERNAL: STRINGS -------------------------------------------------------------------------------
 
+// Workaround: Newer LLVM versions might fail to optimize has_prefix()
+// loop unrolling with the following error:
+//
+//     warning: loop not unrolled: the optimizer was unable to perform
+//     the requested transformation; the transformation might be
+//     disabled or specified as part of an unsupported transformation
+//     ordering
+//
+
+#if defined(__clang__) && __clang_major__ > 13
+
+    #define has_prefix(p, s, n)                                                                    \
+        ({                                                                                         \
+            int rc = 0;                                                                            \
+            char *pre = p, *str = s;                                                               \
+            _Pragma("unroll") for (int z = 0; z < n; pre++, str++, z++)                            \
+            {                                                                                      \
+                if (!*pre) {                                                                       \
+                    rc = 1;                                                                        \
+                    break;                                                                         \
+                } else if (*pre != *str) {                                                         \
+                    rc = 0;                                                                        \
+                    break;                                                                         \
+                }                                                                                  \
+            }                                                                                      \
+            rc;                                                                                    \
+        })
+
+#else
+
 static __inline int has_prefix(char *prefix, char *str, int n)
 {
     int i;
-#pragma unroll
+    #pragma unroll
     for (i = 0; i < n; prefix++, str++, i++) {
         if (!*prefix)
             return 1;
@@ -2498,6 +2528,8 @@ static __inline int has_prefix(char *prefix, char *str, int n)
     // prefix is too long
     return 0;
 }
+
+#endif
 
 // HELPERS: VFS ------------------------------------------------------------------------------------
 
