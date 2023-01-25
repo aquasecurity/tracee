aa9a7c07e5b9c92ce1c58f2804ed37a70c917576
Nadav Strahilevitz <nadav.strahilevitz@aquasec.com>
2023-01-25 17:38:39 +0000

    network: silence dns test prints

--- tests/e2e-net-rules/scripts/dns.sh
+++ tests/e2e-net-rules/scripts/dns.sh
@@ -14,9 +14,9 @@ command -v nslookup > /dev/null || exit_err "missing nslookup tool"
 # start dns test
 
 test() {
-    nslookup -type=mx $HOST $SERVER
-    nslookup -type=ns $HOST $SERVER
-    nslookup -type=soa $HOST $SERVER
+    nslookup -type=mx $HOST $SERVER > /dev/null
+    nslookup -type=ns $HOST $SERVER > /dev/null
+    nslookup -type=soa $HOST $SERVER > /dev/null
     sleep 1
 }
 
