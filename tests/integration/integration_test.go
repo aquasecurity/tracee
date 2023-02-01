fd087a065b717eebeef3547678b8cfdd4123bbe9
Geyslan Gregório <geyslan@gmail.com>
2023-02-01 15:24:37 -0300

    fix: tests: TestInitNamespacesEvent to integration
    Move TestInitNamespacesEvent test to integration tests, since
    InitNamespacesEvent() requires root permission.

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -21,6 +21,23 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestInitNamespacesEvent(t *testing.T) {
+	procNamespaces := [...]string{"mnt", "cgroup", "pid", "pid_for_children", "time", "time_for_children", "user", "ipc", "net", "uts"}
+	evts := events.InitNamespacesEvent()
+	initNamespaces := make(map[string]uint32)
+
+	for _, arg := range evts.Args {
+		namespaceVale, ok := arg.Value.(uint32)
+		assert.Truef(t, ok, "Value of namespace %s is not valid: %v", arg.Name, arg.Value)
+		initNamespaces[arg.Name] = namespaceVale
+	}
+
+	for _, namespace := range procNamespaces {
+		assert.Contains(t, initNamespaces, namespace)
+		assert.NotZero(t, initNamespaces[namespace])
+	}
+}
+
 // small set of actions to trigger a magic write event
 func checkMagicwrite(t *testing.T, gotOutput *[]trace.Event) {
 
