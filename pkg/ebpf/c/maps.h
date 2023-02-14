f467b61e796987817c1ad135d88cdb245966acd2
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2023-02-14 14:27:45 -0300

    network: move typedefs around and fix includes

--- pkg/ebpf/c/maps.h
+++ pkg/ebpf/c/maps.h
@@ -1,6 +1,14 @@
 #ifndef __MAPS_H__
 #define __MAPS_H__
 
+#ifndef CORE
+    #include "missing_noncore_definitions.h"
+#else
+    // CO:RE is enabled
+    #include <vmlinux.h>
+    #include <missing_definitions.h>
+#endif
+
 #include <bpf/bpf_helpers.h>
 #include "types.h"
 
