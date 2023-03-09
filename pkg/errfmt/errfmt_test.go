da560fe886886d436f5354123736b8893480645c
Geyslan Gregório <geyslan@gmail.com>
2023-03-09 12:19:37 -0300

    errfmt: introduce new package for error formatting
    This commit introduces a new error-formatting package, moving and
    re-factoring existing error-formatting code from the logger package
    into it.

---
+++ pkg/errfmt/errfmt_test.go
@@ -0,0 +1,78 @@
+package errfmt
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+func TestErrorf(t *testing.T) {
+	tests := []struct {
+		name   string
+		format string
+		args   []interface{}
+		want   error
+	}{
+		{
+			name:   "empty format string",
+			format: "",
+			args:   []interface{}{},
+			want:   nil,
+		},
+		{
+			name:   "non-empty format string",
+			format: "an error occurred",
+			args:   []interface{}{},
+			want:   fmt.Errorf("errfmt.TestErrorf.func1: an error occurred"),
+		},
+		{
+			name:   "format string with arguments",
+			format: "an error occurred, %d",
+			args:   []interface{}{42},
+			want:   fmt.Errorf("errfmt.TestErrorf.func1: an error occurred, 42"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Errorf(tt.format, tt.args...)
+			if tt.want == nil {
+				assert.Equal(t, tt.want, got)
+				return
+			}
+			assert.Equal(t, tt.want.Error(), got.Error(), "got: %v, want: %v", got.Error(), tt.want.Error())
+		})
+	}
+}
+
+func TestWrapError(t *testing.T) {
+	tests := []struct {
+		name string
+		err  error
+		want error
+	}{
+		{
+			name: "nil error",
+			err:  nil,
+			want: nil,
+		},
+		{
+			name: "non-nil error",
+			err:  errors.New("an error occurred"),
+			want: fmt.Errorf("errfmt.TestWrapError.func1: an error occurred"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := WrapError(tt.err)
+			if tt.want == nil {
+				assert.Equal(t, tt.want, got)
+				return
+			}
+			assert.Equal(t, tt.want.Error(), got.Error(), "got: %v, want: %v", got.Error(), tt.want.Error())
+		})
+	}
+}
