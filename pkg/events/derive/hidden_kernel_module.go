9ce5d6e9656b624e3bf2271ba56c41b96c2fe547
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2023-05-02 00:11:49 -0300

    events: fix hidden_kernel_module derivation
    - missed a lockup in capabilities ring raising during review, fix it
    - fixed logger errors (needs key/value) and used correct error func

--- pkg/events/derive/hidden_kernel_module.go
+++ pkg/events/derive/hidden_kernel_module.go
@@ -3,7 +3,6 @@ package derive
 import (
 	"bufio"
 	"encoding/binary"
-	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -17,6 +16,7 @@ import (
 	"github.com/aquasecurity/libbpfgo/helpers"
 
 	"github.com/aquasecurity/tracee/pkg/capabilities"
+	"github.com/aquasecurity/tracee/pkg/errfmt"
 	"github.com/aquasecurity/tracee/pkg/events"
 	"github.com/aquasecurity/tracee/pkg/events/parse"
 	"github.com/aquasecurity/tracee/pkg/logger"
@@ -74,7 +74,7 @@ func clearMap(bpfMap *bpf.BPFMap) error {
 				err = bpfMap.DeleteKey(unsafe.Pointer(&addr))
 
 				if err != nil {
-					logger.Errorw("err occurred DeleteKey: " + err.Error())
+					logger.Errorw("DeleteKey", "error", err.Error())
 					return err
 				}
 			}
@@ -91,70 +91,77 @@ func ClearModulesState(modsMap *bpf.BPFMap) {
 }
 
 func FillModulesFromProcFs(kernelSymbols helpers.KernelSymbolTable, modulesFromProcFs *bpf.BPFMap) error {
-	return capabilities.GetInstance().EBPF(
+	var file *os.File
+	var err error
+
+	err = capabilities.GetInstance().Specific(
 		func() error {
-			return capabilities.GetInstance().Specific(
-				func() error {
-					file, err := os.Open("/proc/modules")
-					if err != nil {
-						logger.Errorw("error opening /proc/modules", err)
-						return errors.New("error opening /proc/modules")
-					}
-					defer func() {
-						if err := file.Close(); err != nil {
-							logger.Errorw("error closing /proc/modules", err)
-						}
-					}()
+			file, err = os.Open("/proc/modules")
+			return err
+		},
+		cap.DAC_OVERRIDE,
+	)
+	if err != nil {
+		logger.Errorw("error opening /proc/modules", "error", err)
+		return errfmt.WrapError(err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			logger.Errorw("error closing /proc/modules", "error", err)
+		}
+	}()
 
-					scanner := bufio.NewScanner(file)
-					for scanner.Scan() {
-						line := scanner.Text()
-						lineSplit := strings.Split(line, " ")
-						lineLen := len(lineSplit)
-						if lineLen < 3 {
-							logger.Warnw("Unexpected format in /proc/modules", lineSplit)
-							return errors.New("unexpected format in /proc/modules")
-						}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineSplit := strings.Split(line, " ")
+		lineLen := len(lineSplit)
+		if lineLen < 3 {
+			logger.Warnw("Unexpected format in /proc/modules", lineSplit)
+			return errfmt.WrapError(err)
+		}
 
-						moduleName := lineSplit[0]
-						var addr uint64
+		moduleName := lineSplit[0]
+		var addr uint64
 
-						// get module address from kallsyms since /proc/modules doesn't return the address to __this_module
-						ks, err := kernelSymbols.GetSymbolByName(moduleName, "__this_module")
-						if err != nil {
-							// this most likely means /proc/kallsyms is hooked while /proc/modules isn't
-							// fallback to use the address in /proc/modules
-							candOne := lineSplit[len(lineSplit)-1]
-							candTwo := lineSplit[len(lineSplit)-2]
-							var finalCand string
-							if strings.HasPrefix(candOne, "0x") {
-								finalCand = candOne[2:]
-							} else {
-								finalCand = candTwo[2:]
-							}
+		// get module address from kallsyms since /proc/modules doesn't return
+		// the address to __this_module
+		ks, err := kernelSymbols.GetSymbolByName(moduleName, "__this_module")
+		if err != nil {
+			// this likely means /proc/kallsyms is hooked while /proc/modules
+			// isn't fallback to use the address in /proc/modules
+			candOne := lineSplit[len(lineSplit)-1]
+			candTwo := lineSplit[len(lineSplit)-2]
+			var finalCand string
+			if strings.HasPrefix(candOne, "0x") {
+				finalCand = candOne[2:]
+			} else {
+				finalCand = candTwo[2:]
+			}
 
-							result, parseErr := strconv.ParseUint(finalCand, 16, 64)
-							if parseErr == nil {
-								addr = result
-							}
-						} else {
-							addr = ks.Address
-						}
-						seenInProcModules := true
-						err = modulesFromProcFs.Update(unsafe.Pointer(&addr), unsafe.Pointer(&seenInProcModules))
-						if err != nil {
-							logger.Errorw("Failed updating modulesFromProcFs", err)
-							return errors.New("failed updating modulesFromProcFs")
-						}
-					}
+			result, parseErr := strconv.ParseUint(finalCand, 16, 64)
+			if parseErr == nil {
+				addr = result
+			}
+		} else {
+			addr = ks.Address
+		}
+		seenInProcModules := true
 
-					if err := scanner.Err(); err != nil {
-						logger.Errorw("scanner reported error: ", err)
-					}
+		err = capabilities.GetInstance().EBPF(
+			func() error {
+				return modulesFromProcFs.Update(unsafe.Pointer(&addr), unsafe.Pointer(&seenInProcModules))
+			},
+		)
+		if err != nil {
+			logger.Errorw("modulesFromProcFs update", "error", err)
+			return errfmt.WrapError(err)
+		}
+	}
 
-					return nil
-				},
-				cap.DAC_OVERRIDE,
-			)
-		})
+	if err := scanner.Err(); err != nil {
+		logger.Errorw("scanner", "error", err)
+	}
+
+	return nil
 }
