9123647e8e1b3c05d7c655b0c8c604998528abf0
Geyslan Gregório <geyslan@gmail.com>
2023-05-02 15:04:15 -0300

    cdm: fix list command to print signatures
    This also adds the "signatures-dir" flag to the list command, which
    allows listing signatures from a different directory than the default
    one.

--- cmd/tracee/cmd/list.go
+++ cmd/tracee/cmd/list.go
@@ -1,13 +1,25 @@
 package cmd
 
 import (
+	"os"
+
+	"github.com/open-policy-agent/opa/compile"
 	"github.com/spf13/cobra"
 
 	tcmd "github.com/aquasecurity/tracee/pkg/cmd"
+	"github.com/aquasecurity/tracee/pkg/cmd/initialize"
+	"github.com/aquasecurity/tracee/pkg/events"
+	"github.com/aquasecurity/tracee/pkg/logger"
+	"github.com/aquasecurity/tracee/pkg/signatures/signature"
 )
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+	listCmd.Flags().String(
+		"signatures-dir",
+		"",
+		"Directory where to search for signatures in CEL (.yaml), OPA (.rego), and Go plugin (.so) formats",
+	)
 }
 
 var listCmd = &cobra.Command{
@@ -16,6 +28,27 @@ var listCmd = &cobra.Command{
 	Short:   "List traceable events",
 	Long:    ``,
 	Run: func(cmd *cobra.Command, args []string) {
+		// Get signatures to update event list
+
+		sigsDir, err := cmd.Flags().GetString("signatures-dir")
+		if err != nil {
+			logger.Fatalw("Failed to get signatures-dir flag", "err", err)
+			os.Exit(1)
+		}
+
+		sigs, err := signature.Find(
+			compile.TargetRego,
+			false,
+			sigsDir,
+			nil,
+			false,
+		)
+		if err != nil {
+			logger.Fatalw("Failed to find signatures", "err", err)
+			os.Exit(1)
+		}
+
+		initialize.CreateEventsFromSignatures(events.StartSignatureID, sigs)
 		tcmd.PrintEventList(true) // list events
 	},
 	DisableFlagsInUseLine: true,
