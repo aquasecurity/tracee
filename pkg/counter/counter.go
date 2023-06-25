5a025dde603fb5fa4c504b406ce8c89899ad4eb0
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2023-06-25 05:26:01 +0000

    chore(pkg/counter): add method to increase and read atomically

--- pkg/counter/counter.go
+++ pkg/counter/counter.go
@@ -19,35 +19,53 @@ func NewCounter(initialValue uint64) Counter {
 
 // Increase increases the counter by the given value (thread-safe).
 func (c *Counter) Increase(values ...uint64) error {
+	_, err := c.IncreaseAndRead(values...)
+	return err
+}
+
+// IncreaseAndRead increases the counter by the given value (thread-safe) and returns the new value.
+func (c *Counter) IncreaseAndRead(values ...uint64) (uint64, error) {
 	if len(values) == 0 {
 		values = append(values, 1)
 	}
 
+	var n uint64
+
 	for _, value := range values {
-		if atomic.AddUint64(&c.value, value) < value {
-			return errors.New("counter overflow")
+		n = atomic.AddUint64(&c.value, value)
+		if n < value {
+			return n, errors.New("counter overflow")
 		}
 	}
 
-	return nil
+	return n, nil // return last known value (atomically incremented)
 }
 
 // Decrease decreases the counter by the given value (thread-safe).
 func (c *Counter) Decrease(values ...uint64) error {
+	_, err := c.DecreaseAndRead(values...)
+	return err
+}
+
+// DecreaseAndRead decreases the counter by the given value (thread-safe) and returns the new value.
+func (c *Counter) DecreaseAndRead(values ...uint64) (uint64, error) {
 	if len(values) == 0 {
 		values = append(values, 1)
 	}
+
+	var n uint64
 
 	for _, value := range values {
 		if value == 0 {
 			continue
 		}
-		if atomic.AddUint64(&c.value, ^uint64(value-1)) == math.MaxUint64 {
-			return errors.New("counter underflow")
+		n = atomic.AddUint64(&c.value, ^uint64(value-1))
+		if n == math.MaxUint64 {
+			return n, errors.New("counter underflow")
 		}
 	}
 
-	return nil
+	return n, nil // return last known value (atomically decremented)
 }
 
 // Read returns the current value of the counter (thread-safe).
