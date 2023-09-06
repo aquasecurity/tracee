e611022fbeacc8d78a67d9531d09631da7d79232
Jose Donizetti <jdbjunior@gmail.com>
2023-09-06 15:23:27 -0300

    feat: add GetEventDefinition to the grpc server

--- pkg/server/grpc/tracee.go
+++ pkg/server/grpc/tracee.go
@@ -2,7 +2,9 @@ package grpc
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/aquasecurity/tracee/pkg/events"
 	"github.com/aquasecurity/tracee/pkg/version"
 	pb "github.com/aquasecurity/tracee/types/api/v1beta1"
 )
@@ -11,6 +13,63 @@ type TraceeService struct {
 	pb.UnimplementedTraceeServiceServer
 }
 
+func (s *TraceeService) GetEventDefinition(ctx context.Context, in *pb.GetEventDefinitionRequest) (*pb.GetEventDefinitionResponse, error) {
+	definitions, err := getDefinitions(in)
+	if err != nil {
+		return nil, err
+	}
+
+	out := make([]*pb.EventDefinition, 0, len(definitions))
+
+	for _, d := range definitions {
+		ed := convertDefinitionToProto(d)
+		out = append(out, ed)
+	}
+
+	return &pb.GetEventDefinitionResponse{
+		Definitions: out,
+	}, nil
+}
+
 func (s *TraceeService) GetVersion(ctx context.Context, in *pb.GetVersionRequest) (*pb.GetVersionResponse, error) {
 	return &pb.GetVersionResponse{Version: version.GetVersion()}, nil
+}
+
+func getDefinitions(in *pb.GetEventDefinitionRequest) ([]events.Definition, error) {
+	if in.Name == "" {
+		return events.Core.GetDefinitions(), nil
+	}
+
+	id, ok := events.Core.GetDefinitionIDByName(in.Name)
+	if !ok {
+		return nil, fmt.Errorf("event %s not found", in.Name)
+	}
+
+	return []events.Definition{events.Core.GetDefinitionByID(id)}, nil
+}
+
+func convertDefinitionToProto(d events.Definition) *pb.EventDefinition {
+	params := make([]*pb.Param, len(d.GetParams()))
+
+	for i, p := range d.GetParams() {
+		params[i] = &pb.Param{
+			Name: p.Name,
+			Type: p.Type,
+		}
+	}
+
+	v := &pb.Version{
+		Major: d.GetVersion().Major(),
+		Minor: d.GetVersion().Minor(),
+		Patch: d.GetVersion().Patch(),
+	}
+
+	return &pb.EventDefinition{
+		Id:          int32(d.GetID()),
+		Name:        d.GetName(),
+		Version:     v,
+		Description: d.GetDescription(),
+		Tags:        d.GetSets(),
+		Params:      params,
+	}
 }
