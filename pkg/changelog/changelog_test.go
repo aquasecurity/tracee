66819e29a612c0e921f0a34f17699250eaa5305e
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2023-09-08 17:30:49 -0300

    feature(changelog): introduce the changelog package
    The changelog package provides a changelog data structure for any type.
    
    With the Changelog, it is possible to keep a register of any change made
    to a specific variable and obtain historical values by giving close (or
    accurate) time ranges.
    
    Signed-off-by: Rafael David Tinoco <rafaeldtinoco@gmail.com>
    Signed-off-by: Alon Zivony <alon.zivony@aquasec.com>

---
+++ pkg/changelog/changelog_test.go
@@ -0,0 +1,82 @@
+package changelog
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestChangelog(t *testing.T) {
+	cl := &Changelog[int]{}
+
+	// Test GetCurrent on an empty changelog
+	if cl.GetCurrent() != 0 {
+		t.Errorf("GetCurrent on empty changelog should return 0")
+	}
+
+	// Test SetCurrent and GetCurrent
+	cl.SetCurrent(42)
+	if cl.GetCurrent() != 42 {
+		t.Errorf("GetCurrent after SetCurrent should return 42")
+	}
+
+	// Test Get on an empty changelog
+
+	cl = &Changelog[int]{}
+
+	if cl.Get(time.Now()) != 0 {
+		t.Errorf("Get on empty changelog should return 0")
+	}
+
+	// Test 1 second interval among changes
+
+	cl = &Changelog[int]{}
+
+	cl.SetCurrent(1)
+	time.Sleep(2 * time.Second)
+	cl.SetCurrent(2)
+	time.Sleep(2 * time.Second)
+	cl.SetCurrent(3)
+
+	now := time.Now()
+
+	if cl.Get(now.Add(-4*time.Second)) != 1 {
+		t.Errorf("Get on changelog should return 1")
+	}
+	if cl.Get(now.Add(-2*time.Second)) != 2 {
+		t.Errorf("Get on changelog should return 2")
+	}
+	if cl.Get(now) != 3 {
+		t.Errorf("Get on changelog should return 3")
+	}
+
+	// Test 100 milliseconds interval among changes
+	// NOTE: If this test becomes flaky we can change/remove it.
+
+	cl = &Changelog[int]{}
+
+	cl.SetCurrent(1)
+	time.Sleep(100 * time.Millisecond)
+	cl.SetCurrent(2)
+	time.Sleep(100 * time.Millisecond)
+	cl.SetCurrent(3)
+
+	now = time.Now()
+
+	if cl.Get(now.Add(-200*time.Millisecond)) != 1 {
+		t.Errorf("Get on changelog should return 1")
+	}
+	if cl.Get(now.Add(-100*time.Millisecond)) != 2 {
+		t.Errorf("Get on changelog should return 2")
+	}
+	if cl.Get(now) != 3 {
+		t.Errorf("Get on changelog should return 3")
+	}
+
+	// Test getting all values at once
+
+	expected := []int{1, 2, 3}
+	if !reflect.DeepEqual(cl.GetAll(), expected) {
+		t.Errorf("GetAll should return %v, but got %v", expected, cl.GetAll())
+	}
+}
