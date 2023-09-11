f07277e65336b6611228743fd03b900af8c274ab
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2023-09-11 14:28:18 -0300

    chore(controlplane): rename containers file and eventID var

--- pkg/ebpf/controlplane/signal.go
+++ pkg/ebpf/controlplane/signal.go
@@ -8,8 +8,8 @@ import (
 )
 
 type signal struct {
-	eventID events.ID
-	args    []trace.Argument
+	id   events.ID
+	args []trace.Argument
 }
 
 func (sig *signal) Unmarshal(buffer []byte) error {
@@ -19,19 +19,19 @@ func (sig *signal) Unmarshal(buffer []byte) error {
 	if err != nil {
 		return errfmt.Errorf("failed to decode signal event ID: %v", err)
 	}
-	sig.eventID = events.ID(eventIdUint32)
+	sig.id = events.ID(eventIdUint32)
 	var argnum uint8
 	err = ebpfDecoder.DecodeUint8(&argnum)
 	if err != nil {
 		return errfmt.Errorf("failed to decode signal argnum: %v", err)
 	}
 
-	if !events.Core.IsDefined(sig.eventID) {
-		return errfmt.Errorf("failed to get event %d configuration", sig.eventID)
+	if !events.Core.IsDefined(sig.id) {
+		return errfmt.Errorf("failed to get event %d configuration", sig.id)
 	}
-	eventDefinition := events.Core.GetDefinitionByID(sig.eventID)
+	eventDefinition := events.Core.GetDefinitionByID(sig.id)
 	sig.args = make([]trace.Argument, len(eventDefinition.GetParams()))
-	err = ebpfDecoder.DecodeArguments(sig.args, int(argnum), eventDefinition, sig.eventID)
+	err = ebpfDecoder.DecodeArguments(sig.args, int(argnum), eventDefinition, sig.id)
 	if err != nil {
 		return errfmt.Errorf("failed to decode signal arguments: %v", err)
 	}
