5b5521efd067b61c9f268d8f241a31bf9f4158a8
Jose Donizetti <jdbjunior@gmail.com>
2023-09-25 21:24:22 -0300

    fix: convert trace.Arguments into eventData

---
+++ pkg/server/grpc/event_data.go
@@ -0,0 +1,338 @@
+package grpc
+
+import (
+	"strconv"
+
+	"github.com/golang/protobuf/ptypes/wrappers"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+
+	"github.com/aquasecurity/tracee/pkg/bufferdecoder"
+	"github.com/aquasecurity/tracee/pkg/errfmt"
+	"github.com/aquasecurity/tracee/pkg/events"
+	pb "github.com/aquasecurity/tracee/types/api/v1beta1"
+	"github.com/aquasecurity/tracee/types/trace"
+)
+
+// This was copied for this version, I didn't want to export the types yet,
+// because the code for creating a proto will all be removed from here after
+// we finish integrating the event structure
+const (
+	noneT bufferdecoder.ArgType = iota
+	intT
+	uintT
+	longT
+	ulongT
+	offT
+	modeT
+	devT
+	sizeT
+	pointerT
+	strT
+	strArrT
+	sockAddrT
+	bytesT
+	u16T
+	credT
+	intArr2T
+	uint64ArrT
+	u8T
+	timespecT
+)
+
+const (
+	argsArrT bufferdecoder.ArgType = iota + 0x80
+	boolT
+)
+
+func getEventData(e trace.Event) (map[string]*pb.EventValue, error) {
+	data := make(map[string]*pb.EventValue)
+
+	// for syscaslls
+	args := make([]*pb.EventValue, 0)
+
+	for _, arg := range e.Args {
+		eventValue, err := getEventValue(arg)
+		if err != nil {
+			return nil, err
+		}
+
+		if events.Core.GetDefinitionByID(events.ID(e.EventID)).IsSyscall() {
+			args = append(args, eventValue)
+			continue
+		}
+
+		data[arg.ArgMeta.Name] = eventValue
+	}
+
+	if len(args) > 0 {
+		data["args"] = &pb.EventValue{
+			Value: &pb.EventValue_Args{
+				Args: &pb.ArgsValue{
+					Value: args,
+				},
+			},
+		}
+
+		data["returnValue"] = &pb.EventValue{
+			Value: &pb.EventValue_Int64{
+				Int64: wrapperspb.Int64(int64(e.ReturnValue)),
+			},
+		}
+	}
+
+	return data, nil
+}
+
+func getEventValue(arg trace.Argument) (*pb.EventValue, error) {
+	var eventValue *pb.EventValue
+	switch bufferdecoder.GetParamType(arg.Type) {
+	case intT:
+		if v, ok := arg.Value.(int32); ok {
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_Int32{
+					Int32: wrapperspb.Int32(v),
+				},
+			}
+		}
+	case u8T:
+		if v, ok := arg.Value.(uint8); ok {
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_UInt32{
+					UInt32: wrapperspb.UInt32(uint32(v)),
+				},
+			}
+		}
+	case u16T:
+		if v, ok := arg.Value.(uint16); ok {
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_UInt32{
+					UInt32: wrapperspb.UInt32(uint32(v)),
+				},
+			}
+		}
+	case uintT, modeT, devT, sizeT:
+		if v, ok := arg.Value.(uint32); ok {
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_UInt32{
+					UInt32: wrapperspb.UInt32(v),
+				},
+			}
+		}
+	case longT:
+		if v, ok := arg.Value.(int64); ok {
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_Int64{
+					Int64: wrapperspb.Int64(v),
+				},
+			}
+		}
+	case ulongT, offT:
+		if v, ok := arg.Value.(uint64); ok {
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_UInt64{
+					UInt64: wrapperspb.UInt64(v),
+				},
+			}
+		}
+	case boolT:
+		if v, ok := arg.Value.(bool); ok {
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_Bool{
+					Bool: wrapperspb.Bool(v),
+				},
+			}
+		}
+	case strT:
+		if v, ok := arg.Value.(string); ok {
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_Str{
+					Str: wrapperspb.String(v),
+				},
+			}
+		}
+	case strArrT, argsArrT:
+		if v, ok := arg.Value.([]string); ok {
+			strArray := make([]*wrappers.StringValue, 0, len(v))
+
+			for _, str := range v {
+				strArray = append(strArray, &wrappers.StringValue{Value: str})
+			}
+
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_StrArray{
+					StrArray: &pb.StringArrayValue{
+						Value: strArray,
+					},
+				},
+			}
+		}
+	case sockAddrT:
+		if v, ok := arg.Value.(map[string]string); ok {
+			sockaddr, err := getSockaddr(v)
+			if err != nil {
+				return nil, err
+			}
+			eventValue = sockaddr
+		}
+	case bytesT:
+		if v, ok := arg.Value.([]byte); ok {
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_Bytes{
+					Bytes: &wrappers.BytesValue{
+						Value: v,
+					},
+				},
+			}
+		}
+	case intArr2T:
+		if v, ok := arg.Value.([2]int32); ok {
+			intArray := make([]*wrappers.Int32Value, 0, len(v))
+
+			for _, i := range v {
+				intArray = append(intArray, &wrappers.Int32Value{Value: i})
+			}
+
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_Int32Array{
+					Int32Array: &pb.Int32ArrayValue{
+						Value: intArray,
+					},
+				},
+			}
+		}
+	case credT:
+		if v, ok := arg.Value.(trace.SlimCred); ok {
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_Cred{
+					Cred: &pb.CredValue{
+						Uid:            wrapperspb.UInt32(v.Uid),
+						Gid:            wrapperspb.UInt32(v.Gid),
+						Suid:           wrapperspb.UInt32(v.Suid),
+						Sgid:           wrapperspb.UInt32(v.Sgid),
+						Euid:           wrapperspb.UInt32(v.Euid),
+						Egid:           wrapperspb.UInt32(v.Egid),
+						Fsuid:          wrapperspb.UInt32(v.Fsuid),
+						Fsgid:          wrapperspb.UInt32(v.Fsgid),
+						UserNamespace:  wrapperspb.UInt32(v.UserNamespace),
+						SecureBits:     wrapperspb.UInt32(v.SecureBits),
+						CapInheritable: getCaps(v.CapInheritable),
+						CapPermitted:   getCaps(v.CapPermitted),
+						CapEffective:   getCaps(v.CapEffective),
+						CapBounding:    getCaps(v.CapBounding),
+						CapAmbient:     getCaps(v.CapAmbient),
+					},
+				},
+			}
+		}
+	case uint64ArrT:
+		if v, ok := arg.Value.([]uint64); ok {
+			uintArray := make([]*wrappers.UInt64Value, 0, len(v))
+
+			for _, i := range v {
+				uintArray = append(uintArray, &wrappers.UInt64Value{Value: i})
+			}
+
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_UInt64Array{
+					UInt64Array: &pb.UInt64ArrayValue{
+						Value: uintArray,
+					},
+				},
+			}
+		}
+	case timespecT:
+		if v, ok := arg.Value.(float64); ok {
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_Timespec{
+					Timespec: &pb.TimespecValue{
+						Value: wrapperspb.Double(v),
+					},
+				},
+			}
+		}
+	case pointerT:
+		if v, ok := arg.Value.(uintptr); ok {
+			eventValue = &pb.EventValue{
+				Value: &pb.EventValue_UInt64{
+					UInt64: wrapperspb.UInt64(uint64(v)),
+				},
+			}
+		}
+	default:
+		return nil, errfmt.Errorf("unknown arg type: %v", arg.Type)
+	}
+
+	if eventValue == nil {
+		return nil, errfmt.Errorf("can't convert event data: %v - %T", arg.Value, arg.Value)
+	}
+
+	return eventValue, nil
+}
+
+func getCaps(c uint64) []pb.Capability {
+	if c == 0 {
+		return nil
+	}
+
+	caps := make([]pb.Capability, 0)
+
+	for i := uint64(0); i < 64; i++ {
+		if (1<<i)&c != 0 {
+			e := pb.Capability(i)
+			caps = append(caps, e)
+		}
+	}
+
+	return caps
+}
+
+func getSockaddr(v map[string]string) (*pb.EventValue, error) {
+	var sockaddr *pb.SockAddrValue
+	switch v["sa_family"] {
+	case "AF_INET":
+		sinport, err := strconv.ParseUint(v["sin_port"], 10, 32)
+		if err != nil {
+			return nil, err
+		}
+
+		sockaddr = &pb.SockAddrValue{
+			SaFamily: pb.SaFamilyT_AF_INET,
+			SinPort:  uint32(sinport),
+			SinAddr:  v["sin_addr"],
+		}
+	case "AF_UNIX":
+		sockaddr = &pb.SockAddrValue{
+			SaFamily: pb.SaFamilyT_AF_UNIX,
+			SunPath:  v["sun_path"],
+		}
+	case "AF_INET6":
+		sinport, err := strconv.ParseUint(v["sin6_port"], 10, 32)
+		if err != nil {
+			return nil, err
+		}
+
+		sin6Flowinfo, err := strconv.ParseUint(v["sin6_flowinfo"], 10, 32)
+		if err != nil {
+			return nil, err
+		}
+
+		sin6Scopeid, err := strconv.ParseUint(v["sin6_scopeid"], 10, 32)
+		if err != nil {
+			return nil, err
+		}
+
+		sockaddr = &pb.SockAddrValue{
+			SaFamily:     pb.SaFamilyT_AF_INET6,
+			Sin6Port:     uint32(sinport),
+			Sin6Flowinfo: uint32(sin6Flowinfo),
+			Sin6Scopeid:  uint32(sin6Scopeid),
+			Sin6Addr:     v["sin6_addr"],
+		}
+	}
+
+	return &pb.EventValue{
+		Value: &pb.EventValue_Sockaddr{
+			Sockaddr: sockaddr,
+		},
+	}, nil
+}
--- pkg/server/grpc/tracee_test.go
+++ pkg/server/grpc/tracee_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 	"time"
 
-	"gotest.tools/assert"
+	"github.com/stretchr/testify/assert"
 
 	pb "github.com/aquasecurity/tracee/types/api/v1beta1"
 	"github.com/aquasecurity/tracee/types/trace"
@@ -34,7 +34,8 @@ func Test_convertEventWithProcessContext(t *testing.T) {
 		ParentEntityId:      11,
 	}
 
-	protoEvent := convertTraceeEventToProto(traceEvent)
+	protoEvent, err := convertTraceeEventToProto(traceEvent)
+	assert.NoError(t, err)
 
 	assert.Equal(t, uint32(1), protoEvent.Context.Process.NamespacedPid.Value)
 	assert.Equal(t, uint32(2), protoEvent.Context.Process.Thread.NamespacedTid.Value)
@@ -48,7 +49,7 @@ func Test_convertEventWithProcessContext(t *testing.T) {
 	assert.Equal(t, uint32(10), protoEvent.Context.Process.EntityId.Value)
 	assert.Equal(t, uint32(11), protoEvent.Context.Process.Parent.EntityId.Value)
 	assert.Equal(t, "eventTest", protoEvent.Name)
-	assert.DeepEqual(t, []string{"policyTest"}, protoEvent.Policies.Matched)
+	assert.Equal(t, []string{"policyTest"}, protoEvent.Policies.Matched)
 	assert.Equal(t, "processTest", protoEvent.Context.Process.Thread.Name)
 	assert.Equal(t, "syscall", protoEvent.Context.Process.Thread.Syscall)
 	assert.Equal(t, true, protoEvent.Context.Process.Thread.Compat)
@@ -59,7 +60,8 @@ func Test_convertEventWithStackaddresses(t *testing.T) {
 		StackAddresses: []uint64{1, 2, 3},
 	}
 
-	protoEvent := convertTraceeEventToProto(traceEvent)
+	protoEvent, err := convertTraceeEventToProto(traceEvent)
+	assert.NoError(t, err)
 
 	expected := []*pb.StackAddress{
 		{Address: 1},
@@ -68,11 +70,7 @@ func Test_convertEventWithStackaddresses(t *testing.T) {
 	}
 
 	for i := range expected {
-		assert.DeepEqual(
-			t,
-			expected[i].Address,
-			protoEvent.Context.Process.Thread.UserStackTrace.Addresses[i].Address,
-		)
+		assert.Equal(t, expected[i].Address, protoEvent.Context.Process.Thread.UserStackTrace.Addresses[i].Address)
 	}
 }
 
@@ -86,12 +84,13 @@ func Test_convertEventWithContainerContext(t *testing.T) {
 		},
 	}
 
-	protoEvent := convertTraceeEventToProto(traceEvent)
+	protoEvent, err := convertTraceeEventToProto(traceEvent)
+	assert.NoError(t, err)
 
 	assert.Equal(t, "containerID", protoEvent.Context.Container.Id)
 	assert.Equal(t, "containerName", protoEvent.Context.Container.Name)
 	assert.Equal(t, "imageName", protoEvent.Context.Container.Image.Name)
-	assert.DeepEqual(t, []string{"imageDigest"}, protoEvent.Context.Container.Image.RepoDigests)
+	assert.Equal(t, []string{"imageDigest"}, protoEvent.Context.Container.Image.RepoDigests)
 }
 
 func Test_convertEventWithK8sContext(t *testing.T) {
@@ -103,7 +102,8 @@ func Test_convertEventWithK8sContext(t *testing.T) {
 		},
 	}
 
-	protoEvent := convertTraceeEventToProto(traceEvent)
+	protoEvent, err := convertTraceeEventToProto(traceEvent)
+	assert.NoError(t, err)
 
 	assert.Equal(t, "podName", protoEvent.Context.K8S.Pod.Name)
 	assert.Equal(t, "podNamespace", protoEvent.Context.K8S.Namespace.Name)
@@ -118,17 +118,16 @@ func Test_convertEventWithThreat(t *testing.T) {
 				"Severity":    2,
 				"Category":    "privilege-escalation",
 				"Technique":   "Exploitation for Privilege Escalation",
-				"id":          "attack-pattern--b21c3b2d",
 				"external_id": "T1068",
 			},
 		},
 	}
 
-	protoEvent := convertTraceeEventToProto(traceEvent)
+	protoEvent, err := convertTraceeEventToProto(traceEvent)
+	assert.NoError(t, err)
 
 	assert.Equal(t, "An attempt to abuse the Docker UNIX ..", protoEvent.Threat.Description)
-	assert.Equal(t, "privilege-escalation", protoEvent.Threat.MitreTactic.Name)
-	assert.Equal(t, "Exploitation for Privilege Escalation", protoEvent.Threat.MitreTechnique.Name)
-	assert.Equal(t, "attack-pattern--b21c3b2d", protoEvent.Threat.MitreTechnique.Id)
-	assert.Equal(t, "T1068", protoEvent.Threat.MitreTechnique.ExternalId)
+	assert.Equal(t, "privilege-escalation", protoEvent.Threat.Mitre.Tactic.Name)
+	assert.Equal(t, "Exploitation for Privilege Escalation", protoEvent.Threat.Mitre.Technique.Name)
+	assert.Equal(t, "T1068", protoEvent.Threat.Mitre.Technique.Id)
 }
