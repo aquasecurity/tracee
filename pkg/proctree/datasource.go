628d4269d6004563e038ce611049038e79c3e4a9
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2023-10-03 15:23:16 -0300

    chore(proctree): documentation and style changes

--- pkg/proctree/datasource.go
+++ pkg/proctree/datasource.go
@@ -9,7 +9,6 @@ import (
 )
 
 // DataSource is an implementation to detect.Datasource interface, enveloping the ProcessTree type.
-// It exposes all the relevant information of the tree using the interface methods.
 type DataSource struct {
 	procTree *ProcessTree
 }
@@ -18,6 +17,45 @@ func NewDataSource(processTree *ProcessTree) *DataSource {
 	return &DataSource{procTree: processTree}
 }
 
+// Keys returns a list of supported keys by the DataSource.
+func (ptds *DataSource) Keys() []string {
+	return []string{"datasource.ProcKey", "datasource.ThreadKey", "datasource.LineageKey"}
+}
+
+// Schema returns the schema of the DataSource.
+func (ptds *DataSource) Schema() string {
+	schemaMap := map[string]string{
+		"process_info":    "datasource.ProcessInfo",
+		"thread_info":     "datasource.ThreadInfo",
+		"process_lineage": "datasource.ProcessLineage",
+	}
+	schema, _ := json.Marshal(schemaMap)
+	return string(schema)
+}
+
+// Version returns the version of the DataSource.
+func (ptds *DataSource) Version() uint {
+	return 1 // TODO: Change to semantic versioning
+}
+
+// Namespace returns the namespace of the DataSource.
+func (ptds *DataSource) Namespace() string {
+	return "tracee"
+}
+
+// ID returns the ID of the DataSource.
+func (ptds *DataSource) ID() string {
+	return "process_tree"
+}
+
+// Get retrieves information from DataSource based on the provided key.
+// It supports keys of the following types:
+//
+// - datasource.ProcKey (for process information retrieval)
+// - datasource.ThreadKey (for thread information retrieval)
+// - datasource.LineageKey (for process lineage information retrieval)
+//
+// and returns an error if the data isn't found.
 func (ptds *DataSource) Get(key interface{}) (map[string]interface{}, error) {
 	switch typedKey := key.(type) {
 	case datasource.ProcKey:
@@ -44,49 +82,22 @@ func (ptds *DataSource) Get(key interface{}) (map[string]interface{}, error) {
 		return map[string]interface{}{
 			"process_lineage": ptds.exportProcessLineage(process, typedKey.Time, typedKey.MaxDepth),
 		}, nil
-	default:
-		return nil, detect.ErrKeyNotSupported
 	}
+	return nil, detect.ErrKeyNotSupported
 }
 
-func (ptds *DataSource) Keys() []string {
-	return []string{"datasource.ProcKey", "datasource.ThreadKey", "datasource.LineageKey"}
-}
-
-func (ptds *DataSource) Schema() string {
-	schemaMap := map[string]string{
-		"process_info":    "datasource.ProcessInfo",
-		"thread_info":     "datasource.ThreadInfo",
-		"process_lineage": "datasource.ProcessLineage",
-	}
-	schema, _ := json.Marshal(schemaMap)
-	return string(schema)
-}
-
-func (ptds *DataSource) Version() uint {
-	return 1 // TODO: Change to semantic versioning
-}
-
-func (ptds *DataSource) Namespace() string {
-	return "tracee"
-}
-
-func (ptds *DataSource) ID() string {
-	return "process_tree"
-}
-
-// exportProcessInfo return a representation of the given Process information relevant to given time
-// as the expected datasource process structure.
+// exportProcessInfo returns information of the given Process at the given query time.
 func (ptds *DataSource) exportProcessInfo(
-	process *Process,
-	queryTime time.Time,
+	process *Process, queryTime time.Time,
 ) datasource.ProcessInfo {
+	// Pick the objects related to the process from the process tree.
 	info := process.GetInfo()
 	executable := process.GetExecutable()
 	interpreter := process.GetInterpreter()
 	interp := process.GetInterp()
 
-	existingChildren := make(map[int]uint32)
+	// Walk children hashes and discover the ones alive at the query time.
+	aliveChildren := make(map[int]uint32)
 	for _, childHash := range process.GetChildren() {
 		child, ok := ptds.procTree.GetProcessByHash(childHash)
 		if !ok {
@@ -94,11 +105,12 @@ func (ptds *DataSource) exportProcessInfo(
 		}
 		childInfo := child.GetInfo()
 		if childInfo.IsAliveAt(queryTime) {
-			existingChildren[childInfo.GetPid()] = childHash
+			aliveChildren[childInfo.GetPid()] = childHash
 		}
 	}
 
-	existingThreads := make(map[int]uint32)
+	// Walk thread hashes and discover the ones alive at the query time.
+	aliveThreads := make(map[int]uint32)
 	for _, threadHash := range process.GetThreads() {
 		thread, ok := ptds.procTree.GetThreadByHash(threadHash)
 		if !ok {
@@ -106,12 +118,14 @@ func (ptds *DataSource) exportProcessInfo(
 		}
 		threadInfo := thread.GetInfo()
 		if threadInfo.IsAliveAt(queryTime) {
-			existingThreads[threadInfo.GetPid()] = threadHash
+			aliveThreads[threadInfo.GetPid()] = threadHash
 		}
 	}
 
+	// Pick the process information from the process tree.
 	infoFeed := info.GetFeedAt(queryTime)
 
+	// Export the information as the expected datasource process structure.
 	return datasource.ProcessInfo{
 		EntityId:          process.GetHash(),
 		Pid:               infoFeed.Pid,
@@ -126,20 +140,21 @@ func (ptds *DataSource) exportProcessInfo(
 		ExecTime:          time.Unix(0, 0), // TODO: Add
 		ExitTime:          info.GetExitTime(),
 		ParentEntityId:    process.GetParentHash(),
-		ThreadsIds:        existingThreads,
-		ChildProcessesIds: existingChildren,
+		ThreadsIds:        aliveThreads,
+		ChildProcessesIds: aliveChildren,
 		IsAlive:           info.IsAliveAt(queryTime),
 	}
 }
 
-// exportThreadInfo return a representation of the given Thread information relevant to given time
-// as the expected datasource thread structure.
+// exportThreadInfo returns information of the given Thread at the given query time.
 func (ptds *DataSource) exportThreadInfo(
-	thread *Thread,
-	queryTime time.Time,
+	thread *Thread, queryTime time.Time,
 ) datasource.ThreadInfo {
+	// Pick the objects related to the thread from the process tree.
 	info := thread.GetInfo()
 	infoFeed := info.GetFeedAt(queryTime)
+
+	// Export the information as the expected datasource thread structure.
 	return datasource.ThreadInfo{
 		EntityId:  thread.GetHash(),
 		Tid:       infoFeed.Tid,
@@ -154,39 +169,51 @@ func (ptds *DataSource) exportThreadInfo(
 	}
 }
 
-// exportProcessLineage return a representation of the given Process information and up to a given
-// depth of its ancestors as the expected datasource process lineage structure.
-// The information of each struct is relevant to the fork time of its dependent from it.
+// exportProcessLineage returns the lineage of the given Process at the given query time. Lineage is
+// a slice of process information, starting from the given process and going up to the max depth.
 func (ptds *DataSource) exportProcessLineage(
-	process *Process,
-	queryTime time.Time,
-	maxDepth int,
+	process *Process, queryTime time.Time, maxDepth int,
 ) datasource.ProcessLineage {
-	lineage := datasource.ProcessLineage{
-		ptds.exportProcessInfo(process, queryTime),
-	}
-	currentProcess := process
 	var found bool
-	var iterationQueryTime time.Time
+	var start time.Time
+	var lineage datasource.ProcessLineage
+
+	// Pick the process information from the process tree and add it to the lineage.
+	lineage = append(lineage, ptds.exportProcessInfo(process, queryTime))
+
+	// Walk the process tree up (parents) to the max depth.
+
+	current := process
 	for depth := 0; depth < maxDepth; depth++ {
-		// We don't want to get parents processes which are not part of the container.
-		if currentProcess.GetInfo().GetNsPid() == 1 {
+		// If the current process is "init" stop the walk.
+		if current.GetInfo().GetNsPid() == 1 {
 			break
 		}
-		iterationQueryTime = currentProcess.GetInfo().GetStartTime()
-		currentProcess, found = ptds.procTree.GetProcessByHash(currentProcess.GetParentHash())
+
+		// Save the start time of the current process. The parent information
+		// will be obtained at this time, the time the current process was
+		// created, and not the query time.
+		start = current.GetInfo().GetStartTime()
+
+		// Get the parent process.
+		current, found = ptds.procTree.GetProcessByHash(current.GetParentHash())
 		if !found {
 			break
 		}
-		lineage = append(lineage, ptds.exportProcessInfo(currentProcess, iterationQueryTime))
+
+		// Add the parent process to the lineage.
+		lineage = append(lineage, ptds.exportProcessInfo(current, start))
 	}
+
 	return lineage
 }
 
-// exportFileInfo return a representation of the given FileInfo information relevant to given time
-// as the expected datasource file info structure.
+// exportFileInfo returns information of the given FileInfo at the given query time.
 func exportFileInfo(fileInfo *FileInfo, queryTime time.Time) datasource.FileInfo {
+	// Pick the objects related to the file from the process tree.
 	fileInfoFeed := fileInfo.GetFeedAt(queryTime)
+
+	// Export the information as the expected datasource file structure.
 	return datasource.FileInfo{
 		Path:   fileInfoFeed.Path,
 		Hash:   "", // TODO: Add
