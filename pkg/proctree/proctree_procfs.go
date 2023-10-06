1283c628dbfc5ce1108f39ae3994c34bd553519d
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2023-10-06 10:31:41 -0300

    fix(proctree): fix issues when updating from procfs
    - raise channel buffer due to initialization needs (all pids)
    - check if fields from stat and status are valid
    - take care of PIDs 0 and 1 in the right manner
    - only update nodes if they are incomplete in basic fields

--- pkg/proctree/proctree_procfs.go
+++ pkg/proctree/proctree_procfs.go
@@ -41,7 +41,7 @@ func (pt *ProcessTree) feedFromProcFSLoop() {
 func (pt *ProcessTree) FeedFromProcFSAsync(givenPid int) {
 	if pt.procfsChan == nil {
 		logger.Debugw("starting procfs proctree loop") // will tell if called more than once
-		pt.procfsChan = make(chan int, 100)
+		pt.procfsChan = make(chan int, 1000)
 		pt.feedFromProcFSLoop()
 	}
 	if pt.procfsOnce == nil {
@@ -64,7 +64,7 @@ func (pt *ProcessTree) FeedFromProcFS(givenPid int) error {
 	procDir := "/proc"
 
 	// If a PID is given, only deal with that process
-	if givenPid > 0 {
+	if givenPid != AllPIDs {
 		return dealWithProcFsEntry(pt, givenPid)
 	}
 
@@ -129,31 +129,59 @@ func dealWithProc(pt *ProcessTree, givenPid int) error {
 	if status.GetPid() != status.GetTgid() {
 		return errfmt.Errorf("invalid process") // sanity check (process, not thread)
 	}
-	if stat.StartTime == 0 {
-		return errfmt.Errorf("invalid start time")
+
+	name := status.GetName()
+	pid := status.GetPid()   // status: pid == tid
+	tgid := status.GetTgid() // status: tgid == pid
+	ppid := status.GetPPid()
+	nspid := status.GetNsPid()
+	nstgid := status.GetNsTgid()
+	nsppid := status.GetNsPPid()
+	start := stat.StartTime
+
+	// sanity checks
+	switch givenPid {
+	case 0, 1: // PID 0 and 1 are special
+	default:
+		if name == "" || pid == 0 || tgid == 0 || ppid == 0 {
+			return errfmt.Errorf("invalid process")
+		}
 	}
 
-	// given process (pid) hash
-	startTimeNs := utils.ClockTicksToNsSinceBootTime(stat.StartTime)
-	hash := utils.HashTaskID(uint32(status.GetPid()), startTimeNs)
+	// process hash
+	startTimeNs := utils.ClockTicksToNsSinceBootTime(start)
+	hash := utils.HashTaskID(uint32(pid), startTimeNs)
 
 	// update tree for the given process
 	process := pt.GetOrCreateProcessByHash(hash)
 	procInfo := process.GetInfo()
-	procInfo.SetFeed(
+
+	// check if the process info was already set (proctree might miss ppid and name)
+	switch givenPid {
+	case 0, 1: // PID 0 and 1 are special
+	default:
+		if procInfo.GetName() != "" && procInfo.GetPPid() != 0 {
+			return nil
+		}
+	}
+
+	procInfo.SetFeedAt(
 		TaskInfoFeed{
-			Name:        status.GetName(),
-			Tid:         int(status.GetPid()),    // status: pid == tid
-			Pid:         int(status.GetTgid()),   // status: tgid == pid
-			PPid:        int(status.GetPPid()),   // status: ppid == ppid
-			NsTid:       int(status.GetNsPid()),  // status: nspid == nspid
-			NsPid:       int(status.GetNsTgid()), // status: nstgid == nspid
-			NsPPid:      int(status.GetNsPPid()), // status: nsppid == nsppid
-			Uid:         -1,                      // do not change the parent UID
-			Gid:         -1,                      // do not change the parent GID
+			Name:        name,   // command name (add "procfs+" to debug if needed)
+			Tid:         pid,    // status: pid == tid
+			Pid:         tgid,   // status: tgid == pid
+			PPid:        ppid,   // status: ppid == ppid
+			NsTid:       nspid,  // status: nspid == nspid
+			NsPid:       nstgid, // status: nstgid == nspid
+			NsPPid:      nsppid, // status: nsppid == nsppid
+			Uid:         -1,     // do not change the parent uid
+			Gid:         -1,     // do not change the parent gid
 			StartTimeNS: startTimeNs,
 		},
+		utils.NsSinceBootTimeToTime(uint64(start)), // try to be the first changelog entry
 	)
+
+	// TODO: Update executable with information from /proc/<pid>/exe
 
 	// update given process parent (if exists)
 	parent, err := getProcessByPID(pt, status.GetPPid())
@@ -166,55 +194,74 @@ func dealWithProc(pt *ProcessTree, givenPid int) error {
 }
 
 // dealWithThread deals with a thread from procfs.
-func dealWithThread(pt *ProcessTree, pid int, tid int) error {
-	if pid <= 0 {
+func dealWithThread(pt *ProcessTree, givenPid int, givenTid int) error {
+	if givenPid <= 0 {
 		return errfmt.Errorf("invalid PID")
 	}
-	status, err := proc.NewThreadProcStatus(pid, tid)
+	status, err := proc.NewThreadProcStatus(givenPid, givenTid)
 	if err != nil {
 		return errfmt.Errorf("%v", err)
 	}
-	stat, err := proc.NewThreadProcStat(pid, tid)
+	stat, err := proc.NewThreadProcStat(givenPid, givenTid)
 	if err != nil {
 		return errfmt.Errorf("%v", err)
 	}
 
-	// thread
+	name := status.GetName()
+	pid := status.GetPid()   // status: pid == tid
+	tgid := status.GetTgid() // status: tgid == pid
+	ppid := status.GetPPid()
+	nspid := status.GetNsTgid()
+	nstgid := status.GetNsTgid()
+	nsppid := status.GetNsPPid()
+	start := stat.StartTime
 
-	threadTid := status.GetPid()   // status: pid == tid
-	threadPid := status.GetTgid()  // status: tgid == pid
-	threadPpid := status.GetPPid() // status: ppid == ppid
+	// sanity checks
+	if name == "" || pid == 0 || tgid == 0 || ppid == 0 {
+		return errfmt.Errorf("invalid thread")
+	}
 
-	threadStartTimeNs := utils.ClockTicksToNsSinceBootTime(stat.StartTime)
-	threadHash := utils.HashTaskID(uint32(threadTid), threadStartTimeNs)
-	thread := pt.GetOrCreateThreadByHash(threadHash)
+	// thread hash
+	startTimeNs := utils.ClockTicksToNsSinceBootTime(start)
+	hash := utils.HashTaskID(uint32(pid), startTimeNs)
+
+	// update tree for the given thread
+	thread := pt.GetOrCreateThreadByHash(hash)
 	threadInfo := thread.GetInfo()
-	threadInfo.SetFeed(
+
+	// check if the thread info was already set (proctree might miss ppid and name)
+	if threadInfo.GetName() != "" && threadInfo.GetPPid() != 0 {
+		return nil
+	}
+
+	threadInfo.SetFeedAt(
 		TaskInfoFeed{
-			Tid:         int(status.GetPid()),    // status: pid == tid
-			Pid:         int(status.GetTgid()),   // status: tgid == pid
-			PPid:        int(status.GetPPid()),   // status: ppid == ppid
-			NsTid:       int(status.GetNsPid()),  // status: nspid == nspid
-			NsPid:       int(status.GetNsTgid()), // status: nstgid == nspid
-			NsPPid:      int(status.GetNsPPid()), // status: nsppid == nsppid
-			Uid:         -1,                      // do not change the parent UID
-			Gid:         -1,                      // do not change the parent GID
-			StartTimeNS: threadStartTimeNs,
+			Name:        name,   // command name (add "procfs+" to debug if needed)
+			Tid:         pid,    // status: pid == tid
+			Pid:         tgid,   // status: tgid == pid
+			PPid:        ppid,   // status: ppid == ppid
+			NsTid:       nspid,  // status: nspid == nspid
+			NsPid:       nstgid, // status: nstgid == nspid
+			NsPPid:      nsppid, // status: nsppid == nsppid
+			Uid:         -1,     // do not change the parent uid
+			Gid:         -1,     // do not change the parent gid
+			StartTimeNS: startTimeNs,
 		},
+		utils.NsSinceBootTimeToTime(uint64(start)), // try to be the first changelog entry
 	)
 
 	// thread group leader (leader tid is the same as the thread's pid, so we can find it)
 
-	leader, err := getProcessByPID(pt, threadPid)
+	leader, err := getProcessByPID(pt, tgid)
 	if err == nil {
-		leader.AddThread(threadHash) // threads associated with the leader (not parent)
+		leader.AddThread(hash) // threads associated with the leader (not parent)
 		leaderHash := leader.GetHash()
 		thread.SetLeaderHash(leaderHash) // same
 	}
 
 	// parent (real process, parent of all threads)
 
-	parent, err := getProcessByPID(pt, threadPpid)
+	parent, err := getProcessByPID(pt, ppid)
 	if err == nil {
 		thread.SetParentHash(parent.GetHash()) // all threads have the same parent
 	}
