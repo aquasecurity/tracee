23db937cf1364f6da48265b1a6869a5f98ca72de
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2023-10-31 00:22:29 -0300

    chore(controller): minor changes to controller
    - better function descriptions
    - small makefile fix
    - minor positional changes

--- cmd/tracee-operator/main.go
+++ cmd/tracee-operator/main.go
@@ -27,42 +27,29 @@ func init() {
 	utilruntime.Must(policyv1beta1.AddToScheme(scheme))
 }
 
+type Config struct {
+	MetricsAddr          string
+	ProbeAddr            string
+	TraceeNamespace      string
+	TraceeName           string
+	EnableLeaderElection bool
+	LoggingOpts          zap.Options
+}
+
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
-	var traceeNamespace string
-	var traceeName string
+	config := parseConfig()
 
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.StringVar(&traceeNamespace, "tracee-namespace", "tracee-system", "The namespace where Tracee is installed.")
-	flag.StringVar(&traceeName, "tracee-name", "tracee", "The name of the Tracee DaemonSet.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	opts := zap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
-	flag.Parse()
+	// Set up a logger for the controller manager (prefix: "setup").
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&config.LoggingOpts)))
 
-	// if there are env vars set, use them instead of the flags
-	if namespace := os.Getenv("TRACEE_NAMESPACE"); namespace != "" {
-		traceeNamespace = namespace
-	}
-
-	if name := os.Getenv("TRACEE_NAME"); name != "" {
-		traceeName = name
-	}
-
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	// Create a controller manager that will take care of caching, syncing, and making
+	// calls to the API server.
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
-		Metrics:                metricsserver.Options{BindAddress: metricsAddr},
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
+		Metrics:                metricsserver.Options{BindAddress: config.MetricsAddr},
+		HealthProbeBindAddress: config.ProbeAddr,
+		LeaderElection:         config.EnableLeaderElection,
 		LeaderElectionID:       "ecaf1259.my.domain",
 	})
 	if err != nil {
@@ -70,15 +57,21 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&controller.PolicyReconciler{
+	// Register the PolicyReconciler with the controller manager. This will cause the
+	// PolicyReconciler to watch for changes to Policy objects and deal with them.
+
+	reconciler := &controller.PolicyReconciler{
 		Client:          mgr.GetClient(),
 		Scheme:          mgr.GetScheme(),
-		TraceeNamespace: traceeNamespace,
-		TraceeName:      traceeName,
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "CronJob")
+		TraceeNamespace: config.TraceeNamespace,
+		TraceeName:      config.TraceeName,
+	}
+	if err := reconciler.SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "PolicyReconciler")
 		os.Exit(1)
 	}
+
+	// Create health and readyz endpoints for the controller manager to report its health.
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
@@ -89,9 +82,38 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Start the controller manager. This will block until the controller manager is
+	// stopped or until a fatal error occurs.
+
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
+}
+
+// parseConfig parses the command line flags and returns a Config struct
+func parseConfig() Config {
+	var cfg Config
+
+	flag.StringVar(&cfg.MetricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&cfg.ProbeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.StringVar(&cfg.TraceeNamespace, "tracee-namespace", "tracee-system", "The namespace where Tracee is installed.")
+	flag.StringVar(&cfg.TraceeName, "tracee-name", "tracee", "The name of the Tracee DaemonSet.")
+	flag.BoolVar(&cfg.EnableLeaderElection, "leader-elect", false, "Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+	cfg.LoggingOpts = zap.Options{
+		Development: true,
+	}
+	cfg.LoggingOpts.BindFlags(flag.CommandLine)
+
+	flag.Parse()
+
+	if namespace := os.Getenv("TRACEE_NAMESPACE"); namespace != "" {
+		cfg.TraceeNamespace = namespace
+	}
+	if name := os.Getenv("TRACEE_NAME"); name != "" {
+		cfg.TraceeName = name
+	}
+
+	return cfg
 }
