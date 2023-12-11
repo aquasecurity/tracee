db152b1df8749f781578970e4780bd751f0768a7
Nadav Strahilevitz <nadav.strahilevitz@aquasec.com>
2023-12-11 16:17:21 +0000

    feat(filehash): optimize hash by reusing buffer
    Previous implementation of sha256 hash computation used io.Copy.
    This method would allocate small buffers as needed when copying the
    file content into the hash digest. The result would be more allocations
    and more GC calls. Mitigate this by using one large buffer and clearing
    it per call.
    
    Benchmark results:
    goos: linux
    goarch: amd64
    pkg: github.com/aquasecurity/tracee/pkg/exechash
    cpu: AMD EPYC 7571
    BenchmarkComputeFileHashOld-8       	   18355	     65371 ns/op	   33056 B/op	       5 allocs/op
    BenchmarkComputeFileHashCurrent-8   	   30561	     41714 ns/op	     272 B/op	       4 allocs/op

--- pkg/filehash/hash.go
+++ pkg/filehash/hash.go
@@ -1,9 +1,11 @@
 package filehash
 
 import (
+	"bytes"
 	"encoding/hex"
 	"io"
 	"os"
+	"sync"
 
 	miniosha "github.com/minio/sha256-simd"
 
@@ -26,9 +28,23 @@ func ComputeFileHashAtPath(path string) (string, error) {
 	return ComputeFileHash(f)
 }
 
+// Preallocate a copy buffer for use between hash computations.
+// This reduces allocations and garbage collection which would happen
+// from using io.Copy.
+var (
+	hashBuffer        = make([]byte, 1024*32)
+	hashBufferWrapper = bytes.NewBuffer(hashBuffer)
+	bufferMutex       = new(sync.Mutex) // add a mutex to prevent concurrent calls deleting the buffer
+)
+
+// ComputeFileHashAtPath attempts to calculate the sha256 hash of a file
+// (the file must already be opened).
 func ComputeFileHash(file *os.File) (string, error) {
+	bufferMutex.Lock()
+	defer bufferMutex.Unlock()
+	hashBufferWrapper.Reset()
 	h := miniosha.New()
-	_, err := io.Copy(h, file)
+	_, err := io.CopyBuffer(h, file, hashBuffer)
 	if err != nil {
 		return "", errfmt.WrapError(err)
 	}
