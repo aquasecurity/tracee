68a961a5dd6c7b3f365caf507ebba82a8993f4a1
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2024-01-18 03:03:08 -0300

    chore(tests): fix intermittent issues with bpf_attach e2e test

--- tests/e2e-inst-signatures/scripts/bpf_attach.sh
+++ tests/e2e-inst-signatures/scripts/bpf_attach.sh
@@ -4,7 +4,7 @@ TRACEE_STARTUP_TIMEOUT=60
 TRACEE_SHUTDOWN_TIMEOUT=60
 TRACEE_RUN_TIMEOUT=5
 
-TRACEE_TMP_DIR=/tmp/tracee
+TRACEE_TMP_DIR=/tmp/bpf_attach
 
 info_exit() {
     echo -n "INFO: "
@@ -19,11 +19,14 @@ info() {
 
 # run tracee with a single event (to trigger the other instance)
 
-coproc ./dist/tracee \
+rm -f $TRACEE_TMP_DIR/tracee.pid
+
+./dist/tracee \
     --install-path $TRACEE_TMP_DIR \
+    --output none \
     --events security_file_open &
 
-pid=$COPROC_PID
+pid=$?
 
 # wait tracee to be started + 5 seconds
 
@@ -34,7 +37,7 @@ while true; do
     times=$((times + 1))
     sleep 1
 
-    if [[ -f $TRACEE_TMP_DIR/out/tracee.pid ]]; then
+    if [[ -f $TRACEE_TMP_DIR/tracee.pid ]]; then
         info "bpf_attach test tracee instance started"
         break
     fi
