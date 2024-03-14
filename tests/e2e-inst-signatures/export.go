7a942315a7dff63a7a0a16fdf991d13e73cc1ffd
Ofek Shaked <ofek.shaked@aquasec.com>
2024-03-14 13:57:38 +0200

    Added test for set_fs_pwd

--- tests/e2e-inst-signatures/export.go
+++ tests/e2e-inst-signatures/export.go
@@ -18,6 +18,7 @@ var ExportedSignatures = []detect.Signature{
 	&e2eDnsDataSource{},
 	&e2eWritableDatasourceSig{},
 	&e2eSecurityPathNotify{},
+	&e2eSetFsPwd{},
 }
 
 var ExportedDataSources = []detect.DataSource{
