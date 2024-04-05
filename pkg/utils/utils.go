faef01c64c45ea3985285ed16be9e0753edfcc56
Geyslan Gregório <geyslan@gmail.com>
2024-04-05 15:29:18 -0300

    chore: introduce Iterator generic interface
    The Iterator pattern can be used to traverse a collection of elements
    without exposing its underlying representation.
    
    Signed-off-by: Geyslan Gregório <geyslan@gmail.com>

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -13,6 +13,15 @@ type Cloner interface {
 	Clone() Cloner
 }
 
+// Iterator is a generic interface for iterators.
+type Iterator[T any] interface {
+	// HasNext returns true if there are more elements to iterate.
+	HasNext() bool
+
+	// Next returns the next element in the iteration.
+	Next() T
+}
+
 func ParseSymbol(address uint64, table *helpers.KernelSymbolTable) helpers.KernelSymbol {
 	var hookingFunction helpers.KernelSymbol
 
