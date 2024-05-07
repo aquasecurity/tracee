cf391b48cc4d3ba8cdf1cdb82d1b11209b5cef33
Nadav Strahilevitz <nadav.strahilevitz@aquasec.com>
2024-05-07 15:48:44 +0300

    fix: network event context (#4027)
    Commit f806cb4b changed context initialization to occur in the submit
    stage. However, network events do not have access to that context during
    their submit stage (since they happen in a cgroup_skb program).
    
    Therefore, add a call to init_task_context in the pre cgroup bpf program
    (cgroup_bpf_run_filter_skb).
    
    In addition add a validation in the e2e tests so that task context
    initialization in network events doesn't break.

--- tests/e2e-net-signatures/e2e-icmpv6.go
+++ tests/e2e-net-signatures/e2e-icmpv6.go
@@ -43,6 +43,11 @@ func (sig *e2eICMPv6) OnEvent(event protocol.Event) error {
 
 	switch eventObj.EventName {
 	case "net_packet_icmpv6":
+		// validate tast context
+		if eventObj.HostName == "" {
+			return nil
+		}
+
 		src, err := helpers.GetTraceeStringArgumentByName(eventObj, "src")
 		if err != nil {
 			return err
--- tests/e2e-net-signatures/e2e-ipv4.go
+++ tests/e2e-net-signatures/e2e-ipv4.go
@@ -43,6 +43,11 @@ func (sig *e2eIPv4) OnEvent(event protocol.Event) error {
 
 	switch eventObj.EventName {
 	case "net_packet_ipv4":
+		// validate tast context
+		if eventObj.HostName == "" {
+			return nil
+		}
+
 		src, err := helpers.GetTraceeStringArgumentByName(eventObj, "src")
 		if err != nil {
 			return err
