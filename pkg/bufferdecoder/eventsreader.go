773e747142962255a7d8db75a224543be8d5eaec
Geyslan Gregório <geyslan@gmail.com>
2024-07-18 16:29:26 -0300

    fix: improve performance of readStringVarFromBuff
    Optimize readStringVarFromBuff for better performance and memory usage.
    
    - The optimizations were tested using new benchmarks:
      - Short: 33.26% faster, 42.86% less memory usage, 1 fewer allocation.
      - Medium: 28.34% faster, 54.55% less memory usage, 1 fewer allocation.
      - Long: 26.90% faster, 73.42% less memory usage, 3 fewer allocations.
      - Long: with Low Max: 19.12% faster, 48.15% less memory usage, 1 fewer
        allocation.
    
    The overall improvements show significant gains in both execution speed
    and memory efficiency. For more check eventsreader_bench_test.go.
    
    Changes:
    - Preallocated the buffer with a reasonable initial capacity without
      initializing data.
    - Removed TrimLeft call since conversion logic already stops at the
      first nul byte decoded.

--- pkg/bufferdecoder/eventsreader.go
+++ pkg/bufferdecoder/eventsreader.go
@@ -1,7 +1,6 @@
 package bufferdecoder
 
 import (
-	"bytes"
 	"encoding/binary"
 	"net"
 	"strconv"
@@ -373,21 +372,29 @@ func readStringFromBuff(ebpfMsgDecoder *EbpfDecoder) (string, error) {
 func readStringVarFromBuff(decoder *EbpfDecoder, max int) (string, error) {
 	var err error
 	var char int8
-	res := make([]byte, max)
+	res := make([]byte, 0, max)
+
 	err = decoder.DecodeInt8(&char)
 	if err != nil {
 		return "", errfmt.Errorf("error reading null terminated string: %v", err)
 	}
-	var count int
-	for count = 1; char != 0 && count < max; count++ {
+
+	count := 1 // first char is already decoded
+	for char != 0 && count < max {
 		res = append(res, byte(char))
+
+		// decode next char
 		err = decoder.DecodeInt8(&char)
 		if err != nil {
 			return "", errfmt.Errorf("error reading null terminated string: %v", err)
 		}
+		count++
 	}
-	res = bytes.TrimLeft(res[:], "\000")
-	decoder.cursor += max - count // move cursor to end of buffer
+
+	// The exact reason for this Trim is not known, so remove it for now,
+	// since it increases processing time.
+	// res = bytes.TrimLeft(res[:], "\000")
+	decoder.cursor += max - count // move cursor to the end of the buffer
 	return string(res), nil
 }
 
