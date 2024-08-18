72bc4723eaa52b5b3dd6c4c4861945fdfc6f5a53
Yaniv Agman <yanivagman@gmail.com>
2024-08-18 18:10:05 +0300

    feat: Add syscall kprobe support
    We currently use sys_enter/sys_exit tracepoints to trace syscalls.
    These impact performance on hot paths.
    
    This change introduces BPF program attachment to syscall entry/exit
    using kprobes, offering a more efficient mechanism. It handles
    varying symbol prefixes across architectures and includes 32-bit
    compat support.

---
+++ pkg/ebpf/probes/arch_arm64.go
@@ -0,0 +1,8 @@
+//go:build arm64
+// +build arm64
+
+package probes
+
+const SyscallPrefix = "__arm64_sys_"
+const SyscallPrefixCompat = "NOT_SUPPORTED"
+const SyscallPrefixCompat2 = "NOT_SUPPORTED"
