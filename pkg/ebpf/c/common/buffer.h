12510045cc82f1816b33f2a6a3143eb96e9c9ddd
Geyslan Gregório <geyslan@gmail.com>
2024-09-27 16:17:00 -0300

    chore: add BPFPerfEventWrites metric (DEBUG)
    Enabled only when the build with DEBUG=1.
    
    BPFPerfEventWrites counts the number of events processed by the eBPF
    programs and written to the perf event buffer.
    
    It is incremented right before the event is written to the perf buffer,
    making it possible to measure even if the event is lost.
    
    This metric can be used to monitor the performance of individual eBPF
    events and to detect potential bottlenecks.

--- pkg/ebpf/c/common/buffer.h
+++ pkg/ebpf/c/common/buffer.h
@@ -458,6 +458,15 @@ statfunc int save_args_to_submit_buf(event_data_t *event, args_t *args)
     return arg_num;
 }
 
+#ifdef DEBUG
+struct event_counts {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, MAX_EVENT_ID);
+    __type(key, u32);   // eventid
+    __type(value, u64); // count
+} event_counts SEC(".maps");
+#endif
+
 statfunc int events_perf_submit(program_data_t *p, long ret)
 {
     p->event->context.retval = ret;
@@ -483,6 +492,13 @@ statfunc int events_perf_submit(program_data_t *p, long ret)
                  "%[size] = %[max_size];\n"
                  :
                  : [size] "r"(size), [max_size] "i"(MAX_EVENT_SIZE));
+
+#ifdef DEBUG
+    // increment event count before event submission attempt
+    u64 *event_count = bpf_map_lookup_elem(&event_counts, &p->event->context.eventid);
+    if (event_count)
+        __sync_fetch_and_add(event_count, 1);
+#endif
 
     return bpf_perf_event_output(p->ctx, &events, BPF_F_CURRENT_CPU, p->event, size);
 }
