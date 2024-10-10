f33e78363d1423a72d4cc842e5e07a9d58e39eaa
Ofek Shaked <ofek.shaked@aquasec.com>
2024-10-10 15:48:37 +0300

    Remove stack addresses logic The stack addresses feature will be removed in the future with the introduction of full stack traces.

--- pkg/ebpf/processor_funcs.go
+++ pkg/ebpf/processor_funcs.go
@@ -469,7 +469,7 @@ func (t *Tracee) removeContext(event *trace.Event) error {
 	event.Container = trace.Container{}
 	event.Kubernetes = trace.Kubernetes{}
 	event.Syscall = ""
-	event.StackAddresses = []uint64{}
+	event.StackAddresses = nil
 	event.ContextFlags = trace.ContextFlags{}
 	event.ThreadEntityId = 0
 	event.ProcessEntityId = 0
