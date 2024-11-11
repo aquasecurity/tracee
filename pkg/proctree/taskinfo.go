d02ce615ca921e23f5c8390c6c850397e29807b5
Geyslan Gregório <geyslan@gmail.com>
2024-11-11 12:56:55 -0300

    chore(changelog): reduce mem footprint
    The current Changelog structure consumes a significant amount of memory
    due to the allocation of metadata for each field/instance. As the number
    of fields increases, the memory usage grows linearly. Approximately 240
    bytes per field were observed just for metadata, excluding the actual
    data and pointers for each field.
    
    To reduce memory consumption, the new changelog.Changelog[T] type was
    created.
    
    Changelog[T] implementation uses a single slice to store changes for
    a single field of type T. This approach is more memory-efficient than
    the previous implementation, since the map is no longer used. Its main
    advantage is achievable by setting T as a complete structure type, which
    gets rid of the need for metadata of each field.
    
    Avoid distributing Changelog[t] across multiple fields, as this would
    negate the purpose of reducing memory usage by increasing the metadata
    associated with each Changelog. However, one might consider using it as
    structure fields in scenarios where a mutable field (with many entries
    and high frequency of change) has a larger size than the sum of the
    Changelog metadata overhead and the size of the outer data structure
    itself.
    
    FileInfo and TaskInfo were modified to use the new Changelog[T] being
    T the respective feed structures.
    
    ---
    
    | Caches | GOGC | Branch | *Heap Use | *Heap   | Diff of | Proctree |
    |        |      |        | (Avg)     | Growth  | main    |          |
    |--------|------|--------|--------- -|------- -|---------|----------|
    | -      | 5    | main   | 18        | -       | -       | off      |
    | 32768  | 5    | main   | 209       | 191     | -       | on       |
    |---------------------------------- --------- ----------------------|
    | -      | 5    | new    | 18        | -       | -       | off      |
    | 32768  | 5    | new    | 96        | 78      | -59.16% | on       |
    
    * in MB
    
    With GOGC set to 5, the new implementation reduces average heap usage by
    approximately 59% when using cache sizes of 32,768.
    
    The "Heap Use" and "Heap Growth" columns serve as a good indicator of
    memory consumption and can assist in determining optimal cache sizes.
    
    ---
    
    The Set method was hugely improved, reducing the number of allocations,
    memory usage, and execution time. The benchmark results are as follows:
    
    Running tool: /home/gg/.goenv/versions/1.22.4/bin/go test -benchmem
    -run=^$ -tags ebpf -bench ^Benchmark_Set$
    github.com/aquasecurity/tracee/pkg/changelog -benchtime=10000000x
    
    goos: linux
    goarch: amd64
    pkg: github.com/aquasecurity/tracee/pkg/changelog
    cpu: AMD Ryzen 9 7950X 16-Core Processor
    
    Benchmark_Set/All_Scenarios-32 10000000  322.8 ns/op  112 B/op  3 allocs/op
    Benchmark_Set/Within_Limit-32  10000000  506.8 ns/op  496 B/op  5 allocs/op
    PASS
    ok  	github.com/aquasecurity/tracee/pkg/changelog	367.236s
    
    | Scenario      | ns/op. (%)   | B/op Reduc. (%)   | Alloc. Reduc. (%) |
    |:--------------|:-------------|:------------------|:------------------|
    | All Scenarios | -75.9%       | -88.7%            | -92.9%            |
    | Within Limit  | -76.2%       | -85.5%            | -91.7%            |

--- pkg/proctree/taskinfo.go
+++ pkg/proctree/taskinfo.go
@@ -4,23 +4,23 @@ import (
 	"sync"
 	"time"
 
-	ch "github.com/aquasecurity/tracee/pkg/changelog"
+	"github.com/aquasecurity/tracee/pkg/changelog"
 	traceetime "github.com/aquasecurity/tracee/pkg/time"
 )
 
 // TaskInfoFeed allows external packages to set/get multiple values of a task at once.
 type TaskInfoFeed struct {
-	Name        string
-	Tid         int
-	Pid         int
-	PPid        int
-	NsTid       int
-	NsPid       int
-	NsPPid      int
-	Uid         int
-	Gid         int
-	StartTimeNS uint64
-	ExitTimeNS  uint64
+	Name        string // mutable (process name can be changed)
+	Tid         int    // immutable
+	Pid         int    // immutable
+	PPid        int    // mutable (process can be reparented)
+	NsTid       int    // immutable
+	NsPid       int    // immutable
+	NsPPid      int    // mutable (process can be reparented)
+	Uid         int    // mutable (process uid can be changed)
+	Gid         int    // mutable (process gid can be changed)
+	StartTimeNS uint64 // immutable (this is a duration, in ns, since boot)
+	ExitTimeNS  uint64 // immutable (this is a duration, in ns, since boot)
 }
 
 //
@@ -29,342 +29,262 @@ type TaskInfoFeed struct {
 
 // TaskInfo represents a task.
 type TaskInfo struct {
-	name        *ch.Changelog[string] // variable (process name can be changed)
-	tid         int                   // immutable
-	pid         int                   // immutable
-	pPid        *ch.Changelog[int]    // variable (process can be reparented)
-	nsTid       int                   // immutable
-	nsPid       int                   // immutable
-	nsPPid      *ch.Changelog[int]    // variable (process can be reparented)
-	uid         *ch.Changelog[int]    // variable (process uid can be changed)
-	gid         *ch.Changelog[int]    // variable (process gid can be changed)
-	startTimeNS uint64                // this is a duration, in ns, since boot (immutable)
-	exitTimeNS  uint64                // this is a duration, in ns, since boot (immutable)
-	mutex       *sync.RWMutex
+	feed  *changelog.Changelog[TaskInfoFeed]
+	mutex *sync.RWMutex
 }
 
 // NewTaskInfo creates a new task.
 func NewTaskInfo() *TaskInfo {
 	return &TaskInfo{
-		name: ch.NewChangelog[string](5),
-		// All the folloowing values changes are currently not monitored by the process tree.
-		// Hence, for now, they will only contain one value in the changelog
-		pPid:   ch.NewChangelog[int](1),
-		nsPPid: ch.NewChangelog[int](1),
-		uid:    ch.NewChangelog[int](1),
-		gid:    ch.NewChangelog[int](1),
-		mutex:  &sync.RWMutex{},
+		feed:  changelog.NewChangelog[TaskInfoFeed](3),
+		mutex: &sync.RWMutex{},
 	}
 }
 
 // NewTaskInfoFromFeed creates a new task with values from the given feed.
-func NewTaskInfoFromFeed(feed TaskInfoFeed) *TaskInfo {
+func NewTaskInfoNewFromFeed(feed TaskInfoFeed) *TaskInfo {
 	new := NewTaskInfo()
-	new.SetFeed(feed)
+	new.setFeed(feed)
+
 	return new
 }
 
-// Feed: Multiple values at once.
+//
+// Setters
+//
 
-// SetFeed sets the values of the task from the given feed.
+// Multiple values at once (using a feed structure)
+
+// SetFeed sets the values of the task from the given feed at the current time.
 func (ti *TaskInfo) SetFeed(feed TaskInfoFeed) {
 	ti.mutex.Lock()
 	defer ti.mutex.Unlock()
-	ti.setFeedAt(feed, time.Now()) // set current values
+
+	ti.setFeed(feed)
 }
 
 // SetFeedAt sets the values of the task from the given feed at the given time.
 func (ti *TaskInfo) SetFeedAt(feed TaskInfoFeed, targetTime time.Time) {
 	ti.mutex.Lock()
 	defer ti.mutex.Unlock()
-	ti.setFeedAt(feed, targetTime) // set values at the given time
-}
 
-func (ti *TaskInfo) setFeedAt(feed TaskInfoFeed, targetTime time.Time) {
+	atFeed := ti.getFeedAt(targetTime)
+
 	if feed.Name != "" {
-		ti.name.Set(feed.Name, targetTime)
+		atFeed.Name = feed.Name
 	}
 	if feed.Tid >= 0 {
-		ti.tid = feed.Tid
+		atFeed.Tid = feed.Tid
 	}
 	if feed.Pid >= 0 {
-		ti.pid = feed.Pid
+		atFeed.Pid = feed.Pid
 	}
 	if feed.PPid >= 0 {
-		ti.pPid.Set(feed.PPid, targetTime)
+		atFeed.PPid = feed.PPid
 	}
 	if feed.NsTid >= 0 {
-		ti.nsTid = feed.NsTid
+		atFeed.NsTid = feed.NsTid
 	}
 	if feed.NsPid >= 0 {
-		ti.nsPid = feed.NsPid
+		atFeed.NsPid = feed.NsPid
 	}
 	if feed.NsPid >= 0 {
-		ti.nsPPid.Set(feed.NsPid, targetTime)
+		atFeed.NsPPid = feed.NsPPid
 	}
 	if feed.Uid >= 0 {
-		ti.uid.Set(feed.Uid, targetTime)
+		atFeed.Uid = feed.Uid
 	}
 	if feed.Gid >= 0 {
-		ti.gid.Set(feed.Gid, targetTime)
+		atFeed.Gid = feed.Gid
 	}
 	if feed.StartTimeNS != 0 {
-		ti.startTimeNS = feed.StartTimeNS
+		atFeed.StartTimeNS = feed.StartTimeNS
 	}
 	if feed.ExitTimeNS != 0 {
-		ti.exitTimeNS = feed.ExitTimeNS
+		atFeed.ExitTimeNS = feed.ExitTimeNS
 	}
+
+	ti.setFeedAt(atFeed, targetTime)
 }
 
-// GetFeed returns the values of the task as a feed.
-func (ti *TaskInfo) GetFeed() TaskInfoFeed {
-	ti.mutex.RLock()
-	defer ti.mutex.RUnlock()
-	return ti.getFeedAt(time.Now()) // return current values
-}
-
-// GetFeedAt returns the values of the task as a feed at the given time.
-func (ti *TaskInfo) GetFeedAt(targetTime time.Time) TaskInfoFeed {
-	ti.mutex.RLock()
-	defer ti.mutex.RUnlock()
-	return ti.getFeedAt(targetTime) // return values at the given time
-}
-
-func (ti *TaskInfo) getFeedAt(targetTime time.Time) TaskInfoFeed {
-	return TaskInfoFeed{
-		Name:        ti.name.Get(targetTime),
-		Tid:         ti.tid,
-		Pid:         ti.pid,
-		PPid:        ti.pPid.Get(targetTime),
-		NsTid:       ti.nsTid,
-		NsPid:       ti.nsPid,
-		NsPPid:      ti.nsPPid.Get(targetTime),
-		Uid:         ti.uid.Get(targetTime),
-		Gid:         ti.gid.Get(targetTime),
-		StartTimeNS: ti.startTimeNS,
-		ExitTimeNS:  ti.exitTimeNS,
-	}
-}
-
-// Setters
-
-// SetName sets the name of the task.
-func (ti *TaskInfo) SetName(name string) {
-	ti.mutex.Lock()
-	defer ti.mutex.Unlock()
-	ti.name.Set(name, time.Now())
-}
+// Single values
 
 // SetNameAt sets the name of the task at the given time.
 func (ti *TaskInfo) SetNameAt(name string, targetTime time.Time) {
 	ti.mutex.Lock()
 	defer ti.mutex.Unlock()
-	ti.name.Set(name, targetTime)
-}
 
-// SetTid sets the tid of the task.
-func (ti *TaskInfo) SetTid(tid int) {
-	ti.mutex.Lock()
-	defer ti.mutex.Unlock()
-	ti.tid = tid
-}
+	feed := ti.getFeedAt(targetTime)
+	feed.Name = name
 
-// SetPid sets the pid of the task.
-func (ti *TaskInfo) SetPid(pid int) {
-	ti.mutex.Lock()
-	defer ti.mutex.Unlock()
-	ti.pid = pid
-}
-
-// SetNsTid sets the nsTid of the task.
-func (ti *TaskInfo) SetNsTid(nsTid int) {
-	ti.mutex.Lock()
-	defer ti.mutex.Unlock()
-	ti.nsTid = nsTid
-}
-
-// SetNsPid sets the nsPid of the task.
-func (ti *TaskInfo) SetNsPid(nsPid int) {
-	ti.mutex.Lock()
-	defer ti.mutex.Unlock()
-	ti.nsPid = nsPid
-}
-
-// SetStartTimeNS sets the startTimeNS of the task.
-func (ti *TaskInfo) SetStartTimeNS(startTimeNS uint64) {
-	ti.mutex.Lock()
-	defer ti.mutex.Unlock()
-	ti.startTimeNS = startTimeNS
+	ti.setFeedAt(feed, targetTime)
 }
 
 // SetExitTime sets the exitTime of the task.
 func (ti *TaskInfo) SetExitTime(exitTime uint64) {
 	ti.mutex.Lock()
 	defer ti.mutex.Unlock()
-	ti.exitTimeNS = exitTime
+
+	exitTimestamp := traceetime.NsSinceEpochToTime(exitTime)
+
+	feed := ti.getFeedAt(exitTimestamp)
+	feed.ExitTimeNS = exitTime
+
+	ti.setFeedAt(feed, exitTimestamp)
 }
 
-// SetPPid sets the ppid of the task.
-func (ti *TaskInfo) SetPPid(pPid int) {
-	ti.mutex.Lock()
-	defer ti.mutex.Unlock()
-	ti.pPid.Set(pPid, time.Now())
+// private setters
+
+func (ti *TaskInfo) setFeed(feed TaskInfoFeed) {
+	ti.setFeedAt(feed, time.Now())
 }
 
-// SetPPidAt sets the ppid of the task at the given time.
-func (ti *TaskInfo) SetPPidAt(pPid int, targetTime time.Time) {
-	ti.mutex.Lock()
-	defer ti.mutex.Unlock()
-	ti.pPid.Set(pPid, targetTime)
+func (ti *TaskInfo) setFeedAt(feed TaskInfoFeed, targetTime time.Time) {
+	ti.feed.Set(feed, targetTime)
 }
 
-// SetNsPPid sets the nsppid of the task.
-func (ti *TaskInfo) SetNsPPid(nsPPid int) {
-	ti.mutex.Lock()
-	defer ti.mutex.Unlock()
-	ti.nsPPid.Set(nsPPid, time.Now())
-}
-
-// SetNsPPidAt sets the nsppid of the task at the given time.
-func (ti *TaskInfo) SetNsPPidAt(nsPPid int, targetTime time.Time) {
-	ti.mutex.Lock()
-	defer ti.mutex.Unlock()
-	ti.nsPPid.Set(nsPPid, targetTime)
-}
-
-// SetUid sets the uid of the task.
-func (ti *TaskInfo) SetUid(uid int) {
-	ti.mutex.Lock()
-	defer ti.mutex.Unlock()
-	ti.uid.Set(uid, time.Now())
-}
-
-// SetUidAt sets the uid of the task at the given time.
-func (ti *TaskInfo) SetUidAt(uid int, targetTime time.Time) {
-	ti.mutex.Lock()
-	defer ti.mutex.Unlock()
-	ti.uid.Set(uid, targetTime)
-}
-
-// SetGid sets the gid of the task.
-func (ti *TaskInfo) SetGid(gid int) {
-	ti.mutex.Lock()
-	defer ti.mutex.Unlock()
-	ti.gid.Set(gid, time.Now())
-}
-
-// SetGidAt sets the gid of the task at the given time.
-func (ti *TaskInfo) SetGidAt(gid int, targetTime time.Time) {
-	ti.mutex.Lock()
-	defer ti.mutex.Unlock()
-	ti.gid.Set(gid, targetTime)
-}
-
+//
 // Getters
+//
+
+// Multiple values at once (getting a feed structure)
+
+// GetFeed returns the values of the task as a feed.
+func (ti *TaskInfo) GetFeed() TaskInfoFeed {
+	ti.mutex.RLock()
+	defer ti.mutex.RUnlock()
+
+	return ti.getFeed()
+}
+
+// GetFeedAt returns the values of the task as a feed at the given time.
+func (ti *TaskInfo) GetFeedAt(targetTime time.Time) TaskInfoFeed {
+	ti.mutex.RLock()
+	defer ti.mutex.RUnlock()
+
+	return ti.getFeedAt(targetTime)
+}
+
+// Single values
 
 // GetName returns the name of the task.
 func (ti *TaskInfo) GetName() string {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.name.GetCurrent()
+
+	return ti.getFeed().Name
 }
 
 // GetNameAt returns the name of the task at the given time.
 func (ti *TaskInfo) GetNameAt(targetTime time.Time) string {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.name.Get(targetTime)
+
+	return ti.getFeedAt(targetTime).Name
 }
 
 // GetTid returns the tid of the task.
 func (ti *TaskInfo) GetTid() int {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.tid
+
+	return ti.getFeed().Tid
 }
 
 // GetPid returns the pid of the task.
 func (ti *TaskInfo) GetPid() int {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.pid
+
+	return ti.getFeed().Pid
 }
 
 // GetNsTid returns the nsTid of the task.
 func (ti *TaskInfo) GetNsTid() int {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.nsTid
+
+	return ti.getFeed().NsTid
 }
 
 // GetNsPid returns the nsPid of the task.
 func (ti *TaskInfo) GetNsPid() int {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.nsPid
+
+	return ti.getFeed().NsPid
 }
 
 // GetPPid returns the ppid of the task.
 func (ti *TaskInfo) GetPPid() int {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.pPid.GetCurrent()
+
+	return ti.getFeed().PPid
 }
 
 // GetPPidAt returns the ppid of the task at the given time.
 func (ti *TaskInfo) GetPPidAt(targetTime time.Time) int {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.pPid.Get(targetTime)
+
+	return ti.getFeedAt(targetTime).PPid
 }
 
 // GetNsPPid returns the nsPPid of the task.
 func (ti *TaskInfo) GetNsPPid() int {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.nsPPid.GetCurrent()
+
+	return ti.getFeed().NsPPid
 }
 
 // GetNsPPidAt returns the nsPPid of the task at the given time.
 func (ti *TaskInfo) GetNsPPidAt(targetTime time.Time) int {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.nsPPid.Get(targetTime)
+
+	return ti.getFeedAt(targetTime).NsPPid
 }
 
 // GetUid returns the uid of the task.
 func (ti *TaskInfo) GetUid() int {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.uid.GetCurrent()
+
+	return ti.getFeed().Uid
 }
 
 // GetUidAt returns the uid of the task at the given time.
 func (ti *TaskInfo) GetUidAt(targetTime time.Time) int {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.uid.Get(targetTime)
+
+	return ti.getFeedAt(targetTime).Uid
 }
 
 // GetGid returns the gid of the task.
 func (ti *TaskInfo) GetGid() int {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.gid.GetCurrent()
+
+	return ti.getFeed().Gid
 }
 
 // GetGidAt returns the gid of the task at the given time.
 func (ti *TaskInfo) GetGidAt(targetTime time.Time) int {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.gid.Get(targetTime)
+
+	return ti.getFeedAt(targetTime).Gid
 }
 
 // GetStartTimeNS returns the start time of the task in nanoseconds since epoch
 func (ti *TaskInfo) GetStartTimeNS() uint64 {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.startTimeNS
+
+	return ti.getFeed().StartTimeNS
 }
 
 // GetStartTime returns the start time of the task.
@@ -372,28 +292,31 @@ func (ti *TaskInfo) GetStartTime() time.Time {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
 
-	return traceetime.NsSinceEpochToTime(ti.startTimeNS)
+	return traceetime.NsSinceEpochToTime(ti.getFeed().StartTimeNS)
 }
 
 // GetExitTimeNS returns the exitTime of the task in nanoseconds since epoch
 func (ti *TaskInfo) GetExitTimeNS() uint64 {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.exitTimeNS
+
+	return ti.getFeed().ExitTimeNS
 }
 
 // GetExitTime returns the exit time of the task.
 func (ti *TaskInfo) GetExitTime() time.Time {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return traceetime.NsSinceEpochToTime(ti.exitTimeNS)
+
+	return traceetime.NsSinceEpochToTime(ti.getFeed().ExitTimeNS)
 }
 
 // IsAlive returns true if the task has exited.
 func (ti *TaskInfo) IsAlive() bool {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	return ti.exitTimeNS == 0
+
+	return ti.getFeed().ExitTimeNS == 0
 }
 
 // IsAliveAt return whether the task is alive in the given time, either because it didn't start
@@ -401,15 +324,26 @@ func (ti *TaskInfo) IsAlive() bool {
 func (ti *TaskInfo) IsAliveAt(targetTime time.Time) bool {
 	ti.mutex.RLock()
 	defer ti.mutex.RUnlock()
-	if ti.exitTimeNS != 0 {
-		if targetTime.After(traceetime.NsSinceEpochToTime(ti.exitTimeNS)) {
+
+	feed := ti.getFeedAt(targetTime)
+	exitTimeNS := feed.ExitTimeNS
+	if exitTimeNS != 0 {
+		if targetTime.After(traceetime.NsSinceEpochToTime(exitTimeNS)) {
 			return false
 		}
 	}
+
 	// If start time is not initialized it will count as 0 ns, meaning it will be before any
 	// query time given.
-	if targetTime.Before(traceetime.NsSinceEpochToTime(ti.startTimeNS)) {
-		return false
-	}
-	return true
+	return !targetTime.Before(traceetime.NsSinceEpochToTime(feed.StartTimeNS))
+}
+
+// private getters
+
+func (ti *TaskInfo) getFeed() TaskInfoFeed {
+	return ti.feed.GetCurrent()
+}
+
+func (ti *TaskInfo) getFeedAt(targetTime time.Time) TaskInfoFeed {
+	return ti.feed.Get(targetTime)
 }
