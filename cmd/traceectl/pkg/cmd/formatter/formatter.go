963f3042203d183bc8321e10b0fb41f48fad8812
ShohamBit <shohambit@gmail.com>
2024-11-28 08:46:10 +0000

    fix formatter

--- cmd/traceectl/pkg/cmd/formatter/formatter.go
+++ cmd/traceectl/pkg/cmd/formatter/formatter.go
@@ -2,67 +2,41 @@ package formatter
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 const (
-	FormatJSON  = "json"
+	FormatJson  = "json"
 	FormatTable = "table"
-	FormatGoTpl = "gotemplate"
 )
 
-var SupportedFormats = []string{FormatJSON, FormatTable, FormatGoTpl}
+var SupportedFormats = []string{FormatJson, FormatTable}
 
 type Formatter struct {
-	Format string
-	Output string
-	CMD    *cobra.Command
+	format   string
+	cmd      *cobra.Command
+	paddings map[int][]int
 }
 
-func New(format string, output string, cmd *cobra.Command) (*Formatter, error) {
-	if !containsFormat(format) {
+func NewFormatter(format string, cmd *cobra.Command) (*Formatter, error) {
+	switch format {
+	case FormatJson:
+		return &Formatter{
+			format: format,
+			cmd:    cmd,
+		}, nil
+	case FormatTable:
+		//add padding for table
+		return &Formatter{
+			format: format,
+			cmd:    cmd,
+			paddings: map[int][]int{
+				4: {20, 15, 15, 20},     // Padding for 4 columns
+				5: {15, 10, 20, 15, 10}, // Padding for 5 columns
+			},
+		}, nil
+	default:
 		return nil, fmt.Errorf("format %s is not supported", format)
 	}
-	if err := initOutput(cmd, output); err != nil {
-		return nil, err
-	}
-	return &Formatter{
-		Format: format,
-		Output: output,
-		CMD:    cmd,
-	}, nil
-}
-func containsFormat(format string) bool {
-	for _, f := range SupportedFormats {
-		if f == format {
-			return true
-		}
-	}
-	return false
-}
-func initOutput(cmd *cobra.Command, output string) error {
-	if (output != "") && (output != "stdout") {
-		if output == "" || strings.TrimSpace(output) == "" {
-			return fmt.Errorf("output file path is empty or invalid")
-		}
-		dir := filepath.Dir(output)
-		if err := os.MkdirAll(dir, 0755); err != nil {
-			return fmt.Errorf("failed to create directories for output file: %v", err)
-		}
-		file, err := os.Create(output)
-		if err != nil {
-			return fmt.Errorf("failed to open output file: %v", err)
-		}
-
-		cmd.SetOut(file)
-		cmd.SetErr(file)
-		cmd.PersistentPostRun = func(cmd *cobra.Command, args []string) {
-			file.Close()
-		}
-	}
-	return nil
 }
