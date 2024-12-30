75be201244b30300684c0f8e3beedf74e9446953
ShohamBit <shohambit@gmail.com>
2024-12-30 10:21:59 +0000

    added test function for parseMMapProt

--- pkg/events/parse_args_test.go
+++ pkg/events/parse_args_test.go
@@ -278,6 +278,56 @@ func TestParseArgs(t *testing.T) {
 		}
 	})
 }
+func TestParseMMapProt(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		eventId      int
+		name         string
+		args         []trace.Argument
+		expectedArgs []trace.Argument
+	}{{
+		eventId: int(Mmap),
+		name:    "normal flow(PROT_READ)",
+		args: []trace.Argument{
+			{
+				ArgMeta: trace.ArgMeta{
+					Name: "prot",
+					Type: "int",
+				},
+				Value: int32(parsers.PROT_READ.Value()),
+			},
+		},
+		expectedArgs: []trace.Argument{
+			{
+				ArgMeta: trace.ArgMeta{
+					Name: "prot",
+					Type: "string",
+				},
+				Value: "PROT_READ",
+			},
+		},
+	},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			event := &trace.Event{
+				EventID: testCase.eventId,
+				Args:    testCase.args,
+			}
+			err := ParseArgs(event)
+			require.NoError(t, err)
+			for _, expArg := range testCase.expectedArgs {
+				arg := GetArg(event, expArg.Name)
+				assert.Equal(t, expArg, *arg)
+			}
+		})
+	}
+}
 
 func TestParseBPFCmd(t *testing.T) {
 	t.Parallel()
