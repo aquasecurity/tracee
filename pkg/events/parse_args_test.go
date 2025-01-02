c42cd5ab9fe579f80e945c66f2efda4d95f26a4d
ShohamBit <shohambit@gmail.com>
2025-01-02 07:22:49 +0000

    added TestParsePtraceRequestArgument

--- pkg/events/parse_args_test.go
+++ pkg/events/parse_args_test.go
@@ -718,7 +718,6 @@ func TestParseMemProtAlert(t *testing.T) {
 		})
 	}
 }
-
 func TestParseSyscall(t *testing.T) {
 	t.Parallel()
 	testCases := []struct {
@@ -787,7 +786,75 @@ func TestParseSyscall(t *testing.T) {
 		})
 	}
 }
+func TestParsePtraceRequestArgument(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		name         string
+		args         []trace.Argument
+		expectedArgs []trace.Argument
+	}{
+		{
+			name: "normal flow",
+			args: []trace.Argument{
+				{
+					ArgMeta: trace.ArgMeta{
+						Name: "req",
+						Type: "int",
+					},
+					Value: parsers.PTRACE_PEEKTEXT.Value(),
+				},
+			},
+			expectedArgs: []trace.Argument{
+				{
+					ArgMeta: trace.ArgMeta{
+						Name: "req",
+						Type: "string",
+					},
+					Value: "PTRACE_PEEKTEXT",
+				},
+			},
+		},
+		{
+			name: "invalid req",
+			args: []trace.Argument{
+				{
+					ArgMeta: trace.ArgMeta{
+						Name: "req",
+						Type: "int",
+					},
+					Value: uint64(12345),
+				},
+			},
+			expectedArgs: []trace.Argument{
+				{
+					ArgMeta: trace.ArgMeta{
+						Name: "req",
+						Type: "string",
+					},
+					Value: "12345",
+				},
+			},
+		},
+	}
 
+	for _, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			event := &trace.Event{
+				Args: testCase.args,
+			}
+			parsePtraceRequestArgument(GetArg(event, "req"), testCase.args[0].Value.(uint64))
+			for _, expArg := range testCase.expectedArgs {
+				arg := GetArg(event, expArg.Name)
+				assert.Equal(t, expArg, *arg)
+			}
+		})
+	}
+
+}
 func TestParseBPFCmd(t *testing.T) {
 	t.Parallel()
 
