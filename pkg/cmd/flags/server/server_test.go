f7b346ed6ced41a2e03d2408f15248179e778711
ShohamBit <shohambit@gmail.com>
2025-01-06 19:37:06 +0000

    added more test to server flag

--- pkg/cmd/flags/server/server_test.go
+++ pkg/cmd/flags/server/server_test.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/aquasecurity/tracee/pkg/server/grpc"
 	"github.com/aquasecurity/tracee/pkg/server/http"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -10,7 +12,6 @@ import (
 
 func TestPrepareServer(t *testing.T) {
 	t.Parallel()
-
 	testCases := []struct {
 		testName       string
 		serverFlags    []string
@@ -25,6 +26,92 @@ func TestPrepareServer(t *testing.T) {
 			},
 			expectedError: nil,
 		},
+		{
+			testName:    "grpc server only",
+			serverFlags: []string{"grpc.address=unix:/tmp/tracee.sock"},
+			expectedServer: &Server{
+				GRPCServer: grpc.New("unix", "/tmp/tracee.sock"),
+			},
+			expectedError: nil,
+		},
+		{
+			testName: "http and grpc server",
+			serverFlags: []string{
+				"http.address=127.0.0.1:8080",
+				"grpc.address=unix:/tmp/tracee.sock",
+			},
+			expectedServer: &Server{
+				HTTPServer: http.New("127.0.0.1:8080"),
+				GRPCServer: grpc.New("unix", "/tmp/tracee.sock"),
+			},
+			expectedError: nil,
+		},
+		{
+			testName: "grpc server with unix socket",
+			serverFlags: []string{
+				"grpc.address=unix",
+			},
+			expectedServer: &Server{
+				GRPCServer: grpc.New("unix", "/var/run/tracee.sock"),
+			},
+			expectedError: nil,
+		},
+		{
+			testName: "grpc server with tcp address",
+			serverFlags: []string{
+				"grpc.address=tcp:4466",
+			},
+			expectedServer: &Server{
+				GRPCServer: grpc.New("tcp", "4466"),
+			},
+			expectedError: nil,
+		},
+		{
+			testName: "grpc server with tcp  only",
+			serverFlags: []string{
+				"grpc.address=tcp",
+			},
+			expectedServer: &Server{
+				GRPCServer: grpc.New("tcp", "4466"),
+			},
+			expectedError: nil,
+		},
+		{
+			testName:       "invalid server flag",
+			serverFlags:    []string{"invalid"},
+			expectedServer: nil,
+			expectedError:  fmt.Errorf("cannot process the flag: try grpc.Xxx or http.Xxx instead"),
+		},
+		{
+			testName:       "invalid server flag",
+			serverFlags:    []string{"invalid.invalid"},
+			expectedServer: nil,
+			expectedError:  fmt.Errorf("cannot process the flag: try grpc.Xxx or http.Xxx instead"),
+		},
+		{
+			testName:       "invalid http flag",
+			serverFlags:    []string{"http.invalid=true"},
+			expectedServer: nil,
+			expectedError:  fmt.Errorf("invalid http flag, consider using one of the following commands: address, metrics, healthz, pprof, pyroscope"),
+		},
+		{
+			testName:       "invalid grpc flag",
+			serverFlags:    []string{"grpc.invalid=true"},
+			expectedServer: nil,
+			expectedError:  fmt.Errorf("invalid grpc flag, consider using address"),
+		},
+		{
+			testName:       "invalid grpc protocol",
+			serverFlags:    []string{"grpc.address=invalid:4466"},
+			expectedServer: nil,
+			expectedError:  fmt.Errorf("grpc supported protocols are tcp or unix. eg: tcp:4466, unix:/var/run/tracee.sock"),
+		},
+		{
+			testName:       "invalid http protocol",
+			serverFlags:    []string{"http.address=invalid:8080"},
+			expectedServer: nil,
+			expectedError:  fmt.Errorf("invalid http address"),
+		},
 	}
 	for _, testcase := range testCases {
 		t.Run(testcase.testName, func(t *testing.T) {
@@ -38,4 +125,24 @@ func TestPrepareServer(t *testing.T) {
 
 		})
 	}
+}
+
+func GetHttpTestServer(listenAddr string, metrics bool, pprof bool, healthz bool, pyroscope bool) *http.Server {
+	server := http.New(listenAddr)
+	if metrics {
+		server.EnableMetricsEndpoint()
+	}
+	if healthz {
+		server.EnableHealthzEndpoint()
+	}
+	if pprof {
+		server.EnablePProfEndpoint()
+	}
+	if pyroscope {
+		err := server.EnablePyroAgent()
+		if err != nil {
+			panic(err)
+		}
+	}
+	return server
 }
