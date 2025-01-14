42e1c5ebdb82ef994f16c63473e2d5587e6b2e01
Geyslan Gregório <geyslan@gmail.com>
2025-01-14 08:58:23 -0300

    chore(controlplane): add Benchmark_procTreeExecProcessor
    Running tool: /home/gg/.goenv/versions/1.22.4/bin/go test -benchmem
    -run=^$ -tags ebpf -bench ^Benchmark_procTreeExecProcessor$
    github.com/aquasecurity/tracee/pkg/ebpf/controlplane
    -benchtime=100000000x
    
    goos: linux
    goarch: amd64
    pkg: github.com/aquasecurity/tracee/pkg/ebpf/controlplane
    cpu: AMD Ryzen 9 7950X 16-Core Processor
    Benchmark_procTreeExecProcessor-32 100000000  649.7 ns/op  500 B/op  6 allocs/op
    PASS
    ok  github.com/aquasecurity/tracee/pkg/ebpf/controlplane  64.981s

--- pkg/ebpf/controlplane/processes_bench_test.go
+++ pkg/ebpf/controlplane/processes_bench_test.go
@@ -43,3 +43,41 @@ func Benchmark_procTreeForkProcessor(b *testing.B) {
 		_ = ctrl.procTreeForkProcessor(args)
 	}
 }
+
+func Benchmark_procTreeExecProcessor(b *testing.B) {
+	ctrl := &Controller{}
+	ctrl.processTree, _ = proctree.NewProcessTree(
+		context.Background(),
+		proctree.ProcTreeConfig{
+			Source:           proctree.SourceBoth,
+			ProcessCacheSize: proctree.DefaultProcessCacheSize,
+			ThreadCacheSize:  proctree.DefaultThreadCacheSize,
+		},
+	)
+
+	args := []trace.Argument{
+		{ArgMeta: trace.ArgMeta{Name: "timestamp"}, Value: uint64(1)},
+		{ArgMeta: trace.ArgMeta{Name: "task_hash"}, Value: uint32(1)},
+		{ArgMeta: trace.ArgMeta{Name: "parent_hash"}, Value: uint32(1)},
+		{ArgMeta: trace.ArgMeta{Name: "leader_hash"}, Value: uint32(1)},
+		{ArgMeta: trace.ArgMeta{Name: "cmdpath"}, Value: "/bin/bash"},
+		{ArgMeta: trace.ArgMeta{Name: "pathname"}, Value: "/bin/bash"},
+		{ArgMeta: trace.ArgMeta{Name: "dev"}, Value: uint32(1)},
+		{ArgMeta: trace.ArgMeta{Name: "inode"}, Value: uint64(1)},
+		{ArgMeta: trace.ArgMeta{Name: "ctime"}, Value: uint64(1)},
+		{ArgMeta: trace.ArgMeta{Name: "inode_mode"}, Value: uint16(1)},
+		{ArgMeta: trace.ArgMeta{Name: "interpreter_pathname"}, Value: "/lib64/ld-linux-x86-64.so.2"},
+		{ArgMeta: trace.ArgMeta{Name: "interpreter_dev"}, Value: uint32(1)},
+		{ArgMeta: trace.ArgMeta{Name: "interpreter_inode"}, Value: uint64(1)},
+		{ArgMeta: trace.ArgMeta{Name: "interpreter_ctime"}, Value: uint64(1)},
+		{ArgMeta: trace.ArgMeta{Name: "interp"}, Value: "/lib64/ld-linux-x86-64.so.2"},
+		{ArgMeta: trace.ArgMeta{Name: "stdin_type"}, Value: uint16(1)},
+		{ArgMeta: trace.ArgMeta{Name: "stdin_path"}, Value: "/dev/null"},
+		{ArgMeta: trace.ArgMeta{Name: "invoked_from_kernel"}, Value: int32(1)},
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = ctrl.procTreeExecProcessor(args)
+	}
+}
