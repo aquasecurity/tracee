786373fbe0db249efbf06c0912fa246b5779d762
Geyslan Gregório <geyslan@gmail.com>
2025-01-13 22:40:20 -0300

    perf(controlplane): improve procTreeForkProcessor
    | Metric                  | Old Value  | New Value   | Improvement (%) |
    |-------------------------|------------|-------------|-----------------|
    | Time per operation (ns) | 618.2      | 274.0       | 55.67%          |
    | Bytes allocated (B/op)  | 496        | 0           | 100.00%         |
    | Allocations per op      | 5          | 0           | 100.00%         |
    | Total runtime (s)       | 61.827     | 27.415      | 55.67%          |
    
    ---
    
    Running tool: /home/gg/.goenv/versions/1.22.4/bin/go test -benchmem
    -run=^$ -tags ebpf -bench ^Benchmark_procTreeForkProcessor$
    github.com/aquasecurity/tracee/pkg/ebpf/controlplane
    -benchtime=100000000x
    
    goos: linux
    goarch: amd64
    pkg: github.com/aquasecurity/tracee/pkg/ebpf/controlplane
    cpu: AMD Ryzen 9 7950X 16-Core Processor
    Benchmark_procTreeForkProcessor-32  100000000  274.0 ns/op  0 B/op  0 allocs/op
    PASS
    ok  github.com/aquasecurity/tracee/pkg/ebpf/controlplane  27.415s

--- pkg/ebpf/controlplane/processes.go
+++ pkg/ebpf/controlplane/processes.go
@@ -13,8 +13,6 @@ import (
 //
 
 func (ctrl *Controller) procTreeForkProcessor(args []trace.Argument) error {
-	var errs []error
-
 	if ctrl.processTree == nil {
 		return nil // process tree is disabled
 	}
@@ -23,49 +21,74 @@ func (ctrl *Controller) procTreeForkProcessor(args []trace.Argument) error {
 
 	// Process & Event identification arguments
 	timestamp, err := parse.ArgVal[uint64](args, "timestamp")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 
 	// Parent Process (Go up in hierarchy until parent is a process and not a lwp)
 	parentTid, err := parse.ArgVal[int32](args, "parent_process_tid")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 	parentNsTid, err := parse.ArgVal[int32](args, "parent_process_ns_tid")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 	parentPid, err := parse.ArgVal[int32](args, "parent_process_pid")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 	parentNsPid, err := parse.ArgVal[int32](args, "parent_process_ns_pid")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 	parentStartTime, err := parse.ArgVal[uint64](args, "parent_process_start_time")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 
 	// Thread Group Leader (might be the same as the "child", if "child" is a process)
 	leaderTid, err := parse.ArgVal[int32](args, "leader_tid")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 	leaderNsTid, err := parse.ArgVal[int32](args, "leader_ns_tid")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 	leaderPid, err := parse.ArgVal[int32](args, "leader_pid")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 	leaderNsPid, err := parse.ArgVal[int32](args, "leader_ns_pid")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 	leaderStartTime, err := parse.ArgVal[uint64](args, "leader_start_time")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 
 	// Child (might be a process or a thread)
 	childTid, err := parse.ArgVal[int32](args, "child_tid")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 	childNsTid, err := parse.ArgVal[int32](args, "child_ns_tid")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 	childPid, err := parse.ArgVal[int32](args, "child_pid")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 	childNsPid, err := parse.ArgVal[int32](args, "child_ns_pid")
-	errs = append(errs, err)
+	if err != nil {
+		return err
+	}
 	childStartTime, err := parse.ArgVal[uint64](args, "start_time") // child_start_time
-	errs = append(errs, err)
-
-	// Handle errors
-	for _, err := range errs {
-		if err != nil {
-			return err
-		}
+	if err != nil {
+		return err
 	}
 
 	timestamp = time.BootToEpochNS(timestamp)
