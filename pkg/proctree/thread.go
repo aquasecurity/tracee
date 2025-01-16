5ea4bee1ebd9825c9a6c84147bedf01a8715b4cf
Geyslan Gregório <geyslan@gmail.com>
2025-01-16 11:35:20 -0300

    perf(proctree): change Thread concurrency control
    Mutex is a heavy lock, and it's not necessary to use it in the Thread
    concurrency control. This change replaces the mutex with atomic
    operations to reduce contention, what also reduces memory footprint.

--- pkg/proctree/thread.go
+++ pkg/proctree/thread.go
@@ -1,17 +1,13 @@
 package proctree
 
-import (
-	"sync"
-)
+import "sync/atomic"
 
 // Thread represents a thread.
 type Thread struct {
-	threadHash uint32    // hash of thread
+	threadHash uint32    // hash of thread (immutable, so no need of concurrency control)
 	parentHash uint32    // hash of parent
 	leaderHash uint32    // hash of thread group leader
-	info       *TaskInfo // task info
-	// Control fields
-	mutex *sync.RWMutex // mutex to protect the thread
+	info       *TaskInfo // task info (immutable pointer)
 }
 
 // NOTE: The importance of having the thread group leader hash to each thread is the following: the
@@ -25,8 +21,8 @@ func NewThread(hash uint32, info *TaskInfo) *Thread {
 	return &Thread{
 		threadHash: hash,
 		parentHash: 0,
+		leaderHash: 0,
 		info:       info,
-		mutex:      &sync.RWMutex{},
 	}
 }
 
@@ -34,44 +30,32 @@ func NewThread(hash uint32, info *TaskInfo) *Thread {
 
 // GetHash returns the hash of the thread.
 func (t *Thread) GetHash() uint32 {
-	t.mutex.RLock()
-	defer t.mutex.RUnlock()
-	return t.threadHash
+	return t.threadHash // immutable
 }
 
 // GetParentHash returns the hash of the parent.
 func (t *Thread) GetParentHash() uint32 {
-	t.mutex.RLock()
-	defer t.mutex.RUnlock()
-	return t.parentHash
+	return atomic.LoadUint32(&t.parentHash)
 }
 
 // GEtLeaderHash returns the hash of the thread group leader.
 func (t *Thread) GetLeaderHash() uint32 {
-	t.mutex.RLock()
-	defer t.mutex.RUnlock()
-	return t.leaderHash
+	return atomic.LoadUint32(&t.leaderHash)
 }
 
 // GetInfo returns a instanced task info.
 func (t *Thread) GetInfo() *TaskInfo {
-	t.mutex.RLock()
-	defer t.mutex.RUnlock()
-	return t.info
+	return t.info // immutable pointer
 }
 
 // Setters
 
 // SetParentHash sets the hash of the parent.
 func (t *Thread) SetParentHash(parentHash uint32) {
-	t.mutex.Lock()
-	defer t.mutex.Unlock()
-	t.parentHash = parentHash
+	atomic.StoreUint32(&t.parentHash, parentHash)
 }
 
 // SetLeaderHash sets the hash of the thread group leader.
 func (t *Thread) SetLeaderHash(leaderHash uint32) {
-	t.mutex.Lock()
-	defer t.mutex.Unlock()
-	t.leaderHash = leaderHash
+	atomic.StoreUint32(&t.leaderHash, leaderHash)
 }
