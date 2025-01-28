f33ee0b33765a2c3b1406be0fd4fe8876c0fa3d1
Ofek Shaked <ofek.shaked@aquasec.com>
2025-01-28 11:00:03 +0200

    feat(events): add open_file_ns and open_file_mount events

--- pkg/ebpf/c/common/filesystem.h
+++ pkg/ebpf/c/common/filesystem.h
@@ -181,9 +181,16 @@ statfunc size_t get_path_str_buf(struct path *path, buf_t *out_buf)
         return 0;
     }
 
+    // Disconnected dentry - the file in question is not in
+    // the dentry cache and its path cannot be constructed.
+    struct dentry *dentry = BPF_CORE_READ(path, dentry);
+    if (BPF_CORE_READ(dentry, d_flags) & DCACHE_DISCONNECTED) {
+        bpf_probe_read_kernel_str(&(out_buf->buf[0]), MAX_STRING_SIZE, "<disconnected>");
+        return 0;
+    }
+
     char slash = '/';
     int zero = 0;
-    struct dentry *dentry = BPF_CORE_READ(path, dentry);
     struct vfsmount *vfsmnt = BPF_CORE_READ(path, mnt);
     struct mount *mnt_parent_p;
     struct mount *mnt_p = real_mount(vfsmnt);
@@ -297,17 +304,19 @@ statfunc void *get_path_str_cached(struct file *file)
     return &path->buf;
 }
 
-statfunc void *get_dentry_path_str(struct dentry *dentry)
+statfunc void *get_dentry_path_str_buf(struct dentry *dentry, buf_t *out_buf)
 {
+    // Disconnected dentry - the file in question is not in
+    // the dentry cache and its path cannot be constructed.
+    if (BPF_CORE_READ(dentry, d_flags) & DCACHE_DISCONNECTED) {
+        bpf_probe_read_kernel_str(&(out_buf->buf[0]), MAX_STRING_SIZE, "<disconnected>");
+        return 0;
+    }
+
     char slash = '/';
     int zero = 0;
 
     u32 buf_off = (MAX_PERCPU_BUFSIZE >> 1);
-
-    // Get per-cpu string buffer
-    buf_t *string_p = get_buf(STRING_BUF_IDX);
-    if (string_p == NULL)
-        return NULL;
 
 #pragma unroll
     for (int i = 0; i < MAX_PATH_COMPONENTS; i++) {
@@ -324,12 +333,12 @@ statfunc void *get_dentry_path_str(struct dentry *dentry)
         if (off <= buf_off) { // verify no wrap occurred
             len = len & ((MAX_PERCPU_BUFSIZE >> 1) - 1);
             sz = bpf_probe_read_kernel_str(
-                &(string_p->buf[off & ((MAX_PERCPU_BUFSIZE >> 1) - 1)]), len, (void *) d_name.name);
+                &(out_buf->buf[off & ((MAX_PERCPU_BUFSIZE >> 1) - 1)]), len, (void *) d_name.name);
         } else
             break;
         if (sz > 1) {
             buf_off -= 1; // remove null byte termination with slash sign
-            bpf_probe_read_kernel(&(string_p->buf[buf_off & (MAX_PERCPU_BUFSIZE - 1)]), 1, &slash);
+            bpf_probe_read_kernel(&(out_buf->buf[buf_off & (MAX_PERCPU_BUFSIZE - 1)]), 1, &slash);
             buf_off -= sz - 1;
         } else {
             // If sz is 0 or 1 we have an error (path can't be null nor an empty string)
@@ -342,16 +351,26 @@ statfunc void *get_dentry_path_str(struct dentry *dentry)
         // memfd files have no path in the filesystem -> extract their name
         buf_off = 0;
         struct qstr d_name = get_d_name_from_dentry(dentry);
-        bpf_probe_read_kernel_str(&(string_p->buf[0]), MAX_STRING_SIZE, (void *) d_name.name);
+        bpf_probe_read_kernel_str(&(out_buf->buf[0]), MAX_STRING_SIZE, (void *) d_name.name);
     } else {
         // Add leading slash
         buf_off -= 1;
-        bpf_probe_read_kernel(&(string_p->buf[buf_off & (MAX_PERCPU_BUFSIZE - 1)]), 1, &slash);
+        bpf_probe_read_kernel(&(out_buf->buf[buf_off & (MAX_PERCPU_BUFSIZE - 1)]), 1, &slash);
         // Null terminate the path string
-        bpf_probe_read_kernel(&(string_p->buf[(MAX_PERCPU_BUFSIZE >> 1) - 1]), 1, &zero);
+        bpf_probe_read_kernel(&(out_buf->buf[(MAX_PERCPU_BUFSIZE >> 1) - 1]), 1, &zero);
     }
 
-    return &string_p->buf[buf_off];
+    return &out_buf->buf[buf_off];
+}
+
+statfunc void *get_dentry_path_str(struct dentry *dentry)
+{
+    // Get per-cpu string buffer
+    buf_t *string_p = get_buf(STRING_BUF_IDX);
+    if (string_p == NULL)
+        return NULL;
+
+    return get_dentry_path_str_buf(dentry, string_p);
 }
 
 statfunc file_info_t get_file_info(struct file *file)
