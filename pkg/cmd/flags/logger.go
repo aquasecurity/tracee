21e2031dc28e51f99cf6a7fcde4d0e26d10bdf74
ShohamBit <shohambit@gmail.com>
2025-03-06 13:07:10 +0200

    modified logger prepare flag function to align with new config system

--- pkg/cmd/flags/logger.go
+++ pkg/cmd/flags/logger.go
@@ -11,15 +11,32 @@ import (
 	"github.com/aquasecurity/tracee/pkg/logger"
 )
 
+const (
+	LogLevel                    = "level"
+	LogLevelDebug               = "debug"
+	LogLevelInfo                = "info"
+	LogLevelWarn                = "warn"
+	LogLevelError               = "error"
+	LogLevelFatal               = "fatal"
+	LogFile                     = "file"
+	LogFilter                   = "filter"
+	LogInclude                  = "include"
+	LogExclude                  = "exclude"
+	LogAggregation              = "aggregation"
+	LogAggregationEnabled       = "enabled"
+	LogAggregationFlushInterval = "flush-interval"
+)
+
 func logHelp() string {
 	return `Control logger options - aggregation and level priority.
 
 Possible options:
-  --log aggregate[:flush-interval]    | turns log aggregation on, delaying output with an optional interval (s, m) (default: 3s)
-  --log <debug|info|warn|error|panic> | set log level, info is the default
-  --log file:/path/to/file            | write the logs to a specified file. create/trim the file if exists (default: stderr)
-  --log filter:<option;...>           | Filters in logs that match the specified option values.
-  --log filter-out:<option;...>       | Filters out logs that match the specified option values.
+  --log aggregate.enable            			    | turns log aggregation on
+  --log aggregate.flush-interval    				| delaying output with an optional interval (s, m) (default: 3s)
+  --log level=<debug|info|warn|error|panic> 		| set log level, info is the default
+  --log file=/path/to/file            				| write the logs to a specified file. create/trim the file if exists (default: stderr)
+  --log filter.include.<option;...>          		| Filters in logs that match the specified option values.
+  --log filter.exclude.<option;...>       			| Filters out logs that match the specified option values.
 
 Filter options:
   msg=<value,...> 				       | Filters logs that message contains a value.
@@ -30,16 +47,15 @@ Filter options:
   libbpf                               | Filters logs that originate from libbpf.
 
 Examples:
-  --log debug                                        | outputs debug level logs
-  --log debug --log aggregate                        | outputs aggregated debug level logs every 3 seconds (default)
-  --log aggregate:5s                                 | outputs aggregated logs every 5 seconds
-  --log debug --log file:/tmp/tracee.log             | outputs debug level logs to /tmp/tracee.log
-  --log filter:'msg=foo,bar;pkg=core;lvl=error'      | Filters in logs that have either 'foo' or 'bar' in the message, are from the 'core' package, and are of 'error' level.
-  --log filter-out:'msg=foo,bar;pkg=core;lvl=error'  | Filters out logs that have either 'foo' or 'bar' in the message, are from the 'core' package, and are of 'error' level.
-  --log filter:msg=foo,bar --log filter-out:pkg=core | Filters in logs that have either 'foo' or 'bar' in the message, and based on that result, filters out logs that are from the 'core' package.
-  --log filter-out:file=/pkg/cmd/flags/logger.go	 | Filters out logs that are from the '/pkg/cmd/flags/logger.go' file.
-  --log filter:regex='^foo'                          | Filters in logs that messages match the regex '^foo'.
-  --log filter:libbpf                                | Filters in logs that originate from libbpf.
+  --log level=debug                                      		     | outputs debug level logs
+  --log level=debug --log aggregate.enable=true                      | outputs aggregated debug level logs every 3 seconds (default)
+  --log aggregate.flush-interval=5s                                  | outputs aggregated logs every 5 seconds
+  --log level=debug --log file=/tmp/tracee.log            			 | outputs debug level logs to /tmp/tracee.log
+  --log filter.include.msg=foo,bar --log filter.include.pkg=core  	 | Filters in logs that have either 'foo' or 'bar' in the message, and are from the 'core' packag.
+  --log filter.exclude.msg=foo,bar --log filter.exclude.pkg=core  	 | Filters out logs that have either 'foo' or 'bar' in the message, and are from the 'core' package.
+  --log filter.exclude.file=/pkg/cmd/flags/logger.go	 			 | Filters out logs that are from the '/pkg/cmd/flags/logger.go' file.
+  --log filter.include.regex='^foo'                         		 | Filters in logs that messages match the regex '^foo'.
+  --log filter.include.libbpf                               		 | Filters in logs that originate from libbpf.
 `
 }
 
@@ -71,41 +87,20 @@ func invalidLogOptionValue(err error, opt string, newBinary bool) error {
 
 func parseLevel(level string) (logger.Level, error) {
 	switch level {
-	case "debug":
+	case LogLevelDebug:
 		return logger.DebugLevel, nil
-	case "info":
+	case LogLevelInfo:
 		return logger.InfoLevel, nil
-	case "warn":
+	case LogLevelWarn:
 		return logger.WarnLevel, nil
-	case "error":
+	case LogLevelError:
 		return logger.ErrorLevel, nil
-	case "fatal":
+	case LogLevelFatal:
 		return logger.FatalLevel, nil
 	default:
 		return logger.DebugLevel, errors.New("invalid log level")
 	}
 }
-
-func validateLogOption(opt string) error {
-	switch {
-	case strings.HasPrefix(opt, "file"):
-		return nil
-	case strings.HasPrefix(opt, "aggregate"):
-		return nil
-	case strings.HasPrefix(opt, "filter-out"):
-		return nil
-	case strings.HasPrefix(opt, "filter"):
-		return nil
-	}
-
-	if _, err := parseLevel(opt); err == nil {
-		return nil
-	}
-
-	// don't pass the error, it's not relevant in this case
-	return invalidLogOption(nil, opt, false)
-}
-
 func PrepareLogger(logOptions []string, newBinary bool) (logger.LoggingConfig, error) {
 	var (
 		agg           bool
@@ -117,30 +112,34 @@ func PrepareLogger(logOptions []string, newBinary bool) (logger.LoggingConfig, e
 	)
 
 	for _, opt := range logOptions {
-		if err := validateLogOption(opt); err != nil {
-			return logger.LoggingConfig{}, err
+		// split log flag by "." for filter and aggregation
+		logParts := strings.SplitN(opt, ".", 2)
+		if len(logParts) < 2 {
+			// split log flag by "=" for level and file
+			logParts = strings.SplitN(opt, "=", 2)
 		}
 
-		// parse file option
-		if strings.HasPrefix(opt, "file") {
-			vals := strings.Split(opt, ":")
-
-			if len(vals) == 1 || vals[1] == "" {
+		switch logParts[0] {
+		case LogLevel:
+			lvl, err = parseLevel(logParts[1])
+			if err != nil {
+				return logger.LoggingConfig{}, invalidLogOptionValue(err, opt, newBinary)
+			}
+		case LogFile:
+			if len(logParts[1]) == 1 || logParts[1] == "" {
 				return logger.LoggingConfig{}, invalidLogOptionValue(nil, opt, newBinary)
 			}
 
-			w, err = CreateOutputFile(vals[1])
+			w, err = CreateOutputFile(logParts[1])
 			if err != nil {
 				return logger.LoggingConfig{}, err
 			}
 
-			continue
-		}
-
-		// parse aggregate option
-		if strings.HasPrefix(opt, "aggregate") {
-			if !strings.HasSuffix(opt, "aggregate") {
-				vals := strings.Split(opt, ":")
+		case LogAggregation:
+			aggregationParts := strings.SplitN(logParts[1], "=", 2)
+			switch aggregationParts[0] {
+			case LogAggregationFlushInterval:
+				vals := strings.SplitN(opt, "=", 2)
 				if len(vals) != 2 || len(vals[1]) <= 1 {
 					return logger.LoggingConfig{}, invalidLogOptionValue(nil, opt, newBinary)
 				}
@@ -159,118 +158,32 @@ func PrepareLogger(logOptions []string, newBinary bool) (logger.LoggingConfig, e
 				if err != nil {
 					return logger.LoggingConfig{}, invalidLogOptionValue(nil, opt, newBinary)
 				}
-			}
+				agg = true
 
-			agg = true
-			continue
-		}
-
-		// parse filter option
-		filterOpts := ""
-		var filterKind logger.FilterKind
-		if strings.HasPrefix(opt, "filter-out:") {
-			filterOpts = strings.TrimPrefix(opt, "filter-out:")
-			filterKind = logger.FilterOut
-		} else if strings.HasPrefix(opt, "filter:") {
-			filterOpts = strings.TrimPrefix(opt, "filter:")
-			filterKind = logger.FilterIn
-		}
-		if filterOpts != "" {
-			for _, filterOpt := range strings.Split(filterOpts, ";") {
-				optTypeVal := strings.SplitN(filterOpt, "=", 2)
-				optType := optTypeVal[0]
-				optVals := []string{}
-				if len(optTypeVal) == 1 && optType != "libbpf" {
-					return logger.LoggingConfig{}, invalidLogOption(nil, opt, newBinary)
-				}
-				if len(optTypeVal) == 2 {
-					if optTypeVal[1] == "" {
-						return logger.LoggingConfig{}, invalidLogOptionValue(nil, opt, newBinary)
-					}
-					optVals = strings.Split(optTypeVal[1], ",")
-				}
-
-				switch optType {
-				case "msg":
-					for _, val := range optVals {
-						if err := filter.AddMsg(val, filterKind); err != nil {
-							if errors.Is(err, logger.ErrFilterOutExistsForKey) {
-								logger.Warnw(err.Error(), "msg", val)
-								continue
-							}
-
-							return logger.LoggingConfig{}, invalidLogOption(err, opt, newBinary)
-						}
-					}
-				case "pkg":
-					for _, val := range optVals {
-						if err := filter.AddPkg(val, filterKind); err != nil {
-							if errors.Is(err, logger.ErrFilterOutExistsForKey) {
-								logger.Warnw(err.Error(), "pkg", val)
-								continue
-							}
-
-							return logger.LoggingConfig{}, invalidLogOption(err, opt, newBinary)
-						}
-					}
-				case "file":
-					for _, val := range optVals {
-						if err := filter.AddFile(val, filterKind); err != nil {
-							if errors.Is(err, logger.ErrFilterOutExistsForKey) {
-								logger.Warnw(err.Error(), "file", val)
-								continue
-							}
-
-							return logger.LoggingConfig{}, invalidLogOption(err, opt, newBinary)
-						}
-					}
-				case "lvl":
-					for _, val := range optVals {
-						filterLvl, err := parseLevel(val)
-						if err != nil {
-							return logger.LoggingConfig{}, invalidLogOptionValue(err, opt, newBinary)
-						}
-
-						if err := filter.AddLvl(int(filterLvl), filterKind); err != nil {
-							if errors.Is(err, logger.ErrFilterOutExistsForKey) {
-								logger.Warnw(err.Error(), "lvl", val)
-								continue
-							}
-
-							return logger.LoggingConfig{}, invalidLogOptionValue(err, opt, newBinary)
-						}
-					}
-				case "regex":
-					for _, val := range optVals {
-						if err := filter.AddMsgRegex(val, filterKind); err != nil {
-							if errors.Is(err, logger.ErrFilterOutExistsForKey) {
-								logger.Warnw(err.Error(), "regex", val)
-								continue
-							}
-
-							return logger.LoggingConfig{}, invalidLogOptionValue(err, opt, newBinary)
-						}
-					}
-				case "libbpf":
-					if err := filter.AddMsgRegex("^libbpf:", filterKind); err != nil {
-						if errors.Is(err, logger.ErrFilterOutExistsForKey) {
-							logger.Warnw(err.Error(), "regex", "^libbpf:")
-							continue
-						}
-
-						return logger.LoggingConfig{}, invalidLogOptionValue(err, opt, newBinary)
-					}
-				default:
-					return logger.LoggingConfig{}, invalidLogOption(nil, opt, newBinary)
+			case LogAggregationEnabled:
+				if len(aggregationParts) == 1 || strings.Compare(aggregationParts[1], "true") == 0 {
+					agg = true
 				}
 			}
-			continue
-		}
 
-		// parse level option
-		lvl, err = parseLevel(opt)
-		if err != nil {
-			return logger.LoggingConfig{}, invalidLogOption(err, opt, newBinary)
+		case LogFilter:
+			filterParts := strings.SplitN(logParts[1], ".", 2)
+			var filterKind logger.FilterKind
+			switch filterParts[0] {
+			case LogInclude:
+				filterKind = logger.FilterIn
+			case LogExclude:
+				filterKind = logger.FilterOut
+			default:
+				return logger.LoggingConfig{}, invalidLogOptionValue(nil, opt, newBinary)
+			}
+
+			filterOpt := filterParts[1]
+			if filterOpt != "" {
+				processLogFilter(opt, newBinary, filterKind, filterOpt)
+			} else {
+				return logger.LoggingConfig{}, invalidLogOption(nil, opt, newBinary)
+			}
 		}
 	}
 
@@ -284,12 +197,103 @@ func PrepareLogger(logOptions []string, newBinary bool) (logger.LoggingConfig, e
 		loggerCfg.Encoder = logger.NewJSONEncoder(logger.NewProductionEncoderConfig())
 	}
 
-	llogger := logger.NewLogger(loggerCfg)
+	traceeLogger := logger.NewLogger(loggerCfg)
 	return logger.LoggingConfig{
-		Logger:        llogger,
+		Logger:        traceeLogger,
 		LoggerConfig:  loggerCfg,
 		Filter:        filter,
 		Aggregate:     agg,
 		FlushInterval: flushInterval,
 	}, nil
+}
+
+func processLogFilter(opt string, newBinary bool, filterKind logger.FilterKind, filterOpt string) (logger.LoggerFilter, error) {
+	var filter = logger.NewLoggerFilter()
+	optTypeParts := strings.SplitN(filterOpt, "=", 2)
+	optType := optTypeParts[0]
+	optVals := []string{}
+	if len(optTypeParts) == 1 && optType != "libbpf" {
+		return filter, invalidLogOption(nil, opt, newBinary)
+	}
+	if len(optTypeParts) == 2 {
+		if optTypeParts[1] == "" {
+			return filter, invalidLogOptionValue(nil, opt, newBinary)
+		}
+		optVals = strings.Split(optTypeParts[1], ",")
+	}
+
+	switch optType {
+	case "msg":
+		for _, val := range optVals {
+			if err := filter.AddMsg(val, filterKind); err != nil {
+				if errors.Is(err, logger.ErrFilterOutExistsForKey) {
+					logger.Warnw(err.Error(), "msg", val)
+					continue
+				}
+
+				return filter, invalidLogOption(err, opt, newBinary)
+			}
+		}
+	case "pkg":
+		for _, val := range optVals {
+			if err := filter.AddPkg(val, filterKind); err != nil {
+				if errors.Is(err, logger.ErrFilterOutExistsForKey) {
+					logger.Warnw(err.Error(), "pkg", val)
+					continue
+				}
+
+				return filter, invalidLogOption(err, opt, newBinary)
+			}
+		}
+	case "file":
+		for _, val := range optVals {
+			if err := filter.AddFile(val, filterKind); err != nil {
+				if errors.Is(err, logger.ErrFilterOutExistsForKey) {
+					logger.Warnw(err.Error(), "file", val)
+					continue
+				}
+
+				return filter, invalidLogOption(err, opt, newBinary)
+			}
+		}
+	case "lvl":
+		for _, val := range optVals {
+			filterLvl, err := parseLevel(val)
+			if err != nil {
+				return filter, invalidLogOptionValue(err, opt, newBinary)
+			}
+
+			if err := filter.AddLvl(int(filterLvl), filterKind); err != nil {
+				if errors.Is(err, logger.ErrFilterOutExistsForKey) {
+					logger.Warnw(err.Error(), "lvl", val)
+					continue
+				}
+
+				return filter, invalidLogOptionValue(err, opt, newBinary)
+			}
+		}
+	case "regex":
+		for _, val := range optVals {
+			if err := filter.AddMsgRegex(val, filterKind); err != nil {
+				if errors.Is(err, logger.ErrFilterOutExistsForKey) {
+					logger.Warnw(err.Error(), "regex", val)
+					continue
+				}
+
+				return filter, invalidLogOptionValue(err, opt, newBinary)
+			}
+		}
+	case "libbpf":
+		if err := filter.AddMsgRegex("^libbpf:", filterKind); err != nil {
+			if errors.Is(err, logger.ErrFilterOutExistsForKey) {
+				logger.Warnw(err.Error(), "regex", "^libbpf:")
+
+			} else {
+				return filter, invalidLogOptionValue(err, opt, newBinary)
+			}
+		}
+	default:
+		return filter, invalidLogOption(nil, opt, newBinary)
+	}
+	return filter, nil
 }
