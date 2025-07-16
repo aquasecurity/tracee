65f7bd7dacc183e007792f0ef06ff6454fb44f99
Nadav Strahilevitz <nadav.strahilevitz@aquasec.com>
2025-07-16 14:40:05 +0000

    fix(tests): sleep container in data source test
    This prevents the container from being deleted before the signature calls
    the data source. Otherwise, the data source may not find the container.

--- tests/e2e-inst-signatures/e2e-containers_data_source.go
+++ tests/e2e-inst-signatures/e2e-containers_data_source.go
@@ -64,7 +64,7 @@ func (sig *e2eContainersDataSource) OnEvent(event protocol.Event) error {
 
 		containerId := eventObj.Container.ID
 		if containerId == "" {
-			return errors.New("received non container event")
+			return fmt.Errorf("received non container event: %+v", eventObj)
 		}
 
 		container, err := sig.containersData.Get(containerId)
