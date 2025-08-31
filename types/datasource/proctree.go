2404b88133038d2dee36f23e4b220f3877515464
Yaniv Agman <yanivagman@gmail.com>
2025-08-31 18:40:16 +0300

    refactor: move ProcessTreeDS from signatures/helpers to types/datasource
    - Move ProcessTreeDS wrapper and all helper functions to types/datasource
    - Remove original proctree.go from signatures/helpers
    
    ProcessTreeDS logically belongs with datasource types and provides a cleaner
    wrapper API for process tree data source operations.

--- types/datasource/proctree.go
+++ types/datasource/proctree.go
@@ -1,7 +1,12 @@
 package datasource
 
 import (
+	"errors"
+	"fmt"
 	"time"
+
+	"github.com/aquasecurity/tracee/types/detect"
+	"github.com/aquasecurity/tracee/types/trace"
 )
 
 // TimeRelevantInfo is the returned value of all the time specific process tree queries.
@@ -80,4 +85,118 @@ type LineageKey struct {
 	EntityId uint32 // The first process (last descendant) entity ID
 	Time     time.Time
 	MaxDepth int
+}
+
+// ProcessTreeDS is an envelope to the process tree datasource API, to make it intuitive and easy
+// to use.
+type ProcessTreeDS struct {
+	ds detect.DataSource
+}
+
+// InitProcessTreeDS init a datasource envelopment instance with the process tree datasource.
+func InitProcessTreeDS(ds detect.DataSource) *ProcessTreeDS {
+	return &ProcessTreeDS{ds: ds}
+}
+
+// GetProcessTreeDataSource init a datasource envelopment instance using the context all signatures
+// are initialized with.
+// This is the recommended way to initialize an instance, as it simpler to use.
+func GetProcessTreeDataSource(ctx detect.SignatureContext) (*ProcessTreeDS, error) {
+	processTreeDataSource, ok := ctx.GetDataSource("tracee", "process_tree")
+	if !ok {
+		return nil, errors.New("data source tracee/process_tree is not registered")
+	}
+
+	if processTreeDataSource.Version() > 1 {
+		return nil, fmt.Errorf(
+			"data source tracee/process_tree version %d is not supported",
+			processTreeDataSource.Version(),
+		)
+	}
+
+	return InitProcessTreeDS(processTreeDataSource), nil
+}
+
+// GetThreadInfo query the datasource for the information of a specific thread.
+func (ptds *ProcessTreeDS) GetThreadInfo(threadKey ThreadKey) (
+	*TimeRelevantInfo[ThreadInfo], error,
+) {
+	threadQueryAnswer, err := ptds.ds.Get(threadKey)
+	if err != nil {
+		return nil, fmt.Errorf("could not find thread for thread %d", threadKey.EntityId)
+	}
+	threadInfo, ok := threadQueryAnswer["thread_info"].(TimeRelevantInfo[ThreadInfo])
+	if !ok {
+		return nil, fmt.Errorf("could not extract info of thread %d", threadKey.EntityId)
+	}
+	return &threadInfo, nil
+}
+
+// GetEventThreadInfo get the information of the thread emitting the current event
+func (ptds *ProcessTreeDS) GetEventThreadInfo(eventObj *trace.Event) (
+	*TimeRelevantInfo[ThreadInfo], error,
+) {
+	queryKey := ThreadKey{
+		EntityId: eventObj.ThreadEntityId,
+		Time:     time.Unix(0, int64(eventObj.Timestamp)), // at the time event was emitted
+	}
+	return ptds.GetThreadInfo(queryKey)
+}
+
+// GetProcessInfo query the datasource for the information of a specific process.
+func (ptds *ProcessTreeDS) GetProcessInfo(processKey ProcKey) (
+	*TimeRelevantInfo[ProcessInfo], error,
+) {
+	// Pick the process info from the data source
+	procQueryAnswer, err := ptds.ds.Get(processKey)
+	if err != nil {
+		return nil, fmt.Errorf("could not find process for process %d", processKey.EntityId)
+	}
+	procInfo, ok := procQueryAnswer["process_info"].(TimeRelevantInfo[ProcessInfo])
+	if !ok {
+		return nil, fmt.Errorf("could not extract info of process %d", processKey.EntityId)
+	}
+	return &procInfo, nil
+}
+
+// GetEventProcessInfo get the information of the process emitting the current event
+func (ptds *ProcessTreeDS) GetEventProcessInfo(eventObj *trace.Event) (
+	*TimeRelevantInfo[ProcessInfo], error,
+) {
+	queryKey := ProcKey{
+		EntityId: eventObj.ProcessEntityId,
+		Time:     time.Unix(0, int64(eventObj.Timestamp)), // at the time event was emitted
+	}
+	return ptds.GetProcessInfo(queryKey)
+}
+
+// GetProcessLineage query the datasource for the information of the process lineage of a
+// specific process.
+func (ptds *ProcessTreeDS) GetProcessLineage(lineageKey LineageKey) (
+	*ProcessLineage, error,
+) {
+	// Pick the lineage info from the data source.
+	lineageQueryAnswer, err := ptds.ds.Get(lineageKey)
+	if err != nil {
+		return nil, fmt.Errorf("could not find thread of process %d", lineageKey.EntityId)
+	}
+	lineageInfo, ok := lineageQueryAnswer["process_lineage"].(ProcessLineage)
+	if !ok {
+		return nil, fmt.Errorf("could not extract info of process %d", lineageKey.EntityId)
+	}
+	return &lineageInfo, nil
+}
+
+// GetEventProcessLineage get the process lineage information of the process emitting the
+// current event.
+func (ptds *ProcessTreeDS) GetEventProcessLineage(
+	eventObj *trace.Event,
+	maxDepth int,
+) (*ProcessLineage, error) {
+	queryKey := LineageKey{
+		EntityId: eventObj.ProcessEntityId,
+		Time:     time.Unix(0, int64(eventObj.Timestamp)), // at the time event was emitted
+		MaxDepth: maxDepth,
+	}
+	return ptds.GetProcessLineage(queryKey)
 }
