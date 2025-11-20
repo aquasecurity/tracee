66c103e5dee0b231b20fff902fed99aab49a109a
Nadav Strahilevitz <nadav.strahilevitz@aquasec.com>
2025-11-20 10:34:07 +0000

    guard!

--- pkg/ebpf/c/tracee.bpf.c
+++ pkg/ebpf/c/tracee.bpf.c
@@ -3921,6 +3921,11 @@ int BPF_KPROBE(trace_io_issue_sqe)
         return 0;
 
     struct io_kiocb___io_issue_sqe *req = (struct io_kiocb___io_issue_sqe *) PT_REGS_PARM1(ctx);
+    // Only proceed if we have the modern io_kiocb structure (kernel 5.5+)
+    // The __builtin_preserve_type_info check happens at compile time
+    if (!bpf_core_type_exists(struct io_kiocb___io_issue_sqe)) {
+        return 0;
+    }
 
     // get real task info from io_uring_worker_context_map
     event_context_t *real_ctx = bpf_map_lookup_elem(&io_uring_worker_context_map, &req);
