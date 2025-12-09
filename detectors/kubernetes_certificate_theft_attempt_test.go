2e7b2a489237ea8418b42d9a6b6c7fe24363c868
Yaniv Agman <yanivagman@gmail.com>
2025-12-09 19:59:58 +0200

    feat(detectors): migrate kubernetes_certificate_theft_attempt signature to detector
    Migrated from signatures/golang/kubernetes_certificate_theft_attempt.go
    Detector ID: TRC-1018

---
+++ detectors/kubernetes_certificate_theft_attempt_test.go
@@ -0,0 +1,148 @@
+package detectors
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/aquasecurity/tracee/api/v1beta1"
+	"github.com/aquasecurity/tracee/api/v1beta1/detection"
+)
+
+func TestKubernetesCertificateTheft(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name           string
+		execPath       string
+		pathname       string
+		flags          int32
+		expectedOutput bool
+	}{
+		{
+			name:           "malicious process reading k8s cert",
+			execPath:       "/usr/bin/malicious",
+			pathname:       "/etc/kubernetes/pki/apiserver.crt",
+			flags:          0, // O_RDONLY
+			expectedOutput: true,
+		},
+		{
+			name:           "kubelet reading k8s cert - should not trigger",
+			execPath:       "/usr/bin/kubelet",
+			pathname:       "/etc/kubernetes/pki/apiserver.crt",
+			flags:          0, // O_RDONLY
+			expectedOutput: false,
+		},
+		{
+			name:           "kube-apiserver reading k8s cert - should not trigger",
+			execPath:       "/usr/bin/kube-apiserver",
+			pathname:       "/etc/kubernetes/pki/apiserver.crt",
+			flags:          0, // O_RDONLY
+			expectedOutput: false,
+		},
+		{
+			name:           "malicious process reading non-k8s file - should not trigger",
+			execPath:       "/usr/bin/malicious",
+			pathname:       "/etc/ssl/certs/ca-certificates.crt",
+			flags:          0, // O_RDONLY
+			expectedOutput: false,
+		},
+		{
+			name:           "malicious process writing k8s cert - should not trigger (not a read)",
+			execPath:       "/usr/bin/malicious",
+			pathname:       "/etc/kubernetes/pki/apiserver.crt",
+			flags:          1, // O_WRONLY
+			expectedOutput: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			detector := &KubernetesCertificateTheft{}
+			err := detector.Init(detection.DetectorParams{Logger: &mockLogger{}})
+			require.NoError(t, err)
+
+			event := &v1beta1.Event{
+				Id:   v1beta1.EventId_security_file_open,
+				Name: "security_file_open",
+				Workload: &v1beta1.Workload{
+					Process: &v1beta1.Process{
+						Executable: &v1beta1.Executable{Path: tc.execPath},
+					},
+				},
+				Data: []*v1beta1.EventValue{
+					v1beta1.NewStringValue("pathname", tc.pathname),
+					v1beta1.NewInt32Value("flags", tc.flags),
+				},
+			}
+
+			output, err := detector.OnEvent(context.Background(), event)
+			require.NoError(t, err)
+
+			if tc.expectedOutput {
+				assert.Len(t, output, 1, "Expected detection")
+			} else {
+				assert.Len(t, output, 0, "Expected no detection")
+			}
+		})
+	}
+}
+
+func TestKubernetesCertificateTheft_Rename(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name           string
+		oldPath        string
+		expectedOutput bool
+	}{
+		{
+			name:           "rename k8s cert - should detect",
+			oldPath:        "/etc/kubernetes/pki/apiserver.key",
+			expectedOutput: true,
+		},
+		{
+			name:           "rename non-k8s file - should not detect",
+			oldPath:        "/tmp/ca.crt",
+			expectedOutput: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			detector := &KubernetesCertificateTheft{}
+			err := detector.Init(detection.DetectorParams{Logger: &mockLogger{}})
+			require.NoError(t, err)
+
+			event := &v1beta1.Event{
+				Id:   v1beta1.EventId_security_inode_rename,
+				Name: "security_inode_rename",
+				Workload: &v1beta1.Workload{
+					Process: &v1beta1.Process{
+						Executable: &v1beta1.Executable{Path: "/usr/bin/mv"},
+					},
+				},
+				Data: []*v1beta1.EventValue{
+					v1beta1.NewStringValue("old_path", tc.oldPath),
+				},
+			}
+
+			output, err := detector.OnEvent(context.Background(), event)
+			require.NoError(t, err)
+
+			if tc.expectedOutput {
+				assert.Len(t, output, 1, "Expected detection for rename of k8s cert")
+			} else {
+				assert.Len(t, output, 0, "Expected no detection for rename of non-k8s file")
+			}
+		})
+	}
+}
