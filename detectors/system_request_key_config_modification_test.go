6354b72af41c6bdce78f98b059850747d47f3149
Yaniv Agman <yanivagman@gmail.com>
2025-12-09 19:59:58 +0200

    feat(detectors): migrate system_request_key_config_modification signature to detector
    Migrated from signatures/golang/system_request_key_config_modification.go
    Detector ID: TRC-1031

---
+++ detectors/system_request_key_config_modification_test.go
@@ -0,0 +1,86 @@
+package detectors
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/aquasecurity/tracee/api/v1beta1"
+	"github.com/aquasecurity/tracee/api/v1beta1/detection"
+)
+
+func TestSystemRequestKeyConfigModification(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name           string
+		pathname       string
+		flags          int32
+		expectedOutput bool
+	}{
+		{
+			name:           "write to /proc/sys/kernel/sysrq",
+			pathname:       "/proc/sys/kernel/sysrq",
+			flags:          1, // O_WRONLY
+			expectedOutput: true,
+		},
+		{
+			name:           "write to /proc/sysrq-trigger",
+			pathname:       "/proc/sysrq-trigger",
+			flags:          1, // O_WRONLY
+			expectedOutput: true,
+		},
+		{
+			name:           "read from /proc/sys/kernel/sysrq - should not trigger",
+			pathname:       "/proc/sys/kernel/sysrq",
+			flags:          0, // O_RDONLY
+			expectedOutput: false,
+		},
+		{
+			name:           "write to different file - should not trigger",
+			pathname:       "/proc/sys/kernel/other",
+			flags:          1, // O_WRONLY
+			expectedOutput: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			detector := &SystemRequestKeyConfigModification{}
+			err := detector.Init(detection.DetectorParams{Logger: &mockLogger{}})
+			require.NoError(t, err)
+
+			event := &v1beta1.Event{
+				Id:   v1beta1.EventId_security_file_open,
+				Name: "security_file_open",
+				Workload: &v1beta1.Workload{
+					Process: &v1beta1.Process{
+						Executable: &v1beta1.Executable{Path: "/usr/bin/test"},
+					},
+					Container: &v1beta1.Container{
+						Id:      "test-container",
+						Started: true,
+					},
+				},
+				Data: []*v1beta1.EventValue{
+					v1beta1.NewStringValue("pathname", tc.pathname),
+					v1beta1.NewInt32Value("flags", tc.flags),
+				},
+			}
+
+			output, err := detector.OnEvent(context.Background(), event)
+			require.NoError(t, err)
+
+			if tc.expectedOutput {
+				assert.Len(t, output, 1, "Expected detection")
+			} else {
+				assert.Len(t, output, 0, "Expected no detection")
+			}
+		})
+	}
+}
