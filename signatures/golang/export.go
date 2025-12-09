804968cac2b3a62016526e4d313d829c92d37dad
Yaniv Agman <yanivagman@gmail.com>
2025-12-09 19:59:58 +0200

    feat(detectors): migrate ptrace_code_injection signature to detector
    Migrated from signatures/golang/ptrace_code_injection.go
    Detector ID: TRC-103

--- signatures/golang/export.go
+++ signatures/golang/export.go
@@ -11,7 +11,6 @@ var ExportedSignatures = []detect.Signature{
 	&SchedDebugRecon{},
 	&SystemRequestKeyConfigModification{},
 	&RcdModification{},
-	&PtraceCodeInjection{},
 	&SyscallTableHooking{},
 }
 
