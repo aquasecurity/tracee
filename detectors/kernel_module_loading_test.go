ccc444490d9d1b282e1c1cf2fb8d4dc1eb03363a
Yaniv Agman <yanivagman@gmail.com>
2025-12-09 19:59:58 +0200

    feat(detectors): migrate kernel_module_loading signature to detector
    Migrated from signatures/golang/kernel_module_loading.go
    Detector ID: TRC-1017

---
+++ detectors/kernel_module_loading_test.go
@@ -0,0 +1,37 @@
+package detectors
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/aquasecurity/tracee/api/v1beta1"
+	"github.com/aquasecurity/tracee/api/v1beta1/detection"
+)
+
+func TestKernelModuleLoading(t *testing.T) {
+	t.Parallel()
+
+	detector := &KernelModuleLoading{}
+	err := detector.Init(detection.DetectorParams{Logger: &mockLogger{}})
+	require.NoError(t, err)
+
+	event := &v1beta1.Event{
+		Id:   v1beta1.EventId_module_load,
+		Name: "module_load",
+		Workload: &v1beta1.Workload{
+			Process: &v1beta1.Process{
+				Executable: &v1beta1.Executable{Path: "/usr/bin/insmod"},
+			},
+		},
+		Data: []*v1beta1.EventValue{
+			v1beta1.NewStringValue("module_name", "test_module"),
+		},
+	}
+
+	output, err := detector.OnEvent(context.Background(), event)
+	require.NoError(t, err)
+	assert.Len(t, output, 1, "Expected detection for module_load event")
+}
