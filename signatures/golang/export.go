99ad57ced7f26ce83388e7b08034484715dded39
Yaniv Agman <yanivagman@gmail.com>
2025-12-09 19:59:58 +0200

    feat(detectors): migrate illegitimate_shell signature to detector
    Migrated from signatures/golang/illegitimate_shell.go
    Detector ID: TRC-1016

--- signatures/golang/export.go
+++ signatures/golang/export.go
@@ -17,7 +17,6 @@ var ExportedSignatures = []detect.Signature{
 	&ProcMemAccess{},
 	&PtraceCodeInjection{},
 	&ProcessVmWriteCodeInjection{},
-	&IllegitimateShell{},
 	&KernelModuleLoading{},
 	&KubernetesCertificateTheftAttempt{},
 	&ProcFopsHooking{},
