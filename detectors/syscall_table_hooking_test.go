ea012341c3255ee9a25622da236df465b06b8697
Yaniv Agman <yanivagman@gmail.com>
2025-12-09 19:59:58 +0200

    feat(detectors): migrate syscall_table_hooking signature to detector
    Migrated from signatures/golang/syscall_table_hooking.go
    Detector ID: TRC-1030

---
+++ detectors/syscall_table_hooking_test.go
@@ -0,0 +1,35 @@
+package detectors
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/aquasecurity/tracee/api/v1beta1"
+	"github.com/aquasecurity/tracee/api/v1beta1/detection"
+)
+
+func TestSyscallTableHooking(t *testing.T) {
+	t.Parallel()
+
+	detector := &SyscallTableHooking{}
+	err := detector.Init(detection.DetectorParams{Logger: &mockLogger{}})
+	require.NoError(t, err)
+
+	event := &v1beta1.Event{
+		Id:   v1beta1.EventId_hooked_syscall,
+		Name: "hooked_syscall",
+		Workload: &v1beta1.Workload{
+			Process: &v1beta1.Process{
+				Executable: &v1beta1.Executable{Path: "/usr/bin/test"},
+			},
+		},
+		Data: []*v1beta1.EventValue{},
+	}
+
+	output, err := detector.OnEvent(context.Background(), event)
+	require.NoError(t, err)
+	assert.Len(t, output, 1, "Expected detection for hooked_syscall event")
+}
