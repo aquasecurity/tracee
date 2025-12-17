69030cc70f2bf5a245c6b6faa52fa745087d9958
Geyslan Gregório <gg@condado.dev>
2025-12-16 22:17:41 -0300

    wip

--- scripts/installation/pull-test-images.sh
+++ scripts/installation/pull-test-images.sh
@@ -22,7 +22,7 @@ require_cmds docker
 IMAGES="
 public.ecr.aws/docker/library/busybox:1.37.0@sha256:e3652a00a2fabd16ce889f0aa32c38eec347b997e73bd09e69c962ec7f8732ee
 public.ecr.aws/docker/library/ubuntu:jammy-20240911.1@sha256:0e5e4a57c2499249aafc3b40fcd541e9a456aab7296681a3994d631587203f97
-ghcr.io/aquasecurity/tracee-tester:latest
+ghcr.io/aquasecurity/tracee-tester:latest@sha256:7f93e117d9b5ba99797171805139238f6c91a80dbd77846eb7e35587e4c70a6e
 "
 
 # Delay between pulls to diminish load on registries
