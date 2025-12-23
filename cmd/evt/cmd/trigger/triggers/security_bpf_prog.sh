50154532a7e4480ffa1189e4af47f7947ac97ba6
Geyslan Gregório <gg@condado.dev>
2025-12-23 18:17:32 -0300

    chore(triggers): remove commented-out event counts
    The counts are misleading as they do not reflect actual event counts
    which depend on the system (environment tooling versions).

--- cmd/evt/cmd/trigger/triggers/security_bpf_prog.sh
+++ cmd/evt/cmd/trigger/triggers/security_bpf_prog.sh
@@ -2,10 +2,4 @@
 
 # security_bpf_prog
 
-# sched_process_exec   1
-# arch_prctl           2
-# security_bpf_prog  487
-# security_file_open   3
-# sched_process_exit   1
-
 bpftool prog dump xlated name trace_execute_finished
